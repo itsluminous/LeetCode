f91e43708a63e99564aa163a9190f91fe3d70850
Prakash <prakashait@hotmail.com>
2021-11-18 20:37:15 +0530

    better solution

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cs
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cs
@@ -1,4 +1,25 @@
+// O(n) - no sorting
 public class Solution {
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        var n = nums.Length;
+        
+        // loop 1 : mark all indexes which are present by making them negative
+        for(var i=0; i<n; i++) {
+            int m = Math.Abs(nums[i])-1;     // index start from 0 hence -1
+            nums[m] = nums[m] > 0 ? -nums[m] : nums[m];     // we want to keep nums[m] negative
+        }
+        
+        var result = new List<int>();
+        for(var i = 0; i<n; i++) {
+            if(nums[i] > 0)         // if no one marked it negative in above loop
+                result.Add(i+1);    // +1 because i is index, we want number
+        }
+        return result;
+    }
+}
+
+// Accepted - using sorting
+public class Solution1 {
     public IList<int> FindDisappearedNumbers(int[] nums) {
         var n = nums.Length;
         Array.Sort(nums);
