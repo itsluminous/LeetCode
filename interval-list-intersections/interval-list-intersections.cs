aa01e9bc8db970741d530da7bc64f93af09bbd05
Prakash <prakashait@hotmail.com>
2021-11-24 22:27:59 +0530

    All solutions by me

--- interval-list-intersections/interval-list-intersections.cs
+++ interval-list-intersections/interval-list-intersections.cs
@@ -1,7 +1,30 @@
 public class Solution {
     public int[][] IntervalIntersection(int[][] firstList, int[][] secondList) {
-        // if(firstList.Length == 0 || secondList.Length == 0) return new int[][2];
+        if(firstList.Length == 0 || secondList.Length == 0) return new int[0][];
         
+        var result = new List<int[]>();
+        int f=0, s=0, start, end;
+        
+        while(f<firstList.Length && s<secondList.Length){
+            start = Math.Max(firstList[f][0], secondList[s][0]);
+            end = Math.Min(firstList[f][1], secondList[s][1]);
+            if(start <= end)
+                result.Add(new []{start,end});
+            
+            //Now increment the one with lower endpoint
+            if(firstList[f][1] < secondList[s][1])
+                f++;
+            else
+                s++;
+        }
+        
+        return result.ToArray();
+    }
+}
+
+// Accepted
+public class Solution {
+    public int[][] IntervalIntersection(int[][] firstList, int[][] secondList) {
         var intersections = new List<int[]>();
         int i=0, j=0;
         
@@ -40,5 +63,52 @@ public class Solution {
         }
         
         return intersections.ToArray();
+    }
+}
+
+// Accepted
+public class Solution1 {
+    public int[][] IntervalIntersection(int[][] firstList, int[][] secondList) {
+        if(firstList.Length == 0 || secondList.Length == 0)
+            return new int[0][];
+        
+        var result = new List<int[]>();
+        int f=0, s=0, start, end;
+        
+        while(f<firstList.Length && s<secondList.Length){
+            // first list ends before second list starts
+            if(firstList[f][1] < secondList[s][0]){
+                f++;
+                continue;
+            }
+            // second list ends before first list starts
+            else if(secondList[s][1] < firstList[f][0]){
+                s++;
+                continue;
+            }
+            
+            // use the starting point as the highest one of two starts
+            if(firstList[f][0] < secondList[s][0]){
+                start = secondList[s][0];
+            }
+            else
+                start = firstList[f][0];
+            
+            // if first list ends after second list starts
+            if(firstList[f][1] >= secondList[s][0]){
+                // if first list ends after second list ends
+                if(firstList[f][1] >= secondList[s][1])
+                    end = secondList[s++][1];
+                // if first list ends before second list ends
+                else
+                    end = firstList[f++][1];
+                result.Add(new []{start,end});
+            }
+            else{
+                f++;
+            }
+        }
+        
+        return result.ToArray();
     }
 }
