4fc8dfa473b725bd7d05d3b9cd949601e14fcb63
Prakash <prakashait@hotmail.com>
2021-11-26 22:23:42 +0530

    Update search-insert-position.cs

--- search-insert-position/search-insert-position.cs
+++ search-insert-position/search-insert-position.cs
@@ -1,3 +1,4 @@
+// Using inbuilt function
 public class Solution {
     public int SearchInsert(int[] nums, int target) {
         // suppose you are searching 4 in the list 1,2,3,5. BinarySearch will return -4 
@@ -6,5 +7,25 @@ public class Solution {
         if(pos < 0) pos = -1 * (pos+1);
         
         return pos;
+    }
+}
+
+// Accepted
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        var lo = 0;
+        var hi = nums.Length -1;
+        
+        while(lo <= hi){
+            var mid = lo + (hi - lo)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                lo = mid+1;
+            else
+                hi = mid-1;
+        }
+        
+        return lo;
     }
 }
