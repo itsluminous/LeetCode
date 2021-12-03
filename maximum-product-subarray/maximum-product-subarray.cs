531eff3f62bbe5c2413d79c8248214fce9cd53f9
Prakash <prakashait@hotmail.com>
2021-12-03 10:20:19 +0530

    Time: 88 ms (67.12%), Space: 38.2 MB (68.61%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.cs
@@ -0,0 +1,50 @@
+// Simplified
+public class Solution {
+    public int MaxProduct(int[] nums) {
+        var max = nums[0];
+        for(int i=1, currMax=max, currMin=max; i<nums.Length; i++){
+            // multiplied by a negative makes big number smaller, small number bigger
+            // so we redefine the extremums by swapping them
+            if(nums[i] < 0){
+                var temp = currMax;
+                currMax = currMin;
+                currMin = temp;
+            }
+            
+            currMax = Math.Max(currMax*nums[i], nums[i]);
+            currMin = Math.Min(currMin*nums[i], nums[i]);
+            max = Math.Max(max, currMax);
+        }
+        
+        return max;
+    }
+}
+
+// Naive
+public class Solution1 {
+    public int MaxProduct(int[] nums) {
+        int negProduct = 0, posProduct = 0, max=int.MinValue, n = nums.Length;
+        
+        if(n == 1) return nums[0];   // input like [-5] should not return 0
+        
+        for(var i=0; i<n; i++){
+            if(nums[i] == 0){
+                max = Math.Max(negProduct, posProduct);
+                negProduct = 0;
+                posProduct = 0;
+            }
+            else if(nums[i] < 0){
+                var curr = Math.Max(nums[i], negProduct*nums[i]);
+                negProduct = Math.Min(nums[i], posProduct*nums[i]);
+                posProduct = curr;
+            }
+            else{
+                posProduct = Math.Max(nums[i], posProduct*nums[i]);
+                negProduct = Math.Min(nums[i], negProduct*nums[i]);
+            }
+            max = Math.Max(max, posProduct);
+        }
+        
+        return Math.Max(max, posProduct);
+    }
+}
