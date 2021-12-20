c3129d8210cc68350cc62b05ee955a0e3fa6944c
Prakash <prakashait@hotmail.com>
2021-12-20 08:07:00 +0530

    Update minimum-absolute-difference.cs

--- minimum-absolute-difference/minimum-absolute-difference.cs
+++ minimum-absolute-difference/minimum-absolute-difference.cs
@@ -2,16 +2,19 @@ public class Solution {
     public IList<IList<int>> MinimumAbsDifference(int[] arr) {
         Array.Sort(arr);
         var result = new List<IList<int>>();
-        var diff = arr[1] - arr[0];
+        var diff = int.MaxValue;
+        
         for(var i=1; i<arr.Length; i++){
             // if we found a pair with lesser difference, then we start a new list with this difference
             if(arr[i] - arr[i-1] < diff){
                 diff = arr[i] - arr[i-1];
                 result = new List<IList<int>>();
-            }
-            
-            if(arr[i] - arr[i-1] == diff)
                 result.Add(new List<int>{arr[i-1], arr[i]});
+            }
+            // if the current pair difference matches min diff, then add it to result
+            else if(arr[i] - arr[i-1] == diff)
+                result.Add(new List<int>{arr[i-1], arr[i]});
+            
         }
         
         return result;
