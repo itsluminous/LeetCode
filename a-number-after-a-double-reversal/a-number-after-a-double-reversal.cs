7d76937a17c4cbbdab416e7a30a2ab2f8fe08693
Prakash <prakashait@hotmail.com>
2021-12-26 15:42:08 +0530

    Update a-number-after-a-double-reversal.cs

--- a-number-after-a-double-reversal/a-number-after-a-double-reversal.cs
+++ a-number-after-a-double-reversal/a-number-after-a-double-reversal.cs
@@ -1,4 +1,12 @@
+// one liner
 public class Solution {
+    public bool IsSameAfterReversals(int num) {
+        return num == 0 || num % 10 > 0;
+    }
+}
+
+// Accepted - more detailed
+public class Solution1 {
     public bool IsSameAfterReversals(int num) {
         if(num < 10) return true;           // single digit
         if(num % 10 == 0) return false;     // number ends with zero
