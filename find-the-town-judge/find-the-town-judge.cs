0abe2dbffdf1ef887a29a91128a5021c10b6e0ac
Prakash <prakashait@hotmail.com>
2022-01-03 06:04:55 +0530

    added better solution

--- find-the-town-judge/find-the-town-judge.cs
+++ find-the-town-judge/find-the-town-judge.cs
@@ -1,4 +1,27 @@
+// With just one array
 public class Solution {
+    public int FindJudge(int n, int[][] trust) {
+        // array to track who is trusted by how many
+        var trustCount = new int[n+1];
+        
+        // fill up trust count
+        foreach(var t in trust){
+            trustCount[t[0]]--;
+            trustCount[t[1]]++;
+        }
+        
+        // check for any node which is trusted by everyone except them
+        for(var i=1; i<=n; i++)
+            if(trustCount[i] == n-1)
+                return i;
+        
+        // if we did not find any such node, then there is no conclusion
+        return -1;
+    }
+}
+
+// Accepted - with graph
+public class Solution1 {
     public int FindJudge(int n, int[][] trust) {
         // array to track who does not trust anyone
         var hasTrust = new bool[n+1];
