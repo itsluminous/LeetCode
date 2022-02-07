49d8ffb15eb6c074b58101acb4e15f21d7fd9a97
Prakash <prakashait@hotmail.com>
2022-02-07 13:48:40 +0530

    Time: 119 ms (49.14%), Space: 37.9 MB (51.29%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cs
+++ 389-find-the-difference/389-find-the-difference.cs
@@ -1,4 +1,19 @@
+// using XOR
 public class Solution {
+    public char FindTheDifference(string s, string t) {
+        int result = 0, n = s.Length;
+        for(var i=0; i<n; i++){
+            result ^= s[i];
+            result ^= t[i];
+        }
+        result ^= t[n];
+        
+        return (char)result;
+    }
+}
+
+// Accepted - using array
+public class Solution1 {
     public char FindTheDifference(string s, string t) {
         var letters = new int[26];
         foreach(var ch in s)
