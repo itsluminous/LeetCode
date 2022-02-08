140b550027f8cbe22aca302ba98827af122bce00
Prakash <prakashait@hotmail.com>
2022-02-08 18:49:54 +0530

    Time: 53 ms (15.71%), Space: 24.9 MB (97.62%) - LeetHub

--- 258-add-digits/258-add-digits.cs
+++ 258-add-digits/258-add-digits.cs
@@ -1,4 +1,14 @@
+// O(1) - To understand it, look up explaination for "Digital Root"
 public class Solution {
+    public int AddDigits(int num) {
+        if(num == 0) return 0;
+        if(num %9 == 0) return 9;   // if number is divisible by 9
+        return num % 9;             // if number is not divisible by 9
+    }
+}
+
+// Accepted - Not O(1)
+public class Solution1 {
     public int AddDigits(int num) {
         while(num >= 10){
             var sum = 0;
