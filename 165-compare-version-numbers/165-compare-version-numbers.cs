44c33ed208fb5ba7e7b4b327ae90e46b2c715ddf
Prakash <prakashait@hotmail.com>
2022-02-25 22:53:37 +0530

    Time: 72 ms (78.54%), Space: 35.3 MB (39.73%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cs
+++ 165-compare-version-numbers/165-compare-version-numbers.cs
@@ -2,6 +2,25 @@ public class Solution {
     public int CompareVersion(string version1, string version2) {
         var v1 = version1.Split('.');
         var v2 = version2.Split('.');
+        var len = Math.Max(v1.Length, v2.Length);
+        
+        for (var i=0; i<len; i++) {
+            var val1 = i < v1.Length ? Convert.ToInt32(v1[i]) : 0;
+            var val2 = i < v2.Length ? Convert.ToInt32(v2[i]) : 0;
+            
+            if(val1 < val2) return -1;
+            if(val1 > val2) return 1;
+        }
+
+        return 0;
+    }
+}
+
+// Accepted
+public class Solution1 {
+    public int CompareVersion(string version1, string version2) {
+        var v1 = version1.Split('.');
+        var v2 = version2.Split('.');
         
         int v1idx = 0, v2idx = 0;
         while(v1idx < v1.Length && v2idx < v2.Length){
