b9580add3273e2e7d1783f5954bec42eb8ea88eb
Prakash <prakashait@hotmail.com>
2022-04-30 09:56:45 +0530

    Time: 136 ms (99.53%), Space: 45.3 MB (20.28%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cs
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cs
@@ -1,4 +1,26 @@
+// Better time complexity
 public class Solution {
+    public IList<int> Intersection(int[][] nums) {
+        var n = nums.Length;
+        var count  = new int[1001];
+        var result = new List<int>();
+        
+        // check number of occurence of each digit in all nums
+        foreach(var num in nums)
+            foreach(var i in num)
+                count[i]++;
+        
+        // any num which has count equal to n is present in all nums array
+        for(var i=0; i<count.Length; i++)
+           if(count[i] == n)
+               result.Add(i);
+        
+        return result;
+    }
+}
+
+// Accepted
+public class Solution1 {
     public IList<int> Intersection(int[][] nums) {
         var n = nums.Length;
         var arr1 = nums[0].ToList();
