255c0a0603e1cef70e11c8acb48accf5cfee2ef8
Prakash <prakashait@hotmail.com>
2022-05-02 12:42:24 +0530

    Time: 255 ms (9.45%), Space: 45.2 MB (45.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cs
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cs
@@ -1,4 +1,27 @@
+// Better complexity
 public class Solution {
+    public int[] SortArrayByParity(int[] nums) {
+        int start = 0, end = nums.Length-1;
+        while(start < end){
+            // if num at start idx is odd and end idx is even, swap
+            if(nums[start] % 2 > nums[end] % 2){
+                var temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+            }
+            
+            // if num at start idx is even, then check next num
+            if(nums[start] % 2 == 0) start++;
+            // if num at end idx is odd, then search for next even num on left side
+            if(nums[end] % 2 == 1) end--;
+        }
+        
+        return nums;
+    }
+}
+
+// Accepted
+public class Solution1 {
     public int[] SortArrayByParity(int[] nums) {
         var endIdx = nums.Length-1;
         for(var startIdx=0; startIdx<endIdx; startIdx++){
