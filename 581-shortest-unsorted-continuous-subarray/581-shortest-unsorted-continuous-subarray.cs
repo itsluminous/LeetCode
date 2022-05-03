611420301b74ddadd274f199de27cc4b1eaaa0da
Prakash <prakashait@hotmail.com>
2022-05-03 10:54:37 +0530

    Time: 125 ms (66.67%), Space: 42.1 MB (62.74%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cs
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cs
@@ -1,4 +1,34 @@
+// Accepted - using stack - O(n)
 public class Solution {
+    public int FindUnsortedSubarray(int[] nums) {
+        int n = nums.Length, start = n, end = 0;
+        var stack = new Stack<int>();
+        
+        // find start index of unsorted array
+        for(var i=0; i<n; i++){
+            // if curr number is greater than the one on top, keep popping till we find appropriate place
+            while(stack.Count > 0 && nums[stack.Peek()] > nums[i])
+                start = Math.Min(start, stack.Pop());
+            stack.Push(i);
+        }
+        
+        stack.Clear();
+        
+        // find end index of unsorted array
+        for(var i=n-1; i>=0; i--){
+            // if curr number is smaller than the one on top, keep popping till we find appropriate place
+            while(stack.Count > 0 && nums[stack.Peek()] < nums[i])
+                end = Math.Max(end, stack.Pop());
+            stack.Push(i);
+        }
+        
+        // return length of subarray to be sorted
+        return end-start > 0 ? end-start+1 : 0;
+    }
+}
+
+// Accepted - O(n log n)
+public class Solution1 {
     public int FindUnsortedSubarray(int[] nums) {
         int n = nums.Length, start = -1, end = -1;
         
