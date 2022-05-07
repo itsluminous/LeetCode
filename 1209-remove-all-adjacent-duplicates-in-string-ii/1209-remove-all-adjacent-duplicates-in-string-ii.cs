4b8b72ddaeb47aac1b49d1e3fdd33ff3bc4b234d
Prakash <prakashait@hotmail.com>
2022-05-07 09:22:38 +0530

    Time: 129 ms (64.57%), Space: 40 MB (91.34%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cs
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cs
@@ -1,71 +1,24 @@
 public class Solution {
-    public string RemoveDuplicates(string s, int k) 
-    {
-	   Stack<(char c, int frequency)> stack = new Stack<(char, int)>();
-
-		foreach (char c in s)
-		{
-			if (stack.Count > 0 && stack.Peek().c == c)
-			{
-				var top = stack.Pop();
-				top.frequency++;
-
-				if (top.frequency != k)
-				{
-					stack.Push(top);
-				}
-			}
-			else
-			{
-				stack.Push((c, 1));
-			}
-		}
-
-		StringBuilder result = new StringBuilder();
-		var resultList = stack.ToArray().Reverse();
-		foreach ((char c, int frequency) in resultList)            
-		{
-			result.Append(c, frequency);
-		}
-		return result.ToString();
-    }
-    
-    public string RemoveDuplicates1(string s, int k) {
-        int n = s.Length, currLen = 0;
-        char curr = '0';
-        StringBuilder sb = new StringBuilder(), currsb = new StringBuilder();
-        
-        while(s.Length > 0){
-            for(var i=0; i<n; i++){
-                if(currLen == 0){       // starting new substring
-                    curr = s[i];
-                    currLen++;
-                    currsb.Append(s[i]);
-                }
-                else if(s[i] == curr){  // if curr char is part of existing substring
-                    currLen++;
-                    currsb.Append(s[i]);
-                }   
-                else{                   // if curr char is different from prev substring
-                    Console.WriteLine($"currsb={currsb.ToString()}");
-                    if(currsb.Length < k)
-                        sb.Append(currsb);
-                    else{
-                        var remaining = currsb.Length % k;
-                        sb.Append(currsb.ToString(0, remaining));
-                    }
-                    currsb = new StringBuilder();
-                    currLen = 0;
-                    curr = '0';
-                }
+    public string RemoveDuplicates(string s, int k) {
+        Stack<(char ch, int freq)> stack = new Stack<(char, int)>();
+        foreach(var c in s){
+            if(stack.Count > 0 && stack.Peek().ch == c){
+                var top = stack.Pop();
+                top.freq++;
+                
+                if(top.freq != k)       // if freq == k then drop this substring
+                    stack.Push(top);
             }
-            var newstr = sb.ToString();
-            Console.WriteLine($"s={s}, newstr={newstr}");
-            if(newstr.Equals(s)) break; // cannot minify now
-            s = newstr;
+            else
+                stack.Push((c, 1));
         }
-        return s;
+        
+        var sb = new StringBuilder();
+        while(stack.Count > 0){
+            var top = stack.Pop();
+            sb.Append(top.ch, top.freq);
+        }
+        
+        return new string(sb.ToString().Reverse().ToArray());   // reverse the stringbuilder
     }
 }
-
-
