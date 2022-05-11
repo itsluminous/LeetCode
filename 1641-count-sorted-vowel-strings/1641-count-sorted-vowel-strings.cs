f1632e8ace01cd2b27693b17d1c3c56ee0d57184
Prakash <prakashait@hotmail.com>
2022-05-11 12:14:32 +0530

    Time: 28 ms (71.43%), Space: 25.4 MB (52.38%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cs
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cs
@@ -1,5 +1,18 @@
-// 0 = a, 1 = e, 2 = i, 3 = o, 4 = u
+// Simply try to figure out how to place four separators inside result string
+// all a's will be before 1st separator, b's before 2nd, c's before 3rd, d's before 4th and e's after 4th
+// now it becomes a combination problem
+// Not permutations because numbers need to be sorted, so we only count "112", not "121", "211".
 public class Solution {
+    public int CountVowelStrings(int n) {
+        // formula for combination i.e. arranging k items in n set => (n+k-1)! / (n! * (k-1)!) 
+        // in our case k = 5, so (n+4)! / (n! * 4!)  =  (n+4)*(n+3)*(n+2)*(n+1)* n! / (n! * 24)
+        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
+    }
+}
+
+// Accepted - Backtracking
+// 0 = a, 1 = e, 2 = i, 3 = o, 4 = u
+public class Solution1 {
     int count = 0;
     
     public int CountVowelStrings(int n) {
