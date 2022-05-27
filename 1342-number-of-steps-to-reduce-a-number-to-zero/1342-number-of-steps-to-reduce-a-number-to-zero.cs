6ac22e642cefe37cdd4ef89f986b22d5b98a278e
Prakash <prakashait@hotmail.com>
2022-05-27 19:39:57 +0530

    Time: 47 ms (6.80%), Space: 24.9 MB (97.28%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -1,4 +1,23 @@
+// Bit manipulation
+// In binary, for 0, result += 1 because we are doing divide;
+// for 1, result += 2 because we first do "-1" then do a divide;
 public class Solution {
+    public int NumberOfSteps(int num) {
+        if(num == 0) return 0;
+        
+        var steps = 0;
+        do{
+            if((num&1) == 1) steps+= 2;     // if odd, then two steps
+            else steps++;                   // if even, then one step
+            num = num>>1;
+        }while(num != 0);
+        
+        return steps-1; // leftmost 1 we only need to -1 and we no need to divide by 2, so reducing 1 step
+    }
+}
+
+// Accepted
+public class Solution1 {
     public int NumberOfSteps(int num) {
         var steps = 0;
         while(num != 0){
