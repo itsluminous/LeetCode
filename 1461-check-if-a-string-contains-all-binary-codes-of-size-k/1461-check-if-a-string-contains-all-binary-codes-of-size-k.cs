dedc431828c13adfaf3a7bf01a53ea87aa3b403b
Prakash <prakashait@hotmail.com>
2022-05-31 19:41:29 +0530

    Time: 354 ms (11.11%), Space: 55 MB (88.89%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
@@ -1,6 +1,24 @@
 public class Solution {
     public bool HasAllCodes(string s, int k) {
         // create a set of all possible numbers in given string
+        var codes = new HashSet<string>();
+        for(var i=0; i<=s.Length-k; i++){
+            codes.Add(s.Substring(i, k));
+        }
+        
+        // max possible number with k size
+        var max = 1 << k;
+        
+        // if total items in set is not same as max possible, then some numbers are missing
+        if(max != codes.Count) return false;
+        return true;
+    }
+}
+
+// Accepted - same logic as above but extra loops and string to int conversion
+public class Solution1 {
+    public bool HasAllCodes(string s, int k) {
+        // create a set of all possible numbers in given string
         var codes = new HashSet<int>();
         for(var i=0; i<=s.Length-k; i++){
             var str = s.Substring(i, k);
