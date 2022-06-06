247fd39df156c997290a00090e066d5157ecd144
Prakash <prakashait@hotmail.com>
2022-06-06 22:12:58 +0530

    Time: 190 ms (29.93%), Space: 45.3 MB (94.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cs
@@ -1,4 +1,24 @@
+// Without using any extra space
+// Explaination - https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/49785/Java-solution-without-knowing-the-difference-in-len!/165648
+
 public class Solution {
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        
+        while(a != b){
+            // if a or b reaches end then point them to head of alternate list.
+            // this way, the difference in length of the lists will be offset
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        
+        return a;
+    }
+}
+
+// Accepted - using hashset
+public class Solution1 {
     public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
         var set = new HashSet<ListNode>();
         ListNode a = headA, b = headB;
