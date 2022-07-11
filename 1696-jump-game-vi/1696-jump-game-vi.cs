64fb9f20e71eb7c5d079694abe44221e22ec08c0
Prakash <prakashait@hotmail.com>
2022-07-11 20:11:35 +0530

    Time: 364 ms (63.39%), Space: 49.2 MB (72.32%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cs
+++ 1696-jump-game-vi/1696-jump-game-vi.cs
@@ -1,24 +1,31 @@
 public class Solution {
     public int MaxResult(int[] nums, int k)
     {
-        int[] scores = new int[nums.Length];
+        var n = nums.Length;
+        
+        // keep track of max score achievable for each index
+        int[] scores = new int[n];
         scores[0] = nums[0];
+        
         LinkedList<int> q = new LinkedList<int>();
-        q.AddLast(0);
+        q.AddLast(0);   // first step we have to always include
 
-        for (int i = 1; i < nums.Length; i++)
+        for (int i = 1; i < n; i++)
         {
+            // we want to keep only k items in queue, so remove extra from start of window
             while (i - q.First.Value > k)
                 q.RemoveFirst();
 
+            // the topmost index in queue will have highest sum, so add it to current index's score
             scores[i] = scores[q.First.Value] + nums[i];
 
+            // if we had reached any index with lesser score than current, remove it from queue
             while (q.Count > 0 && scores[i] >= scores[q.Last.Value])
                 q.RemoveLast();
 
             q.AddLast(i);
         }
 
-        return scores[nums.Length - 1];
+        return scores[n-1];
     }
 }
