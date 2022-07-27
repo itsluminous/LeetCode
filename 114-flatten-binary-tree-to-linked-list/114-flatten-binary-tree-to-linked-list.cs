d949602c5e50c95f82950822b360a12ece3d2cdf
Prakash <prakashait@hotmail.com>
2022-07-27 20:51:32 +0530

    Time: 90 ms (94.94%), Space: 39.4 MB (8.23%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    TreeNode prev = null;
+    
+    public void Flatten(TreeNode root) {
+        if(root == null) return;
+        
+        Flatten(root.right);
+        Flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
