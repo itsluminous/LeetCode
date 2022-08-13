82896e5a35940f143e066ba9589c49b0962154dd
Prakash <prakashait@hotmail.com>
2022-08-13 19:47:14 +0530

    Time: 477 ms (45.41%), Space: 46.1 MB (86.49%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cs
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public IList<int> FindSubstring(string s, string[] words) {
+        // create dictionary of words and their count
         var map = new Dictionary<string, int>();
         foreach(var word in words){
             if(map.ContainsKey(word)) map[word]++;
@@ -11,7 +12,7 @@ public class Solution {
         var substrlen = words.Length * wordlen;     // this will be sliding window length
         for(var l=0; l+substrlen <= s.Length; l++){
             var i = l;
-            var dict = map.ToDictionary(entry => entry.Key, entry => entry.Value);
+            var dict = map.ToDictionary(entry => entry.Key, entry => entry.Value);  // clone dict
             do{
                 var word = s.Substring(i, wordlen);
                 if(dict.ContainsKey(word) && dict[word] > 0) dict[word]--;
@@ -20,6 +21,7 @@ public class Solution {
                 i += wordlen;
             }while(i < l + substrlen);
             
+            // if we found all words, then this index can be part of ans
             if(i >= l + substrlen) result.Add(l);
         }
         
