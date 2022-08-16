f66bb40e029893a12527cb6041d4d07d402d3d5f
Prakash <prakashait@hotmail.com>
2022-08-16 10:54:21 +0530

    Time: 73 ms (51.72%), Space: 28.6 MB (51.72%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cs
@@ -0,0 +1,27 @@
+public class Solution1 {
+    public bool IsPalindrome(int x) {
+        // negative nums & nums ending with 0 can never be palindrome
+        if(x < 0 || (x % 10 == 0 && x!= 0)) return false;    
+        
+        var rev = 0;    // reverse only half the number and then compare the two halves
+        while(x > rev){
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        
+        return x == rev || x == rev/10;
+    }
+}
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        // negative nums & nums ending with 0 can never be palindrome
+        if(x < 0 || (x % 10 == 0 && x!= 0)) return false;    
+        
+        var str = x.ToString();
+        var n = str.Length;
+        for(var i=0; i<n/2; i++)
+            if(str[i] != str[n-i-1]) return false;
+        return true;
+    }
+}
