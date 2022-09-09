946f349b0dcfe22405e6c998451969adb15b310a
Prakash <prakashait@hotmail.com>
2022-09-09 19:04:46 +0530

    Time: 656 ms (68.08%), Space: 71.2 MB (8.51%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cs
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cs
@@ -1,4 +1,28 @@
+// same as other soln, except that we sort same attack players by desc defence
+// this way automatically same attack players never get compared
 public class Solution {
+    public int NumberOfWeakCharacters(int[][] properties) {
+        int n = properties.Length, weak = 0;
+        
+        // 1. sort the array by attack. so any player at next idx will always have bigger attack
+        var prop = properties.OrderBy(p => p[0]).ThenByDescending(p => p[1]).ToArray();
+        
+        // 2. for each idx, find out max defence of right side players
+        var lis = new int[n];
+        lis[n-1] = -1;    // nothing greater than the last one
+        for(var i=n-2; i>=0; i--)
+            lis[i] = Math.Max(lis[i+1], prop[i+1][1]);
+        
+        // 3. if there is any player on right side with better defence, then they already have better attack.
+        for(var i=0; i<n; i++)
+            if(lis[i] > prop[i][1]) weak++;
+        
+        return weak;
+    }
+}
+
+// Accepted
+public class Solution1 {
     public int NumberOfWeakCharacters(int[][] properties) {
         int n = properties.Length, weak = 0;
         
