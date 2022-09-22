941530299a6b55011e3ce35ed7a6a83ce4138c51
Prakash <prakashait@hotmail.com>
2022-09-22 22:56:08 +0530

    Time: 141 ms (48.99%), Space: 39.3 MB (94.41%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cs
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cs
@@ -9,3 +9,33 @@ public class Solution {
         return string.Join(" ", words);
     }
 }
+
+// Accepted
+public class Solution1 {
+    public string ReverseWords(string s) {
+        var wEnd = 0;
+        var wStart = 0;
+        var sb = new StringBuilder();
+        
+        while(wEnd < s.Length){
+            if(s[wEnd] == ' '){
+                sb.Append(ReverseWord(s.Substring(wStart, wEnd-wStart).ToCharArray()));
+                sb.Append(' ');
+                wStart = wEnd+1;
+            }
+            wEnd++;
+        }
+        
+        sb.Append(ReverseWord(s.Substring(wStart, wEnd-wStart).ToCharArray()));
+        return sb.ToString();
+    }
+    
+    private string ReverseWord(char[] s){
+        for(int start=0, end=s.Length-1;start < end;start++,end--){
+            var temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+        }
+        return new string(s);
+    }
+}
