49a03dc99dc602c83eb5a487571a374a564b157e
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-01-05 21:09:39 +0530

    updated implementation of lis

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cs
@@ -1,5 +1,25 @@
-// https://www.youtube.com/watch?v=mouCn3CFpgg
+// Greedy with binary search
+// https://leetcode.com/problems/longest-increasing-subsequence/solutions/1326308/c-python-dp-binary-search-bit-segment-tree-solutions-picture-explain-o-nlogn/?envType=daily-question&envId=2024-01-05
 public class Solution {
+    public int LengthOfLIS(int[] nums) {
+		List<int> lst = new(nums.Length);
+        foreach(var num in nums){
+            if(lst.Count == 0 || lst[lst.Count-1] < num)
+                lst.Add(num);
+            else{
+                var idx = lst.BinarySearch(num);
+                // idx will be >= 0 if exact match is found
+                // bitwise complement to find the first number greater than num
+                if (idx < 0) idx = ~idx;
+                lst[idx] = num;
+            }
+        }
+        return lst.Count;
+    }
+}
+
+// https://www.youtube.com/watch?v=mouCn3CFpgg
+public class SolutionDP {
     public int LengthOfLIS(int[] nums) {
         var n = nums.Length;
         if(n <= 1) return n;
