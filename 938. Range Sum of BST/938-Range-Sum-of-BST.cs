6113f41f0851f5e03ea948c643d06263c2c08954
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-01-09 06:30:50 +0530

    938. Range Sum of BST

--- 938. Range Sum of BST/938-Range-Sum-of-BST.cs
+++ 938. Range Sum of BST/938-Range-Sum-of-BST.cs
@@ -1,38 +1,33 @@
-// Logic : sort based on start time, then find best possible combination using DFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
+ // Ignore branches which will never have right value
 public class Solution {
-    Dictionary<int,int> dp;
-
-    public int JobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        var n = startTime.Length;
-        var jobs = new int[n][];
-        for(var i=0; i<n; i++){
-            jobs[i] = new []{startTime[i], endTime[i], profit[i]};
-        }
-
-        jobs = jobs.OrderBy(row => row[0]).ToArray();
-
-        dp = new Dictionary<int,int>();
-        return DFS(0, jobs);
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) return 0;
+        if(root.val < low) return RangeSumBST(root.right, low, high);
+        if(root.val > high) return RangeSumBST(root.left, low, high);
+        return root.val + RangeSumBST(root.right, low, high) + RangeSumBST(root.left, low, high);
     }
+}
 
-    private int DFS(int cur, int[][] jobs){
-        if(cur == jobs.Length) return 0;
-        if(dp.ContainsKey(cur)) return dp[cur];
-        
-        var next = FindNext(cur, jobs);
-        var inclCurr = jobs[cur][2] + (next == -1 ? 0 : DFS(next, jobs));
-        var exclCurr = DFS(cur+1, jobs);
-
-        dp[cur] = Math.Max(inclCurr, exclCurr);
-        return dp[cur];
-    }
-
-    private int FindNext(int cur, int[][] jobs){
-        for(var next=cur+1; next < jobs.Length; next++){
-            if(jobs[next][0] >= jobs[cur][1])
-                return next;
-        }
-        return -1;
+// This also passes
+public class Solution1 {
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) return 0;
+        var sum = (root.val >= low && root.val <= high) ? root.val : 0;
+        sum += RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        return sum;
     }
 }
