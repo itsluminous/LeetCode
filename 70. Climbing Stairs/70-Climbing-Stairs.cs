d3f90cba5503bf61a48a84755f0206745ef1f31b
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-01-18 20:41:42 +0530

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/70-Climbing-Stairs.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        var dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
+        for(var i=2; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+
+        return dp[n];
+    }
+}
