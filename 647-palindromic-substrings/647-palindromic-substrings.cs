43aa13555da488269cb4c56baafbe8408f1f3102
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-02-10 12:10:56 +0530

    647. Palindromic Substrings

--- 647-palindromic-substrings/647-palindromic-substrings.cs
+++ 647-palindromic-substrings/647-palindromic-substrings.cs
@@ -1,5 +1,52 @@
-// Idea is that for each character, expand on both sides
+// DP with less loops
 public class Solution {
+    public int CountSubstrings(string s) {
+        int n = s.Length, count = 0;
+        var dp = new bool[n,n];  // to check if string with given start & end is palindrome index
+        
+        for(var len=1; len<=n; len++){
+            for(int start=0, end=start+len-1; end<n; start++, end++){
+                if((len <= 2 ||  dp[start+1,end-1]) && s[start] == s[end]){
+                    dp[start,end] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+// Accepted - DP
+public class SolutionDP {
+    public int CountSubstrings(string s) {
+        int n = s.Length, count = s.Length;
+        var dp = new bool[n,n];  // to check if string with given start & end is palindrome index
+        for(var i=0; i<n; i++) dp[i,i] = true;  // single char is always palindrome
+        // if 2 consecutive chars is same, then mark it as palindrome
+        for(var i=0; i<n-1; i++){
+            if(s[i] == s[i+1]){
+                dp[i,i+1] = true;
+                count++;
+            }
+        }
+        
+        // now check for string length >= 3
+        for(var len=3; len<=n; len++){
+            for(int start=0, end=start+len-1; end<n; start++, end++){
+                if(dp[start+1,end-1] && s[start] == s[end]){
+                    dp[start,end] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+// Accepted - Idea is that for each character, expand on both sides
+public class SolutionExpand {
     public int CountSubstrings(string s) {
         var count = 0;
         for(var i=0; i<s.Length; i++){
