b800ae0dd2c8197090067db312eafa670c76486d
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-12 20:40:09 +0530

    much better git hook for file renaming

--- 00-hooks/remove_spaces.sh
+++ 00-hooks/remove_spaces.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-repo_root=$(git rev-parse --show-toplevel)
+dir_list=()
+file_list=()
 
-# Get the list of folders
-folders=$(find $repo_root -maxdepth 1 -type d -name "* *")
-
-# Loop through each folder and rename
+# Loop through each file in the staging area
 IFS=$'\n' # Set Internal Field Separator to newline to handle folder names with spaces
-for folder in $folders; do
-    parent_path=$(dirname "$folder")
-    old_name=$(basename "$folder")
-    new_name=$(echo "$old_name" | sed 's/ /-/g' | sed 's/\.-/-/g')
-    mv "$parent_path/$old_name" "$parent_path/$new_name"
-    git add "$parent_path/$old_name"
-    git add "$parent_path/$new_name"
+for file in $(git diff --cached --name-only --diff-filter=ACM); do
+    old_file_name=$(basename "$file")
+    new_file_name=$(echo "$old_file_name" | sed 's/ /-/g' | sed 's/\.-/-/g')
+    old_dir_name=$(dirname "$file")
+    new_dir_name=$(echo "$old_dir_name" | sed 's/ /-/g' | sed 's/\.-/-/g')
+
+    # update file list
+    file_list+=("$new_dir_name/$old_file_name|$new_dir_name/$new_file_name")
+
+    # if we already encountered this folder, then skip renaming
+    if [[ "$old_dir_name" != "$new_dir_name" ]] && ! [[ "${dir_list[@]}" =~ "$old_dir_name|$new_dir_name" ]]; then
+        dir_list+=("$old_dir_name|$new_dir_name")
+    fi
 done
 
-# Add and commit changes
-# git commit --amend --no-edit --no-verify
+# now rename all folders
+for entry in "${dir_list[@]}"; do
+    IFS='|' read -r old new <<< "$entry"
+    mv "$old" "$new"
+    git add "$old"
+    git add "$new"
+done
 
-echo "Spaces removed in folders"
+# now rename all files
+for entry in "${file_list[@]}"; do
+    IFS='|' read -r old new <<< "$entry"
+    mv "$old" "$new"
+    git add "$old"
+    git add "$new"
+done
+
+echo "Spaces removed in files & folders"
