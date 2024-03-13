d4518b8c5a091c79afed6a3d72b1487fdd41a6fa
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-13 11:51:21 +0530

    2485. Find the Pivot Integer

---
+++ 2485-Find-the-Pivot-Integer/2485-Find-the-Pivot-Integer.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int PivotInteger(int n) {
+        int l=1, r=n, sumL = 1, sumR = n;
+        while(l<r){
+            if(sumL < sumR){
+                 l++;
+                 sumL += l;
+            }
+            else{
+                r--;
+                sumR += r;
+            }
+        }
+        if( sumL == sumR) return r;
+        return -1;
+    }
+}
