0876245920c05291019ac25838de09f625a6e968
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-13 13:58:55 +0530

    3. Longest Substring Without Repeating Characters

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
@@ -1,6 +1,29 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
         if(s.Length == 0) return 0;
+        if(s.Length == 1) return 1;
+        const int LIMIT = 256;
+        var visited = new bool[LIMIT];
+        int max = 0, left = 0, right = 0, n = s.Length;   
+        do{
+            var ch = s[right];
+            // keep removing from left till duplicate is gone
+            while(visited[ch]){
+                visited[s[left]] = false;
+                left++;
+            }
+            visited[ch] = true;
+            max = Math.Max(max, right-left+1);
+            right++;
+        }while(left < n && right < n);
+        return max;
+    }
+}
+
+// Accepted
+public class SolutionSet {
+    public int LengthOfLongestSubstring(string s) {
+        if(s.Length == 0) return 0;
         
         var set = new HashSet<char>();
         int maxlen = 0, l=0, r=0;
