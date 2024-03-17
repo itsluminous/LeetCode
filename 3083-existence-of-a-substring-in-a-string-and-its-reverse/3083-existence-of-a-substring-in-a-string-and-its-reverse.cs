eb9cc1a7442054de1d2d35e31e84237b68a747b6
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-17 09:55:19 +0530

    Time: 43 ms (100%), Space: 41.1 MB (100%)

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cs
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cs
@@ -2,11 +2,9 @@ public class Solution {
     public bool IsSubstringPresent(string s) {
         var reverse = new string(s.Reverse().ToArray());
         
-        var words = new List<string>();
-        for(var i=0; i<s.Length-1; i++)
-            words.Add(s.Substring(i, 2));
-        
-        foreach(var word in words){
+        // for each possible substring, check match
+        for(var i=0; i<s.Length-1; i++){
+            var word = s.Substring(i, 2);
             if(s.Contains(word) && reverse.Contains(word))
                 return true;
         }
