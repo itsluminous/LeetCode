c2ec01cd01e41a50b726f81afa1ccf53afbd31a4
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-28 05:07:54 +0530

    Time: 174 ms (83.49%), Space: 57.9 MB (92.78%)

--- 0041-first-missing-positive/0041-first-missing-positive.cs
+++ 0041-first-missing-positive/0041-first-missing-positive.cs
@@ -3,16 +3,13 @@
 class Solution {
     public int FirstMissingPositive(int[] nums) {
         var n = nums.Length;
-        for(var i=0; i<n; i++){
-            while(nums[i] > 0 && nums[i] < n && nums[nums[i]-1] != nums[i]){
+        for(var i=0; i<n; i++)
+            while(nums[i] > 0 && nums[i] < n && nums[nums[i]-1] != nums[i])
                 (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]);
-            }
-        }
 
-        for(var i=1; i<=n; i++){
+        for(var i=1; i<=n; i++)
             if(nums[i-1] != i)
                 return i;
-        }
 
         return n+1;
     }
