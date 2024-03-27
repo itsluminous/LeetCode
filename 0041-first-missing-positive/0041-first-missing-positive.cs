822522b06b186f863491f2a4501813b4d50ff53f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-28 05:00:18 +0530

    Time: 176 ms (77.82%), Space: 57.9 MB (85.73%)

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cs
@@ -0,0 +1,19 @@
+// logic : for every positive number, put it in right index
+// later check which pos number is not at right index
+class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        var n = nums.Length;
+        for(var i=0; i<n; i++){
+            while(nums[i] > 0 && nums[i] < n && nums[nums[i]-1] != nums[i]){
+                (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]);
+            }
+        }
+
+        for(var i=1; i<=n; i++){
+            if(nums[i-1] != i)
+                return i;
+        }
+
+        return n+1;
+    }
+}
