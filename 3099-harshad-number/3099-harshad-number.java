270585d2b34d533e79cc94a10e5b4d4857ece05e
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-03-31 10:23:39 +0530

    Time: 0 ms (100%), Space: 40 MB (66.67%)

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int og = x, sumOfDigits = 0;
+        while(x != 0){
+            sumOfDigits += x%10;
+            x /= 10;
+        }
+        
+        if(og % sumOfDigits == 0) return sumOfDigits;
+        return -1;
+    }
+}
