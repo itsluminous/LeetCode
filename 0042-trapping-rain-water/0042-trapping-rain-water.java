4e393870a8988475e57570ea83d55d6fdc43e006
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-12 09:04:53 +0530

    Time: 1 ms (71.17%), Space: 46.1 MB (55.56%)

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,24 @@
+// water level for any index will be equal to smaller one of largest block on left and right
+// now, subtract height[i] from that to get actual water at that index
+public class Solution {
+    public int trap(int[] height) {
+        var n = height.length;
+
+        // find the maximum height on either side of an index
+        int[] lmax = new int[n], rmax = new int[n];
+        for(int i=1, j=n-2; i<n; i++, j--){
+            lmax[i] = Math.max(lmax[i-1], height[i-1]);
+            rmax[j] = Math.max(rmax[j+1], height[j+1]);
+        }
+
+        // now calculated water at an index, using intution
+        var trapped = 0;
+        for(var i=1; i<n-1; i++){
+            var water = Math.min(lmax[i], rmax[i]);
+            var curr = (water - height[i]) > 0 ? (water - height[i]) : 0;
+            trapped += curr;
+        }
+
+        return trapped;
+    }
+}
