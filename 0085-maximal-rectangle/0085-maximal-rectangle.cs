dc38f9ec9d4391a3cbaf5c0685ac9262d9692c6a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-13 13:41:12 +0530

    Time: 120 ms (75%), Space: 50.9 MB (22.5%)

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cs
@@ -0,0 +1,56 @@
+// this is basically same problem as #84
+// for each row in matrix, create a histogram and then find largest area in histogram
+// explaination video : https://www.youtube.com/watch?v=dAVF2NpC3j4
+public class Solution {
+    public int MaximalRectangle(char[][] matrix) {
+        int m = matrix.Length, n = matrix[0].Length;
+        var max = 0;
+        var heights = new int[n];
+
+        for(var i=0; i<m; i++){
+            for(var j=0; j<n; j++){
+                if(matrix[i][j] == '0')
+                    heights[j] = 0;
+                else
+                    heights[j] += 1;
+            }
+            var area = LargestRectangleArea(heights);
+            max = Math.Max(max, area);
+        }
+
+        return max;
+    }
+
+    // function to find largest square in a histogram
+    private int LargestRectangleArea(int[] heights) {
+        var n = heights.Length;
+        int[] leftLessIndex = new int[n], rightLessIndex = new int[n];
+        leftLessIndex[0] = -1;
+        rightLessIndex[n-1] = n;
+        
+        // for each bar, find the point on left where we get lesser sized bar
+        for(var i=1; i<n; i++){
+            var curr = i-1;
+            while(curr >= 0 && heights[curr] >= heights[i])
+                curr = leftLessIndex[curr];
+            leftLessIndex[i] = curr;
+        }
+        
+        // for each bar, find the point on right where we get lesser sized bar
+        for(var i=n-2; i>=0; i--){
+            var curr = i+1;
+            while(curr < n && heights[curr] >= heights[i])
+                curr = rightLessIndex[curr];
+            rightLessIndex[i] = curr;
+        }
+        
+        // now using this data, find max rectangle
+        var max = 0;
+        for(var i=0; i<n; i++){
+            var area = heights[i] * (rightLessIndex[i] - leftLessIndex[i] - 1);
+            max = Math.Max(max, area);
+        }
+        
+        return max;
+    }
+}
