d7ee6136f3ea3581255ea7d1f44438c2cd7d03b2
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-14 14:39:31 +0530

    Time: 294 ms (50%), Space: 119.1 MB (50%)

---
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.java
@@ -0,0 +1,56 @@
+public class Solution {
+    // 3D dp for "nums index", "andValues index" as row & cols, and  "calculated AND value" as key of HashMap
+    private HashMap<Integer, Integer>[][] dp;
+    int allBitsSet = (1 << 30) - 1;
+    int n, m;
+    int maxVal = (int)1e9;
+
+    public int minimumValueSum(int[] nums, int[] andValues) {
+        n = nums.length;
+        m = andValues.length;
+        dp = new HashMap[n][m];
+
+        // Initialize the calculation with all bits set to 1 in the mask
+        int ans = CalculateMinSum(nums, andValues, allBitsSet, 0, 0);
+
+        // Return the result if it's finite, otherwise return -1
+        return ans < maxVal ? ans : -1;
+    }
+
+    // Define a recursive function to calculate the minimum sum
+    private int CalculateMinSum(int[] nums, int[] andValues, int mask, int n1, int m1) {
+        // if both arrays are exhausted
+        if (m1 == m && n1 == n) return 0;
+        // if either array is exhausted
+        if (m1 == m || n1 == n) return maxVal;
+
+        // Check if the result is already calculated and cached
+        if (dp[n1][m1] != null && dp[n1][m1].containsKey(mask))
+            return dp[n1][m1].get(mask);
+
+        // Update the mask with bitwise AND operation
+        mask &= nums[n1];
+
+        // If the updated mask is less than the corresponding and_value, it's not possible to get the expected value
+        // because the AND value can only decrease if we add more numbers to it
+        if (mask < andValues[m1])
+            return maxVal;
+
+        // If the updated mask equals the corresponding and_value
+        // then try to split the array at this point
+        var split = maxVal;
+        if (mask == andValues[m1])
+            // Add nums[n1] to the sum and increment both n1 and m1
+            split = nums[n1] + CalculateMinSum(nums, andValues, allBitsSet, n1 + 1, m1 + 1);
+
+        // find out the case when we don't split
+        // Increment n1 to move to the next element in nums
+        var notSplit = CalculateMinSum(nums, andValues, mask, n1 + 1, m1);
+
+        // Return the minimum of two cases
+        var result = Math.min(notSplit, split);
+        if (dp[n1][m1] == null) dp[n1][m1] = new HashMap<>();
+        dp[n1][m1].put(mask, result);
+        return result;
+    }
+}
