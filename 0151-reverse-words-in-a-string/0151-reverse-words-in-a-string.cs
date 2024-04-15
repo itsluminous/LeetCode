f51ed42ee516d0552572d95a6dbdaddaf7f5d491
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-15 13:17:15 +0530

    Time: 52 ms (86.63%), Space: 40.8 MB (81.51%)

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return string.Join(' ', words.Reverse());
+    }
+}
