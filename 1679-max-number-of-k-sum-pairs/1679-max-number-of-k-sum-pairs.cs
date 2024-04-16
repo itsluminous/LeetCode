8f12ac7be81736f85b95b9d2a91be42c53121852
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-16 10:44:20 +0530

    Time: 267 ms (78.62%), Space: 65.3 MB (32.7%)

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cs
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cs
@@ -1,4 +1,27 @@
+// O(n) - using extra space
 public class Solution {
+    public int MaxOperations(int[] nums, int k) {
+        var dict = new Dictionary<int, int>();
+        var ops = 0;
+        foreach(var num in nums){
+            if(dict.ContainsKey(num)){
+                ops++;
+                dict[num]--;
+
+                if(dict[num] == 0) dict.Remove(num);
+            }
+            else{
+                var compliment = k - num;
+                if(dict.ContainsKey(compliment)) dict[compliment]++;
+                else dict[compliment] = 1;
+            }
+        }
+        return ops;
+    }
+}
+
+// Accepted, using no space - O(NlogN)
+public class SolutionSort {
     public int MaxOperations(int[] nums, int k) {
         Array.Sort(nums);
         int l = 0, r = nums.Length-1, ops = 0;
