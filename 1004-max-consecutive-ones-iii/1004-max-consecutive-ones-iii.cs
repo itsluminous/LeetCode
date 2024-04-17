6640c1365f2920f1eecb3e8d412e1e98834ba508
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-17 17:36:12 +0530

    Time: 205 ms (19.52%), Space: 61.9 MB (16.1%)

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
@@ -1,8 +1,19 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
-        int n = nums.Length, kOrig = k;
+        int n = nums.Length, l = 0, r = 0;
+        for(; r<n; r++){
+            if(nums[r] == 0) k--;
+            if(k < 0 && nums[l++] == 0) k++;
+        }
 
-        int l = 0, r = 0, z = 0, max = 0;
+        return r-l;
+    }
+}
+
+// Accepted
+public class SolutionComplex {
+    public int LongestOnes(int[] nums, int k) {
+        int n = nums.Length, l = 0, r = 0, z = 0, max = 0;
         for(; r<n; r++){
             if(nums[r] == 1) continue;
             if(nums[r] == 0 && z < k){
