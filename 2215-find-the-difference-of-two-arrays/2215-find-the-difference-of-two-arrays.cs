035dbf8b5ceaacfa68c70a0b393a5602cb2e1552
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-18 15:06:22 +0530

    Time: 167 ms (47.24%), Space: 65.2 MB (5.66%)

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cs
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cs
@@ -1,19 +1,7 @@
 public class Solution {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
-        // step 1 : convert array to set
-        HashSet<int> set1 = new HashSet<int>(), set2 = new HashSet<int>();
-        foreach(var n1 in nums1) set1.Add(n1);
-        foreach(var n2 in nums2) set2.Add(n2);
-        
-        // step 2 : remove all repeating numbers
-        foreach(var k in set1){
-            if(set2.Contains(k)){
-                set1.Remove(k);
-                set2.Remove(k);
-            }
-        }
-        
-        // step 3 : return the sets
-        return new List<IList<int>>{set1.ToList(), set2.ToList()};
+        var n1 = nums1.Distinct().Except(nums2).ToList();
+        var n2 = nums2.Distinct().Except(nums1).ToList();
+        return new List<IList<int>>{n1, n2};
     }
 }
