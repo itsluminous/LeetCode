e419506935d702b7604d40b464482163033930fb
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-22 11:47:34 +0530

    Time: 397 ms (75.23%), Space: 71.6 MB (96.18%)

--- 0739-daily-temperatures/0739-daily-temperatures.cs
+++ 0739-daily-temperatures/0739-daily-temperatures.cs
@@ -1,17 +1,48 @@
+// Without using extra space
 public class Solution {
-    public int[] DailyTemperatures(int[] temperatures) {
-        var stack = new Stack<int>();
-        var n = temperatures.Length;
-        var ans = new int[n];
+    public int[] DailyTemperatures(int[] temp) {
+        var n = temp.Length;
+        var wait = new int[n];
+        wait[n-1] = 0;
 
-        for(var i=0; i<n; i++){
-            while(stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i]){
-                var idx = stack.Pop();
-                ans[idx] = i-idx;
+        for(var i=n-2; i>=0; i--){
+            var idx = i+1;
+            while(idx < n){
+                // found a temp greater than current
+                if(temp[idx] > temp[i]){
+                    wait[i] = idx-i;
+                    break;
+                }
+                // case where bigger temp does not exist
+                if(wait[idx] == 0){
+                    idx = n;
+                    break;
+                }
+                // find next bigger temp
+                idx += wait[idx];
             }
-            stack.Push(i);
+            if(idx == n) wait[i] = 0;
         }
 
-        return ans;
+        return wait;
+    }
+}
+
+// Accepted - using stack
+public class SolutionStack {
+    public int[] DailyTemperatures(int[] temperatures) {
+        var n = temperatures.Length;
+        var stack = new Stack<int>();
+        var wait = new int[n];
+
+        for(var curr=0; curr<n; curr++){
+            while(stack.Count > 0 && temperatures[stack.Peek()] < temperatures[curr]){
+                var prev = stack.Pop();
+                wait[prev] = curr-prev;
+            }
+            stack.Push(curr);
+        }
+
+        return wait;
     }
 }
