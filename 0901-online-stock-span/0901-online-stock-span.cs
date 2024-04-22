e73f5bd36a1b17cc677386502b0e95e6b78949ec
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-22 12:52:36 +0530

    Time: 359 ms (66.96%), Space: 127.1 MB (53.13%)

---
+++ 0901-online-stock-span/0901-online-stock-span.cs
@@ -0,0 +1,23 @@
+public class StockSpanner {
+    Stack<(int, int)> stack;
+    int idx;
+
+    public StockSpanner() {
+        stack = new Stack<(int, int)>();
+        idx = 0;
+    }
+
+    public int Next(int price) {
+        var ans = 1;
+        while(stack.Count > 0 && stack.Peek().Item1 <= price)
+            ans += stack.Pop().Item2;
+        stack.Push((price, ans));
+        return ans;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.Next(price);
+ */
