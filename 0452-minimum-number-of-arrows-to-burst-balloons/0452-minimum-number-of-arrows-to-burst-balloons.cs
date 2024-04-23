921f2f945448e5eb3d1ff48bfe5a754fdd462d60
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-23 20:06:09 +0530

    Time: 569 ms (90.59%), Space: 81.2 MB (43.1%)

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cs
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cs
@@ -1,42 +1,21 @@
+// idea is to always shoot at the end of first point in a batch
 public class Solution {
     public int FindMinArrowShots(int[][] points) {
-        var n = points.Length;
-        Array.Sort(points, (x, y) => x[0].CompareTo(y[0])); // sort based on start idx
-
-        var arrows = 1;
-        var (prevStart, prevEnd) = (points[0][0], points[0][1]);
-
-        for(var i=1; i<n; i++){
-            var (currStart, currEnd) = (points[i][0], points[i][1]);
-            if(currStart <= prevEnd)
-                prevEnd = Math.Min(prevEnd, currEnd);   // no need to shoot new arrow
-            else{      // shoot new arrow                                 
-                prevEnd = currEnd;
-                arrows++;
-            }
-            prevStart = currStart;
-        }
-
-        return arrows;
-    }
-}
-
-// Accepted - Sort based on End point
-public class SolutionEndPoint {
-    public int FindMinArrowShots(int[][] points) {
-        // sort the array based on ending point
-        Array.Sort(points, (x, y) => x[1].CompareTo(y[1]));
-        // initialize variables based on first balloon
-        var shooting_point = points[0][1];
-        var arrows = 1;
-        // now check how many arrows will be needed
+        Array.Sort(points, (p1, p2) => p1[0].CompareTo(p2[0]));     // sort by start point
+        var shots = 1;
+        
+        var (prevl, prevr) = (points[0][0], points[0][1]);
         for(var i=1; i<points.Length; i++){
-            // if this balloon has starting point before shooting_point, then it would already be shot
-            if(points[i][0] <= shooting_point) continue;
-            // else we need to fire another arrow
-            shooting_point = points[i][1];
-            arrows++;
+            var (curl, curr) = (points[i][0], points[i][1]);
+
+            // if this point is not fitting in range of previous shot, then fire new shot
+            if(curl > prevr){
+                (prevl, prevr) = (curl, curr);
+                shots++;
+            }
+            else prevr = Math.Min(prevr, curr);     // can only go as right as the leftest balloon in the batch
         }
-        return arrows;
+
+        return shots;
     }
 }
