cbafc7cb439c312704a66e76f56e6aba3dd4f787
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-24 10:49:38 +0530

    Time: 75 ms (67.25%), Space: 42.3 MB (27.54%)

--- 0338-counting-bits/0338-counting-bits.cs
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -1,7 +1,8 @@
+// Accepted - populate all numbers which change bits, then figure out answer
 public class Solution {
     public int[] CountBits(int n) {
-        var nums = new List<int>();
         // populate all possible bit setters
+        var nums = new List<int>();
         for(var i=16; i>=0; i--) nums.Add((int)Math.Pow(2, i));
 
         var ans = new int[n+1];
@@ -24,3 +25,26 @@ public class Solution {
         return ans;
     }
 }
+
+// using Brian Kernighan’s Algorithm
+public class SolutionBKA {
+    public int[] CountBits(int n) {
+        var result = new int[n+1];
+        for(var i=0; i<=n; i++){
+            result[i] = CountSetBits(i);
+        }
+        return result;
+    }
+    
+    // Brian Kernighan’s Algorithm
+    // the loop runs only for as many set bits we have
+    private int CountSetBits(int n){
+        var count = 0;
+        while(n > 0){
+            n &= (n-1);
+            count++;
+        }
+        return count;
+    }
+}
+
