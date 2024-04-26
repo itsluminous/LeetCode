0e46796eec088a241ebee14d3d14dc168298eea3
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-26 12:10:03 +0530

    Time: 135 ms (100%), Space: 50.6 MB (100%)

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cs
@@ -0,0 +1,24 @@
+// we need to only know the smallest two numbers in any row.
+// one of them has to definitely be part of answer
+public class Solution {
+    public int MinFallingPathSum(int[][] grid) {
+        var n = grid.Length;
+        int small1 = 0, small2 = 0, smallpos = -1;
+        for(var i=0; i<n; i++){
+            int sm1 = int.MaxValue, sm2 = int.MaxValue, smpos = -1;
+            for(var j=0; j<n; j++){
+                var prevVal = (j == smallpos ? small2 : small1);    // pick the previous min value from different col
+
+                if(prevVal + grid[i][j] < sm1){
+                    sm2 = sm1;
+                    sm1 = prevVal + grid[i][j];
+                    smpos = j;
+                }
+                else
+                    sm2 = Math.Min(sm2, prevVal + grid[i][j]);
+            }
+            small1 = sm1; small2 = sm2; smallpos = smpos;
+        }
+        return small1;
+    }
+}
