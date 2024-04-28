58a6ee62143701ed742c89d4ba4ee391981311ca
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-28 10:27:35 +0530

    Time: 75 ms (32.53%), Space: 43.1 MB (37.55%)

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public bool LeafSimilar(TreeNode root1, TreeNode root2) {
+        var nodes1 = GetLeafNodes(root1);
+        var nodes2 = GetLeafNodes(root2);
+
+        if(nodes1.Count != nodes2.Count) return false;
+        for(var i=0; i<nodes1.Count; i++)
+            if(nodes1[i] != nodes2[i]) return false;
+        
+        return true;
+    }
+
+    public List<int> GetLeafNodes(TreeNode root) {
+        if(root == null) return new List<int>();
+        if(root.left == null && root.right == null) return new List<int>{root.val};
+
+        var lst = new List<int>();
+        lst.AddRange(GetLeafNodes(root.left));
+        lst.AddRange(GetLeafNodes(root.right));
+
+        return lst;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
