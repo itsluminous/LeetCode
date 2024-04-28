5ad4fff5e90c330676e966aa35ed1c0e6b5e572f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-28 17:24:57 +0530

    Time: 1763 ms (100%), Space: 27.3 MB (100%)

---
+++ 3133-minimum-array-end/3133-minimum-array-end.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public long MinEnd(int n, int x) {
+        long ans = x;    // start with x, because that is the smallest number we can have
+        while(--n > 0){
+            ans++;      // increment ans
+            ans |= x;   // OR with x to keep the required bits always 1
+        }
+        return ans;
+    }
+}
