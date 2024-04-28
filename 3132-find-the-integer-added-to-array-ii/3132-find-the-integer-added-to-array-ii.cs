46f18a36c29063f5074d4ba9871bc1b3151e1283
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-28 17:16:43 +0530

    Time: 62 ms (100%), Space: 43.3 MB (100%)

---
+++ 3132-find-the-integer-added-to-array-ii/3132-find-the-integer-added-to-array-ii.cs
@@ -0,0 +1,25 @@
+// after sorting, the diff has to be one of these [nums2[0] - nums1[0], nums2[0] - nums1[1], nums2[0] - nums1[2]]
+// for each of these diff, just check which fits the other numbers
+public class Solution {
+    public int MinimumAddedInteger(int[] nums1, int[] nums2) {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        var ans = int.MaxValue;
+
+        // one by one skip first 3 indexes in nums1
+        for(var i=0; i<3; i++){
+            var currDiff = nums2[0] - nums1[i];
+            var skip = i;   // we can skip only 2 elements. so if more than 2 are skipped, it is not an ans
+            for(var j=i+1; skip<3 && j < nums2.Length+skip; j++){
+                
+                if(currDiff != nums2[j - skip] - nums1[j])
+                    skip++;
+            }
+
+            if(skip < 3)
+                ans = Math.Min(ans, currDiff);
+        }
+
+        return ans;
+    }
+}
