a49d1176a9da3616ee26b2bf96c246ead77c55ff
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-29 08:29:52 +0530

    Time: 351 ms (57.14%), Space: 71.4 MB (28.57%)

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cs
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public int MinOperations(int[] nums, int k) {
+        // find out which bits in k are not matching
+        foreach(var num in nums) k ^= num;
+        // return the bits which are not matching
+        return BitOperations.PopCount((uint)k);
+    }
+}
+
+// Accepted - same approach, but verbose
+public class SolutionVerbose {
+    public int MinOperations(int[] nums, int k) {
         // find out xor value of nums array
         var allxor = nums[0];
         for(var i=1; i<nums.Length; i++)
