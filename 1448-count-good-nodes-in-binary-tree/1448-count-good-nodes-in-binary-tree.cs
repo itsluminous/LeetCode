9d0c559ed7a24c2920f1b469b72704747ae5aa1a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-04-30 09:58:17 +0530

    Time: 204 ms (17.84%), Space: 65 MB (15.22%)

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cs
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cs
@@ -1,10 +1,13 @@
-// DFS
 public class Solution {
-    public int GoodNodes(TreeNode root, int max = -10001) {
+    public int GoodNodes(TreeNode root, int biggest = int.MinValue) {
         if(root == null) return 0;
-        var curr =  root.val < max ? 0 : 1;
-        max = Math.Max(max, root.val);
-        return curr + GoodNodes(root.left, max) + GoodNodes(root.right, max);
+        
+        var count = 0;
+        if(root.val >= biggest) count++;
+        count += GoodNodes(root.left, Math.Max(biggest, root.val));
+        count += GoodNodes(root.right, Math.Max(biggest, root.val));
+
+        return count;
     }
 }
 
