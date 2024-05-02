4b40728c3d24aa05a9739c7690ec623d1b3a810a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-02 15:49:05 +0530

    Time: 46 ms (89.81%), Space: 39.1 MB (78.7%)

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cs
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cs
@@ -1,21 +1,22 @@
 // The robot stays in the circle only if at the end it doesn't stay pointing north, or it returns to home.
 public class Solution {
     public bool IsRobotBounded(string instructions) {
-        int x = 0, y = 0, dir = 0;
-        var directions = new []{new []{0,1}, new []{-1,0}, new []{0,-1}, new []{1,0}};  // N, W, S, E
-        
-        foreach(var inst in instructions){
-            if(inst == 'L')
-                dir = (dir+1)%4;
-            else if(inst == 'R')
-                dir = (dir+3)%4;
-            else{    // 'G'
-                x += directions[dir][0];
-                y += directions[dir][1];
+        var (x,y) = (0,0);
+        (int x, int y)[] dirs = new []{(0,1), (1,0), (0,-1), (-1, 0)};
+        var dirIdx = 0;
+
+        foreach(var dir in instructions){
+            if(dir == 'G'){
+                x += dirs[dirIdx].x;
+                y += dirs[dirIdx].y;
             }
+            else if(dir == 'L')
+                dirIdx = (dirIdx+3) % 4;
+            else if(dir == 'R')
+                dirIdx = (dirIdx+1) % 4;
         }
-        
-        if((x == 0 && y == 0) || dir != 0) return true;
-        return false;
+
+        if((x|y) == 0) return true;  // robot returned to home
+        return dirIdx > 0;           // any direction except north facing will lead to cycle eventually
     }
 }
