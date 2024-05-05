ed9c5b0e4db0bd49850598ce0b08f18c4f7d8f61
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-05 10:50:00 +0530

    Time: 76 ms (17.65%), Space: 53.4 MB (34.39%)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+    let temp = node.next;
+    node.val = temp.val;
+    node.next = temp.next;
+};
