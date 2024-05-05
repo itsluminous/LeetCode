6f9a0a47b9273657f2574dde07369594a7acec4e
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-05 10:48:27 +0530

    Time: 4 ms (59.07%), Space: 6.4 MB (8.83%)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    struct ListNode* temp = node->next;
+    node->val = temp->val;
+    node->next = temp->next;
+}
