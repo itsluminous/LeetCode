073c94cd6b4ed489426353eddd7297a18a37453b
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-05 10:54:18 +0530

    Time: 7 ms (90.54%), Space: 15.8 MB (6.76%)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.swift
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func deleteNode(_ node: ListNode?) {
+        if let node = node, let temp = node.next {
+            node.val = temp.val
+            node.next = temp.next
+        }
+    }
+}
