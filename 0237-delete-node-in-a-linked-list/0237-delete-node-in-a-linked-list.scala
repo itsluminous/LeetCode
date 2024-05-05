c821ecbf0009baeb53e7c7061da06df182a99917
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-05 10:58:31 +0530

    Time: 663 ms (63.64%), Space: 60.2 MB (72.73%)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.scala
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+
+object Solution {
+    def deleteNode(node: ListNode): Unit = {
+        node.x = node.next.x
+        node.next = node.next.next
+    }
+}
