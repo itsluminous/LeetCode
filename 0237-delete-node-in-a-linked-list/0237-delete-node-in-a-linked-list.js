b59b2bce8f69062b6cced5aae182f910f01756b9
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-05 10:49:25 +0530

    Time: 65 ms (54.37%), Space: 52.3 MB (34.9%)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    temp = node.next;
+    node.val = temp.val;
+    node.next = temp.next;
+};
