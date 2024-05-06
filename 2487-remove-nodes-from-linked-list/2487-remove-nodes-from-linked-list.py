8047b371d0c84121b11cf3cebeb0e39afb49e8c2
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-06 09:20:59 +0530

    Time: 414 ms (63.34%), Space: 62.3 MB (15.68%)

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return head
+        head.next = self.removeNodes(head.next)
+        if head.next and head.val < head.next.val: return head.next
+        return head
