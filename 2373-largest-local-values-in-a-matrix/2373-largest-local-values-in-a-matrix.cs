f4e30d0dbfe031b1f532853b9833e18c49dc750f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-12 07:20:18 +0530

    Time: 124 ms (88%), Space: 51.4 MB (38%)

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cs
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cs
@@ -1,24 +1,14 @@
 public class Solution {
     public int[][] LargestLocal(int[][] grid) {
         var n = grid.Length;
-        var result = new int[n-2][];
-        for(var i=0; i<n-2; i++) result[i] = new int[n-2];
-        
-        for(var i=1; i<=n-2; i++){
-            for(var j=1; j<=n-2; j++){
-                result[i-1][j-1] = LargestLocal(grid, i, j);
-            }
-        }
-        
-        return result;
-    }
-    
-    public int LargestLocal(int[][] grid, int i, int j) {
-        var nums = new int[9];
-        nums[0] = grid[i-1][j-1]; nums[1] = grid[i][j-1]; nums[2] = grid[i+1][j-1];
-        nums[3] = grid[i-1][j]; nums[4] = grid[i][j]; nums[5] = grid[i+1][j];
-        nums[6] = grid[i-1][j+1]; nums[7] = grid[i][j+1]; nums[8] = grid[i+1][j+1];
-        
-        return nums.Max();
+        var ans = new int[n-2][];
+        for(var i=0; i<n-2; i++) ans[i] = new int[n-2];
+
+        for(var i=1; i<n-1; i++)
+            for(var j=1; j<n-1; j++)
+                for(var x=-1; x <= 1; x++)
+                    for(var y=-1; y <= 1; y++)
+                        ans[i-1][j-1] = Math.Max(ans[i-1][j-1], grid[i+x][j+y]);
+        return ans;
     }
 }
