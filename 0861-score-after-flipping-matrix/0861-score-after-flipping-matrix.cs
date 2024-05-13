75b76e82bc603767e14ac4365f4c94793c0bc45b
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-13 06:28:24 +0530

    Time: 49 ms (100%), Space: 41.7 MB (9.09%)

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int MatrixScore(int[][] grid) {
+        int m = grid.Length, n = grid[0].Length, halfRows = (m+1)/2;
+
+        // row operation - make sure that we have 1st bit set (Most Significant Bit)
+        // because `10000` is greater than `01111` in binary
+        for(var i=0; i<m; i++){
+            if(grid[i][0] == 1) continue;
+            for(var j=0; j<n; j++) grid[i][j] ^= 1;  // flip the bit
+        }
+
+        // col operation - make sure that count of 1 in col is >= count of 0
+        // exclude col-1 because we just now ensured that they are all 1
+        for(var j=1; j<n; j++){
+            var setBit = 0;
+            for(var i=0; i<m; i++) setBit += grid[i][j];
+
+            if(setBit >= halfRows) continue;
+            for(var i=0; i<m; i++) grid[i][j] ^= 1;  // flip the bit
+        }
+
+        // calculate score of matrix
+        var score = 0;
+        for(var i=0; i<m; i++){
+            var binStr = string.Join("", grid[i]);
+            score += Convert.ToInt32(binStr, 2);   // binary to decimal conversion
+        }
+
+        return score;
+    }
+}
