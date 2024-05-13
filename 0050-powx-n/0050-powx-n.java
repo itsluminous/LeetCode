9d885ab0d3fb098d0779ad6fa394c13732caff3a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-13 08:44:38 +0530

    Time: 0 ms (100%), Space: 41.6 MB (84.14%)

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        // convert negative power to positive
+        if(n < 0){
+            n = -n;
+            x = 1/x;
+        }
+
+        double ans = 1;
+        while(n != 0){
+            if ((n&1) == 1) ans *= x;   // when the LSB is 1, then x will be part of ans
+
+            // x^1 = x, x^2 = x * x, x^3 = x * x * x, so on....
+            // at every step, x will multiply with itself
+            x *= x;     
+            n >>>= 1;    // unsigned right shift by 1 (equivalent to divide by 2)
+        }
+
+        return ans;
     }
 }
