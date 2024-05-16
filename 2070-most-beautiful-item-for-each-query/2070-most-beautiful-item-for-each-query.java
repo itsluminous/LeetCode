5455950314ac9ff0964508c0c42c057cce5b9202
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-16 17:48:10 +0530

    Time: 46 ms (52.85%), Space: 81 MB (60.98%)

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        // sort by price
+        Arrays.sort(items, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+        var n = items.length;
+
+        // find max beauty for each index
+        var beauty = new int[n];
+        beauty[0] = items[0][1];
+        for(var i=1; i<n; i++)
+            beauty[i] = Math.max(beauty[i-1], items[i][1]);
+
+        // evaluate the query
+        // find the appropriate index in items arr using binary search
+        // then find the corresponding max beauty value in beauty array
+        for(var i=0; i<queries.length; i++){
+            // if the query is lower than lowest price, then ans will be 0
+            if(items[0][0] > queries[i]){
+                queries[i] = 0;
+                continue;
+            }
+
+            // binary search
+            int low=0, high=n-1;
+            while(low <= high){
+                var mid = low + (high-low)/2;
+                if(items[mid][0] > queries[i]) high = mid-1;
+                else low = mid+1;
+            }
+            queries[i] = beauty[low-1];
+        }
+
+        return queries;
+    }
+}
