1103288d5d2d01883b25c8ca16e53083639a42ff
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-16 14:19:46 +0530

    Time: 53 ms (32.23%), Space: 65.6 MB (99.17%)

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length, n = grid[0].length;
+        var nums = new ArrayList<Integer>();
+
+        // convert 2d array to 1d array
+        for(var i=0; i<m; i++)
+            for(var j=0; j<n; j++)
+                nums.add(grid[i][j]);
+
+        // find median using quick select. all numbers will be converted to this value
+        var mid = (m*n)/2;
+        var median = getMedian(nums, mid+1);
+
+        // try to convert every number to median
+        var ops = 0;
+        for(var num : nums){
+            var diff = Math.abs(num - median);
+            if(diff % x != 0) return -1;    // can never be converted
+            ops += (diff / x);
+        }
+
+        return ops;
+    }
+
+    // get median using quick select
+    private int getMedian(List<Integer> nums, int idx){
+        // pick random pivot to avoid worse case complexity in quick sort
+        var rand = new Random();
+        var pivot = nums.get(rand.nextInt(nums.size()));
+
+        List<Integer> left = new ArrayList<>(), right = new ArrayList<>(), mid = new ArrayList<>();
+        for(var num : nums){
+            if(num < pivot) left.add(num);
+            else if(num > pivot) right.add(num);
+            else mid.add(num);
+        }
+
+        int l = left.size(), m = mid.size(), r=right.size();
+        if(l >= idx) return getMedian(left, idx);
+        if(l + m < idx) return getMedian(right, idx-(l+m));
+        return mid.get(0);  // all items in mid will have same value
+    }
+
+    // to find median by sorting array - slower
+    private int getMedianSort(List<Integer> nums, int idx){
+        nums.sort(null);
+        return nums.get(idx-1);
+    }
+}
