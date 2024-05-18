b60880ce87b457b9f1fdf904700a7bdfc6b91c98
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-18 15:51:32 +0530

    Time: 3 ms (45.84%), Space: 56.6 MB (80.44%)

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalFuel = 0, currFuel = 0;    // total fuel will be sum of all gas minus sum of all cost
+        var startIdx = 0;
+
+        for(var idx=0; idx<gas.length; idx++){
+            totalFuel += (gas[idx] - cost[idx]);
+            currFuel += (gas[idx] - cost[idx]);
+
+            // if fuel is negative, then we can guarantee that all indexes before this cannot be start point
+            // because a valid start point will always keep fuel in positive
+            if(currFuel < 0){
+                currFuel = 0;
+                startIdx = idx+1;
+            }
+        }
+        
+        if(totalFuel < 0) return -1;    // the consumption is more than gas earned
+        return startIdx;
+    }
+}
