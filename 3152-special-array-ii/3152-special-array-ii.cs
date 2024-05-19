f0c7638d8993ff92f9581f62ea7e6b585f0c5cfd
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-19 10:25:23 +0530

    Time: 552 ms (100%), Space: 102.5 MB (100%)

--- 3152-special-array-ii/3152-special-array-ii.cs
+++ 3152-special-array-ii/3152-special-array-ii.cs
@@ -3,6 +3,29 @@ public class Solution {
         var n = queries.Length;
         var ans = new bool[n];
 
+        // calculate how many bad pairs we have seen till now
+        var bad = new int[nums.Length];
+        for(int i=1, j=0; i<nums.Length; i++){
+            if((nums[i] & 1) == (nums[i-1] & 1))  bad[i] = ++j;
+            else bad[i] = j;
+        }
+
+        // for each query, if the count of bad pairs at start index is same as that at end index, then its special
+        for(var i=0; i<n; i++){
+            int start = queries[i][0], end = queries[i][1];
+            if(bad[start] == bad[end]) ans[i] = true;
+        }
+
+        return ans;
+    }
+}
+
+// accepted - using binary search
+public class SolutionBS {
+    public bool[] IsArraySpecial(int[] nums, int[][] queries) {
+        var n = queries.Length;
+        var ans = new bool[n];
+
         // find starting index of all pairs who are bad
         var badIndex = new List<int>();
         for(var i=0; i<nums.Length-1; i++){
