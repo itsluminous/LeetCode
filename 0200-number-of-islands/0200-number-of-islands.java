e45fdf6839b18f15e222fc1319a44d6760c3e7e5
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-22 16:47:28 +0530

    Time: 2 ms (98.16%), Space: 49.7 MB (25.71%)

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,23 +1,26 @@
-public class Solution {
+class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length, n = grid[0].length, count = 0;
         for(var i=0; i<m; i++)
             for(var j=0; j<n; j++)
                 if(grid[i][j] == '1'){
                     count++;
-                    traverseIsland(grid, i, j);
+                    dfs(grid, i, j);
                 }
         
         return count;
     }
 
-    // DFS
-    private void traverseIsland(char[][] grid, int x, int y) {
-        if(x == -1 || y == -1 || x == grid.length || y == grid[0].length || grid[x][y] == '0') return;
-        grid[x][y] = '0';
-        traverseIsland(grid, x-1, y);
-        traverseIsland(grid, x+1, y);
-        traverseIsland(grid, x, y-1);
-        traverseIsland(grid, x, y+1);
+    private void dfs(char[][] grid, int x, int y){
+        int m = grid.length, n = grid[0].length;
+        if(x == -1 || y == -1 || x == m || y == n) return;  // out of bound
+        if(grid[x][y] == '0') return;   // already visited or not land
+        grid[x][y] = '0';   // mark visited
+
+        // now travel in all four directions
+        dfs(grid, x-1, y);
+        dfs(grid, x+1, y);
+        dfs(grid, x, y-1);
+        dfs(grid, x, y+1);
     }
 }
