8c52d982069bf2baa9adbaf276e2fb2bffbaf5a6
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-24 07:18:32 +0530

    Time: 1 ms (81.78%), Space: 43.3 MB (26.7%)

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxScoreWords(String[] words, char[] letters, int[] score) {
+        var freq = new int[26]; // frequency of each char in letters
+        for(var ch : letters) freq[ch-'a']++;
+
+        return wordScore(words, freq, score, 0, 0);
+    }
+
+    private int wordScore(String[] words, int[] freq, int[] score, int wIdx, int scoreTillNow){
+        if(wIdx == words.length) return 0;
+        var origFreq = freq.clone();
+
+        var notMakeCurrWord = wordScore(words, freq, score, wIdx+1, scoreTillNow);
+        freq = origFreq;
+        
+        var makeCurrWord = makeWord(words, freq, score, wIdx);
+        if(makeCurrWord != -1)
+            makeCurrWord += wordScore(words, freq, score, wIdx+1, scoreTillNow+makeCurrWord);
+        freq = origFreq;
+        
+        var maxScore = Math.max(notMakeCurrWord, makeCurrWord);
+        return maxScore == -1 ? 0 : maxScore;
+    }
+
+    // if a word can be formed, then return its score and modify freq array
+    private int makeWord(String[] words, int[] freq, int[] score, int wIdx) {
+        var totalScore = 0;
+        var origFreq = freq.clone();
+        for(var ch : words[wIdx].toCharArray()){
+            if(freq[ch-'a'] == 0){
+                freq = origFreq;
+                return -1;
+            }
+            freq[ch-'a']--;
+            totalScore += score[ch-'a'];
+        }
+
+        return totalScore;
+    }
+}
