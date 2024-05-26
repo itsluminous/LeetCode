d1730a117c3dfe0a5044ebd6bddb046d72fae883
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-26 17:08:01 +0530

    Time: 101 ms (100%), Space: 72.2 MB (100%)

--- 3165-maximum-sum-of-subsequence-with-non-adjacent-elements/3165-maximum-sum-of-subsequence-with-non-adjacent-elements.java
+++ 3165-maximum-sum-of-subsequence-with-non-adjacent-elements/3165-maximum-sum-of-subsequence-with-non-adjacent-elements.java
@@ -1,48 +1,80 @@
-public class Solution {
+// approach : https://leetcode.com/problems/maximum-sum-of-subsequence-with-non-adjacent-elements/solutions/5208917/segment-tree/
+
+// each node in seg tree will have to maintain 4 values, so that we can apply house robber algo
+// [0][0] : leftmost & rightmost in curr range is not selected
+// [0][1] : leftmost in curr range is not selected, rightmost is selected
+// [0][0] : leftmost in curr range is selected, rightmost is not selected
+// [0][0] : leftmost & rightmost in curr range are both selected
+
+class Solution {
     public int maximumSumSubsequence(int[] nums, int[][] queries) {
         var MOD = 1_000_000_007;
-        var n = nums.length;
-        long totalSum = 0;
-        var dp_incl = new long[n+1];     // to track max sum till now when including
-        var dp_excl = new long[n+1];     // to track max sum till now when including
+        var segTree = new Node(nums, 0, nums.length - 1);
+        long res = 0;
 
-        // fill dp
-        long incll = 0, excll = 0;
-        for(var i=0; i<n; i++){
-            var num = nums[i];
-            long newExcl = Math.max(incll, excll);
-            incll = (excll + num) % MOD;
-            excll = newExcl;
+        for(var query : queries){
+            segTree.update(query[0], query[1]);
+            res += segTree.getMaxSum();
+            res %= MOD;
+        }
+        return (int)res;
+    }
+}
 
-            dp_incl[i] = incll;
-            dp_excl[i] = excll;
+class Node {
+    long inf = (long) 1e18;
+    Node l = null, r = null;
+    int lo, hi;
+    long[][] selected = new long[2][2];
+
+    public Node(int[] nums, int lo, int hi){
+        this.lo = lo;
+        this.hi = hi;
+
+        if(lo < hi){
+            var mid = (lo + hi) / 2;
+            l = new Node(nums, lo, mid);
+            r = new Node(nums, mid+1, hi);
+            combine();
+        }
+        else{
+            selected[0][0] = 0;
+            selected[0][1] = -inf;
+            selected[1][0] = -inf;
+            selected[1][1] = nums[lo];  // same as nums[hi]
+        }
+    }
+
+    // house robber algo
+    private void combine(){
+        selected[0][0] = Math.max(l.selected[0][0] + r.selected[0][0], 
+                         Math.max(l.selected[0][1] + r.selected[0][0], l.selected[0][0] + r.selected[1][0]));
+        selected[0][1] = Math.max(l.selected[0][0] + r.selected[0][1], 
+                         Math.max(l.selected[0][1] + r.selected[0][1], l.selected[0][0] + r.selected[1][1]));
+        selected[1][0] = Math.max(l.selected[1][0] + r.selected[0][0], 
+                         Math.max(l.selected[1][1] + r.selected[0][0], l.selected[1][0] + r.selected[1][0]));
+        selected[1][1] = Math.max(l.selected[1][0] + r.selected[0][1], 
+                         Math.max(l.selected[1][1] + r.selected[0][1], l.selected[1][0] + r.selected[1][1]));
+    }
+
+    public void update(int idx, long val){
+        if(idx < lo || idx > hi) return;
+
+        // reached root node
+        if(lo == hi){
+            selected[0][0] = 0;
+            selected[1][1] = val;
+            return;
         }
 
-        for (var query : queries) {
-            int pos = query[0];
-            int val = query[1];
-            nums[pos] = val;
+        l.update(idx, val);
+        r.update(idx, val);
+        combine();
+    }
 
-            long incl = 0, excl = 0;
-            if(pos != 0){
-                incl = dp_incl[pos-1];
-                excl = dp_excl[pos-1];
-            }
-
-            for(var i=pos; i<n; i++){
-                var num = nums[i];
-                long newExcl = Math.max(incl, excl);
-                incl = (excl + num) % MOD;
-                excl = newExcl;
-
-                dp_incl[i] = incl;
-                dp_excl[i] = excl;
-            }
-
-            long maxSum = Math.max(incl, excl);
-            totalSum = (totalSum + maxSum) % MOD;
-        }
-
-        return (int) totalSum;
+    public long getMaxSum(){
+        return Math.max(selected[0][0],
+               Math.max(selected[0][1], 
+               Math.max(selected[1][0], selected[1][1])));
     }
 }
