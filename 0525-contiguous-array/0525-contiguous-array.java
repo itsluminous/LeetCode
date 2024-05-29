1b314023a54df3f1429b11f17f05f7ae69b49667
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-29 20:19:52 +0530

    Time: 23 ms (84.61%), Space: 54.8 MB (86.63%)

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,34 @@
+/*
+O(n) approach using Hashmap/Dictionary
+
+1. Keep a variable called runningSum
+2. Keep a Hashmap called seen which will store index of first time a runningSum was seen
+3. set runningSum[0] = -1. meaning, that without picking any element, running sum is 0.
+4. Start traversing the array
+5. Everytime you encounter a 0, subtract 1, and when you encounter 1, then add 1 to the runningSum
+6. If the current runningSum exists in dictionary, then it means that all zeroes and ones between current index and the first time we saw this runningSum are equal. Update max length if (currIdx - seen.get(runningSum) > max length)
+7. If the current runningSum does not exists in dictionary, then add it with current index as value
+8. At the end of loop, you will have answer
+*/
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n = nums.length;
+        int runningSum = 0, maxLen = 0;
+
+        var seen = new HashMap<Integer, Integer>();
+        seen.put(0, -1);
+
+        for (var i = 0; i < n; i++) {
+            runningSum += nums[i] == 0 ? -1 : 1;
+
+            if (seen.containsKey(runningSum)) {
+                var lastSeen = seen.get(runningSum);
+                maxLen = Math.max(maxLen, i - lastSeen);
+            } else
+                seen.put(runningSum, i);
+        }
+
+        return maxLen;
+    }
+}
