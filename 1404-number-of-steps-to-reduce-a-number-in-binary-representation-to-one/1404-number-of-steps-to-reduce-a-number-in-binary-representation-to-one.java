fec512ca321eac5cbf4c45d646737d15913ea46e
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-29 17:39:49 +0530

    Time: 0 ms (100%), Space: 41.1 MB (70.44%)

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numSteps(String s) {
+        int steps = 0, carry = 0;
+
+        // carry will never become zero once it changes to 1, because of how the rules are in question
+        for(var i = s.length()-1; i > 0; i--){
+            if(s.charAt(i) - '0' + carry == 1){
+                steps += 2; // one step to add 1, and one step to divide by 2
+                carry = 1;
+            } else {
+                steps += 1;
+            }
+        }
+
+        return steps + carry;
+    }
+}
+
+// This fails for really long string, because conversion to long fails
+// Eg. 1111110011101010110011100100101110010100101110111010111110110010
+class SolutionFailed {
+    public int numSteps(String s) {
+        var num = Long.parseLong(s, 2);
+
+        var steps = 0;
+        while(num != 1){
+            if((num & 1) == 1) num++;
+            else num >>= 1;
+            steps++;
+        }
+
+        return steps;
+    }
+}
