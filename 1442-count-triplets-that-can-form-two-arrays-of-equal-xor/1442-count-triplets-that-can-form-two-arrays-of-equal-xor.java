ed9180399e89c799f536180c5035d6adfad1f92a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-30 21:00:42 +0530

    Time: 2 ms (77.44%), Space: 41 MB (56.92%)

---
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -0,0 +1,33 @@
+// a = xor of i to j-1
+// b = xor of j to k
+// if a == b, then a ^ b == b ^ b
+// i.e.  a ^ b = 0
+// thus,  arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1] ^ arr[j] ^ arr[j + 1] ^ ... ^ arr[k] = 0
+// now this is a prefix problem. We need to find out how many times we have seen a particular xor value
+class Solution {
+    public int countTriplets(int[] arr) {
+        var n = arr.length;
+        Map<Integer, Integer> countOfCumulativeXor = new HashMap<>(), 
+                              subArrWithCumulativeXor = new HashMap<>();
+
+        // one possibility to get 0 xor is not have any num inculded
+        countOfCumulativeXor.put(0, 1);
+        subArrWithCumulativeXor.put(0, -1);
+
+        int prefixXor = 0, count = 0;
+        for(var i=0; i<n; i++){
+            prefixXor ^= arr[i];
+            var countOfCumXor = countOfCumulativeXor.getOrDefault(prefixXor, 0);
+            var subArrWithCumXor = subArrWithCumulativeXor.getOrDefault(prefixXor, 0);
+
+            // update total count
+            count += countOfCumXor * i - countOfCumXor - subArrWithCumXor;
+
+            // update count and occurrence for current xor
+            countOfCumulativeXor.put(prefixXor, countOfCumXor + 1);
+            subArrWithCumulativeXor.put(prefixXor, subArrWithCumXor + i);
+        }
+
+        return count;
+    }
+}
