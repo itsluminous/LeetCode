7a8968f3a468b184a31206bec296d28b6ba54e82
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-05-31 10:16:40 +0530

    Time: 114 ms (67.26%), Space: 48.2 MB (59.29%)

---
+++ 0260-single-number-iii/0260-single-number-iii.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] SingleNumber(int[] nums) {
+        // get the xor value of all numbers
+        var allXor = 0;
+        foreach(var num in nums)
+            allXor ^= num;
+        
+        // find any position which has bit set
+        // we are choosing the rightmost set bit, but idea is to find any bit which is set
+        var setBit = allXor & -allXor;
+
+        // we are sure that out of two numbers, one will have setBit = 0 and other will have setBit = 1
+        // so we group all numbers in two categories : those with setBit = 1 and those with setBit = 0
+        int numWithSet = 0, numWithoutSet = 0;
+        foreach(var num in nums){
+            if((num & setBit) == 0) numWithSet ^= num;
+            else numWithoutSet ^= num;
+        }
+
+        return [numWithSet, numWithoutSet];
+    }
+}
