953dfe1a379aa6705f4d46ece7a025982fb2d55c
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-06 19:21:15 +0530

    Time: 21 ms (72.95%), Space: 26.5 MB (99.94%)

---
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MySqrt(int x) {
+        if(x <= 1) return x;
+
+        int left = 1, right = x/2;
+        while(true){
+            var mid = left + (right - left)/2;
+            if(mid > x / mid)
+                right = mid - 1;
+            else if(mid+1 > x / (mid+1) )
+                    return mid;
+            else
+                left = mid + 1;
+        }
+    }
+}
