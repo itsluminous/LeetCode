2d724f27969a19d5bb75b4f0aed1739f2a306fc0
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-10 07:41:22 +0530

    Time: 64 ms (43.96%), Space: 39.9 MB (95.67%)

---
+++ 1051-height-checker/1051-height-checker.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        var n = heights.Length;
+
+        // count how many students have given height
+        var freq = new int[101];
+        foreach(var h in heights) freq[h]++;
+
+        // now find misplaced ones
+        int pos = 0, misplaced = 0, processed = 0;
+        for(var h = 1; h <= 100 && processed < n; h++){
+            var maxPos = pos + freq[h];
+            processed += freq[h];
+            for(var i = pos; i < maxPos && processed <= n; i++)
+                if(heights[i] != h) misplaced++;
+            pos = maxPos;
+        }
+
+        return misplaced;
+    }
+}
