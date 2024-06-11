4dbe42a2e5d1cfb4dc4504be7514272fe7bb8bb3
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-11 08:39:07 +0530

    Time: 98 ms (75%), Space: 45.5 MB (85.23%)

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int[] RelativeSortArray(int[] arr1, int[] arr2) {
+        var freq = new int[1001];
+
+        // find freq of each number in arr1
+        foreach(var num in arr1)
+            freq[num]++;
+        
+        // put all matching nums from arr2 in final ans
+        var pos = 0;
+        foreach(var num in arr2){
+            var count = freq[num];
+            freq[num] = 0;
+            for(var i=0; i<count; i++)
+                arr1[pos++] = num;
+        }
+
+        // sort remaining nums and put in ans
+        for(var num=0; num<1001; num++){
+            var count = freq[num];
+            for(var i=0; i<count; i++)
+                arr1[pos++] = num;
+        }
+
+        return arr1;
+    }
+}
