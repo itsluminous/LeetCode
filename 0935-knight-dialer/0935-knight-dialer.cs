76d160dd723b7a090b5a265e1cf2113cdd93a496
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-11 20:07:42 +0530

    Time: 122 ms (49.12%), Space: 33 MB (61.4%)

---
+++ 0935-knight-dialer/0935-knight-dialer.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    int[,] dp;              // for given [moves][num], how many possibilities
+    int[][] keypadMap;       // neighbours for each number
+    int MOD = 1_000_000_007;
+
+    public int KnightDialer(int n) {
+        Initialize(n);
+        var count = 0;
+        for(var digit=0; digit<10; digit++)
+            count = (count + Move(digit, n-1)) % MOD;
+        return count;
+    }
+
+    private int Move(int digit, int moves){
+        if(moves == 0) return 1;
+        if(dp[moves, digit] > 0) return dp[moves, digit];
+
+        var count = 0;
+        var neighbours = keypadMap[digit];
+        foreach(var next in neighbours)
+            count = (count + Move(next, moves-1)) % MOD;
+        
+        dp[moves, digit] = count;
+        return count;
+    }
+
+    private void Initialize(int n){
+        dp = new int[n, 10];
+        keypadMap = new int[][] {
+            [4, 6],     [6, 8],     [7, 9],
+            [4, 8],     [0, 3, 9],  [],
+            [0, 1, 7],  [2, 6],     [1, 3],
+            [2, 4]
+        };
+    }
+}
