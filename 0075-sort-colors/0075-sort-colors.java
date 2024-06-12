1990a0fac6fbe0d07c733e6d0d7946a40f8afa72
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-12 09:46:40 +0530

    Time: 0 ms (100%), Space: 41.7 MB (42.89%)

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,29 @@
+// using 3 pointers, one pass
+// keep all 0 before low ptr, all 2 after high ptr, all 1 from low to high
+// mid ptr will be used for traversing from 0 to n-1
 class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0)
+                swap(nums, low++, mid++);
+            else if(nums[mid] == 1)
+                mid++;
+            else 
+                swap(nums, high--, mid);
+        }
+    }
+
+    private void swap(int[] nums, int idx1, int idx2){
+        var temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+}
+
+// Accepted - using 2 pass
+class Solution2pass {
     public void sortColors(int[] nums) {
         var freq = new int[3];
         for(var num : nums) freq[num]++;
