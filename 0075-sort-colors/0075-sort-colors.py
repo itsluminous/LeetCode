d42e6846efb5b1748af83cf73f73e31bfae8687c
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-12 09:50:11 +0530

    Time: 30 ms (92.93%), Space: 16.5 MB (79.02%)

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,4 +1,23 @@
+# using 3 pointers, one pass
+# keep all 0 before low ptr, all 2 after high ptr, all 1 from low to high
+# mid ptr will be used for traversing from 0 to n-1
 class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+
+# Accepted - using 2 pass
+class Solution2pass:
     def sortColors(self, nums: List[int]) -> None:
         freq = [0] * 3
         for num in nums: freq[num] += 1
