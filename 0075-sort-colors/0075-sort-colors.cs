273aed1a6eb70383b32ec8e00866ce186f15b3af
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-12 09:46:33 +0530

    Time: 92 ms (82%), Space: 45.3 MB (77.57%)

--- 0075-sort-colors/0075-sort-colors.cs
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -1,4 +1,28 @@
+// using 3 pointers, one pass
+// keep all 0 before low ptr, all 2 after high ptr, all 1 from low to high
+// mid ptr will be used for traversing from 0 to n-1
 public class Solution {
+    public void SortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.Length - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                (nums[low], nums[mid]) = (nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+                mid++;
+            else {
+                (nums[high], nums[mid]) = (nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+}
+
+// Accepted - using 2 pass
+public class Solution2Pass {
     public void SortColors(int[] nums) {
         var freq = new int[3];
         foreach(var num in nums) freq[num]++;
