cebaeefbdf79cc7ccde0fa7b49880d3a1aa37114
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-12 13:21:58 +0530

    Time: 53 ms (51.4%), Space: 38.7 MB (67.29%)

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    int[] perm;
+    bool[] set;
+
+    public string GetPermutation(int n, int k) {
+        Initialize();
+        return Solve(n, k);
+    }
+
+    private String Solve(int n, int k){
+        if(n == 1){
+            for(var i=1; i<10; i++)
+                if(!set[i]) return i.ToString();
+        }
+
+        // find out how many digits need to be skipped to get relevant count
+        var count = perm[n-1];
+        var curr = 1;
+        while(curr * count < k) curr++;
+        curr--;
+        k -= curr * count;
+
+        // find unused number in set, which is at position "count"
+        var sb = new StringBuilder();
+        var idx = 1;
+        for(; idx<10; idx++){
+            if(set[idx]) continue;
+            if(curr-- == 0) break;
+        }
+
+        // mark the "idx" number as used, and look for number in next position
+        set[idx] = true;
+        sb.Append(idx);
+        sb.Append(Solve(n-1, k));
+        return sb.ToString();
+    }
+
+    private void Initialize(){
+        perm = new int[10];
+        perm[1] = 1;
+        for(var i=2; i<10; i++) perm[i] = i * perm[i-1];
+
+        set = new bool[10];
+        set[0] = true;  // we cannot pick 0, so mark it as visited
+    }
+}
