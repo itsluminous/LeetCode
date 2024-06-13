032fbdb88c5b6e888fa338c4b3b415ad307110e5
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-13 10:11:10 +0530

    Time: 13 ms (5.12%), Space: 45.2 MB (5.62%)

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isNumber(String s) {
+        /*
+        ^ = start of string, $ = end of string, | = OR condition, ? = 0 to 1 times, + = 1 or more
+        (-|\\+)?  = minus or plus 0 to 1 times
+        (\\.\\d+)  = decimal followed by at least one digit
+        (\\d+\\.\\d+)  = at least one digit, then exactly one decimal, followed by at least one digit
+        (\\d+\\.)  = at least one digit followed by exactly one decimal
+        \\d+  = at least one digit
+        (e|E)  = small e or big E, exactly once
+        (-|\\+)?  = minus or plus 0 to 1 times
+        \\d+  = at least one digit
+        */
+        var pattern = "^(-|\\+)?((\\.\\d+)|(\\d+\\.\\d+)|(\\d+\\.)|\\d+)((e|E)(-|\\+)?\\d+)?$";
+        return s.matches(pattern);
+    }
+}
