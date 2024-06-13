a35a9b48d63e49a33df6a43784a67791a515a94b
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-06-13 10:13:22 +0530

    Time: 58 ms (61%), Space: 44.6 MB (19.92%)

---
+++ 0065-valid-number/0065-valid-number.cs
@@ -0,0 +1,19 @@
+using System.Text.RegularExpressions;
+
+public class Solution {
+    public bool IsNumber(string s) {
+        /*
+        ^ = start of string, $ = end of string, | = OR condition, ? = 0 to 1 times, + = 1 or more
+        (-|\+)?  = minus or plus 0 to 1 times
+        (\.\d+)  = decimal followed by at least one digit
+        (\d+\.\d+)  = at least one digit, then exactly one decimal, followed by at least one digit
+        (\d+\.)  = at least one digit followed by exactly one decimal
+        \d+  = at least one digit
+        (e|E)  = small e or big E, exactly once
+        (-|\+)?  = minus or plus 0 to 1 times
+        \d+  = at least one digit
+        */
+        var pattern = """^(-|\+)?((\.\d+)|(\d+\.\d+)|(\d+\.)|\d+)((e|E)(-|\+)?\d+)?$""";
+        return Regex.IsMatch(s, pattern);
+    }
+}
