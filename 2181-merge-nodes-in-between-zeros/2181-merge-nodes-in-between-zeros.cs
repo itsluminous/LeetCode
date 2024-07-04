f1771b31d44809a88cb8191bd1fbda88580cfec2
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-04 11:28:31 +0530

    Time: 664 ms (20.37%), Space: 123.9 MB (40.74%)

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cs
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cs
@@ -1,15 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
 public class Solution {
+    public ListNode MergeNodes(ListNode head) {
+        var mergeHead = new ListNode(-1);   // temp head
+        var mergePtr = mergeHead;
+        head = head.next;
+
+        var curr = 0;
+        while(head != null){
+            if(head.val == 0){
+                var node = new ListNode(curr);
+                mergePtr.next = node;
+                mergePtr = mergePtr.next;
+                curr = 0;
+            }
+            else curr += head.val;
+            head = head.next;
+        }
+
+        return mergeHead.next;
+    }
+}
+
+// Without additional space
+public class SolutionNoSpace {
     public ListNode MergeNodes(ListNode head) {
         if(head == null || head.val != 0) return head;
         ListNode curr = head.next, start = head.next, root = start;
@@ -29,3 +41,15 @@ public class Solution {
         return root;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
