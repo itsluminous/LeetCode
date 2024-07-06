c0f60fb214254f97e3ec67dc6d06c55c3ab6dc85
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-06 08:41:00 +0530

    Time: 24 ms (36.96%), Space: 26.9 MB (56.52%)

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int PassThePillow(int n, int time) {
+        // remove full cycles
+        var roundTrip = (n-1) * 2;
+        time %= roundTrip;
+
+        // left to right
+        if(time < n) return time + 1;
+        
+        // right to left
+        time -= n;
+        return n - time - 1;
+    }
+}
