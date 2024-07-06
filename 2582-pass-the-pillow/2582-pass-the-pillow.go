c10478c32bcaecb362bccc1682e7d3cc1b40b736
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-06 08:42:25 +0530

    Time: 1 ms (61.54%), Space: 2.2 MB (46.15%)

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.go
@@ -0,0 +1,14 @@
+func passThePillow(n int, time int) int {
+    // remove full cycles
+    roundTrip := (n-1) * 2
+    time %= roundTrip
+
+    // left to right
+    if time < n {
+        return time + 1
+    }
+    
+    // right to left
+    time -= n
+    return n - time - 1
+}
