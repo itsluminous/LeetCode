9128b0ef50d4548085c54fc4b22f744bcad9dfc5
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-06 08:44:23 +0530

    Time: 0 ms (100%), Space: 7.2 MB (30.58%)

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        // remove full cycles
+        int roundTrip = (n-1) * 2;
+        time %= roundTrip;
+
+        // left to right
+        if(time < n) return time + 1;
+
+        // right to left
+        time -= n;
+        return n - time - 1;
+    }
+};
