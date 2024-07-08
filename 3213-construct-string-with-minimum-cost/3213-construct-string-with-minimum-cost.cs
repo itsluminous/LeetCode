3456bec83ce4029a2fc4c9181ad59a3761eed7cb
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-08 18:04:50 +0530

    Time: 416 ms (100%), Space: 131.7 MB (100%)

--- 3213-construct-string-with-minimum-cost/3213-construct-string-with-minimum-cost.cs
+++ 3213-construct-string-with-minimum-cost/3213-construct-string-with-minimum-cost.cs
@@ -3,32 +3,31 @@ public class Solution {
         var n = target.Length;
         var dict = new Dictionary<string, int>();
 
-        var dp = Enumerable.Repeat(int.MaxValue, n+1).ToArray();
+        var dp = Enumerable.Repeat(int.MaxValue, n + 1).ToArray();
         dp[0] = 0;
 
         var trie = new Trie();
-        for (var i=0; i < words.Length; i++){
-            if(!dict.ContainsKey(words[i])){
+        for (var i = 0; i < words.Length; i++) {
+            if (!dict.ContainsKey(words[i])) {
                 trie.Insert(words[i], i);
                 dict[words[i]] = costs[i];
-            }   
-            else if(dict[words[i]] > costs[i]){
+            } else if (dict[words[i]] > costs[i]) {
                 trie.Insert(words[i], i);
                 dict[words[i]] = costs[i];
             }
         }
 
-        for(var i=0; i<n; i++){
-            if(dp[i] == int.MaxValue) continue;
+        for (var i = 0; i < n; i++) {
+            if (dp[i] == int.MaxValue) continue;
 
             var root = trie.root;
-            for(var j=i; j<n; j++){
-                if(!root.children.ContainsKey(target[j])) break;
-                root = root.children[target[j]];
-                
-                if(root.index != -1) {
+            for (var j = i; j < n; j++) {
+                root = root.children[target[j] - 'a'];
+                if (root == null) break;
+
+                if (root.index != -1) {
                     var wIdx = root.index;
-                    dp[j+1] = Math.Min(dp[j+1], dp[i] + costs[wIdx]);
+                    dp[j + 1] = Math.Min(dp[j + 1], dp[i] + costs[wIdx]);
                 }
             }
         }
@@ -37,7 +36,7 @@ public class Solution {
     }
 
     public class TrieNode {
-        public Dictionary<char, TrieNode> children = new();
+        public TrieNode[] children = new TrieNode[26];
         public int index = -1;
     }
 
@@ -46,11 +45,11 @@ public class Solution {
 
         public void Insert(string word, int index) {
             var node = root;
-            foreach(var ch in word){
-                if(!node.children.ContainsKey(ch))
-                    node.children[ch] = new TrieNode();
-                node =   node.children[ch];
-            }   
+            foreach (var ch in word) {
+                if (node.children[ch - 'a'] == null)
+                    node.children[ch - 'a'] = new TrieNode();
+                node = node.children[ch - 'a'];
+            }
             node.index = index;
         }
     }
