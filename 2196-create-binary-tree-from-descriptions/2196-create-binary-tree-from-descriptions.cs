be3bf2f87d5292e871b75663fdb105804f4fbde2
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-15 08:09:40 +0530

    Time: 731 ms (55.56%), Space: 69.4 MB (66.67%)

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cs
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cs
@@ -1,3 +1,41 @@
+public class Solution {
+    public TreeNode CreateBinaryTree(int[][] descriptions) {
+        var dict = new Dictionary<int,TreeNode>();
+        var parents = new HashSet<int>();       // candidates for root
+        var children = new HashSet<int>();      // can never be root, as they have a parent
+
+        foreach(var desc in descriptions){
+            int parent = desc[0], child = desc[1];
+            var isLeft = desc[2] == 1;
+
+            // create nodes if they do not exist
+            if(!dict.ContainsKey(parent)){
+                var node = new TreeNode(parent);
+                dict[parent] = node;
+            }
+            if(!dict.ContainsKey(child)){
+                var node = new TreeNode(child);
+                dict[child] = node;
+            }
+
+            // link parent and child
+            if(isLeft)
+                dict[parent].left = dict[child];
+            else
+                dict[parent].right = dict[child];
+
+            // update parents and children set
+            if(!children.Contains(parent)) parents.Add(parent);
+            parents.Remove(child);
+            children.Add(child);
+        }
+
+        // parents should now have only one value
+        var p = parents.Single();
+        return dict[p];
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,39 +49,3 @@
  *     }
  * }
  */
-public class Solution {
-    public TreeNode CreateBinaryTree(int[][] descriptions) {
-        var root = new TreeNode();
-        var dict = new Dictionary<int, TreeNode>();
-        var rootCandidates = new HashSet<TreeNode>();
-        var removed = new HashSet<TreeNode>();
-        
-        foreach(var desc in descriptions){
-            var (p, c, isLeft) = (desc[0], desc[1], desc[2]);
-            
-            var par = new TreeNode(p);
-            var ch = new TreeNode(c);
-            
-            // parent node
-            if(dict.ContainsKey(p)) par = dict[p];
-            else dict[p] = par;
-            
-            // child node
-            if(dict.ContainsKey(c)) ch = dict[c];
-            else dict[c] = ch;
-            
-            // add child node to parent
-            if(isLeft == 1) par.left = ch;
-            else par.right = ch;
-            
-            // add current parent as root node candidate
-            if(!removed.Contains(par)) rootCandidates.Add(par);
-            
-            // remove current child from root nodes candidate list
-            rootCandidates.Remove(ch);
-            removed.Add(ch);
-        }
-        
-        return rootCandidates.Single();
-    }
-}
