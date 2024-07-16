6841526297b985dca2a338418dd20b85245653c7
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-16 19:31:49 +0530

    Time: 226 ms (91.94%), Space: 0B (100%)

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
-/* Write your PL/SQL query statement below */
+-- Write your PostgreSQL query statement below
 select emp.name as Employee
 from Employee emp
 inner join Employee mgr on mgr.id = emp.managerId
