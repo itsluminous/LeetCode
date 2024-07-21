a74940cac5ad87fc5af726f8d163046bb9d131a5
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-21 16:05:03 +0530

    Time: 773 ms (100%), Space: 30.3 MB (100%)

--- 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.py
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.py
@@ -12,9 +12,9 @@ class Solution:
                 incr = diff
                 decr = 0
             elif diff < 0:
-                if decr < -diff:
-                    ops += -diff - decr
-                decr = -diff
+                if diff < decr:
+                    ops += decr - diff
+                decr = diff
                 incr = 0
             else:
                 incr = decr = 0
