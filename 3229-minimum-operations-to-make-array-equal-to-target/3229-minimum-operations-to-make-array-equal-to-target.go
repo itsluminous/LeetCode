1cf11c627ca4c206e2b67b82f3c940980c32e97f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-21 13:27:43 +0530

    Time: 92 ms (100%), Space: 9.5 MB (100%)

---
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.go
@@ -0,0 +1,27 @@
+func minimumOperations(nums []int, target []int) int64 {
+    n := len(nums)
+    var incr, decr, ops int64
+
+    for i:=0; i<n; i++ {
+        diff := int64(target[i] - nums[i])
+
+        if diff > 0 {
+            if incr < diff {
+                ops += diff - incr
+            }
+            incr = diff
+            decr = 0
+        } else if diff < 0 {
+            if decr < -diff {
+                ops += -diff - decr
+            }
+            decr = -diff
+            incr = 0
+        } else {
+            incr = 0
+            decr = 0
+        }
+    }
+
+    return ops
+}
