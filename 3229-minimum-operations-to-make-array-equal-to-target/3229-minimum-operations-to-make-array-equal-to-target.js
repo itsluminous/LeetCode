dfeefb97a2d20aaf1f0345cf419d93831fe4f3a9
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-21 13:27:49 +0530

    Time: 86 ms (100%), Space: 60.8 MB (100%)

---
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} target
+ * @return {number}
+ */
+var minimumOperations = function(nums, target) {
+    let n = nums.length;
+    let incr = 0, decr = 0, ops = 0;
+
+    for (let i = 0; i < n; i++) {
+        let diff = target[i] - nums[i];
+
+        if (diff > 0) {
+            if (incr < diff)
+                ops += diff - incr;
+            incr = diff;
+            decr = 0;
+        } else if (diff < 0) {
+            if (decr < -diff)
+                ops += -diff - decr;
+            decr = -diff;
+            incr = 0;
+        } else {
+            incr = decr = 0;
+        }
+    }
+
+    return ops;
+};
