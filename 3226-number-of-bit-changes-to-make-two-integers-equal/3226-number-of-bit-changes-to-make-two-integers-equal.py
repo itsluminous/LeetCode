a5d1e2d46d57f23bcf607d220d7856d42b7c845e
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-21 13:20:06 +0530

    Time: 40 ms (50%), Space: 16.4 MB (100%)

---
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minChanges(self, n: int, k: int) -> int:
+        if n == k: return 0
+        if n < k: return -1
+
+        xor = n ^ k
+        if (k & ~n) != 0: return -1
+
+        return bin(xor).count('1')
