477230d2035a2b86aa3e33406e4837b0033338d4
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-21 13:28:02 +0530

    Time: 778 ms (100%), Space: 30.3 MB (100%)

---
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumOperations(self, nums: List[int], target: List[int]) -> int:
+        n = len(nums)
+        incr, decr, ops = 0, 0, 0
+
+        for i in range(n):
+            diff = target[i] - nums[i]
+
+            if diff > 0:
+                if incr < diff:
+                    ops += diff - incr
+                incr = diff
+                decr = 0
+            elif diff < 0:
+                if decr < -diff:
+                    ops += -diff - decr
+                decr = -diff
+                incr = 0
+            else:
+                incr = decr = 0
+
+        return ops
