fca129b0cc4d9d29b21eea323e487e19887b50e7
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-29 09:26:58 +0530

    Time: 86 ms (71.05%), Space: 42.2 MB (23.68%)

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cs
@@ -0,0 +1,25 @@
+// for each number in array, count how many on left are smaller & how many on right are greater
+// do opposite for descending sequence
+public class Solution {
+    public int NumTeams(int[] rating) {
+        int count = 0, n = rating.Length;
+
+        for(var i=1; i<n-1; i++){
+            int[] smaller = new int[2], greater = new int[2];
+            for(var j=0; j<n; j++){
+                if(i == j) continue;
+                if(rating[i] < rating[j]){
+                    if(j > i) smaller[1]++;
+                    else smaller[0]++;
+                }
+                else{
+                    if(j > i) greater[1]++;
+                    else greater[0]++;
+                }
+            }
+
+            count += (smaller[0] * greater[1]) + (greater[0] * smaller[1]);
+        }
+        return count;
+    }
+}
