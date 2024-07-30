cabea629433375968b170c66c5c57cbbc690af7a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-30 09:35:34 +0530

    Time: 239 ms (81.67%), Space: 17.8 MB (66.67%)

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        n, minDel, b = len(s), 0, 0
+        
+        for ch in s:
+            if ch == 'b':
+                b += 1
+            else:
+                minDel = min(minDel + 1, b)
+
+        return minDel
