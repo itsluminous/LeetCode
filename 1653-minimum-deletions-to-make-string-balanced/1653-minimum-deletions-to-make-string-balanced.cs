7d519cc003294e3eaf9d6207ff5a8a10a472bae4
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-30 09:38:54 +0530

    Time: 71 ms (42.86%), Space: 49.4 MB (71.43%)

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cs
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cs
@@ -1,7 +1,6 @@
 public class Solution {
     public int MinimumDeletions(string s) {
-        int n = s.Length, minDel = 0, b = 0;
-        
+        int minDel = 0, b = 0;
         foreach(var ch in s){
             if(ch == 'b')
                 b++;
