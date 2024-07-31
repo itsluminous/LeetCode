db72ff9b726ec0c47662683d0dd2747dbaef2da3
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-31 09:03:47 +0530

    Time: 2 ms (19.15%), Space: 47.2 MB (18.54%)

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -0,0 +1,27 @@
+class Solution {
+    int[][] dp;
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        dp = new int[books.length][shelfWidth+1];
+        for(var i=0; i<books.length; i++)
+            Arrays.fill(dp[i], -1);
+
+        return minHeightShelves(books, shelfWidth, 0, shelfWidth, 0);
+    }
+
+    private int minHeightShelves(int[][] books, int shelfWidth, int idx, int width, int height){
+        if(idx == books.length) return 0;
+        if(dp[idx][width] != -1) return dp[idx][width];
+
+        int bookWidth = books[idx][0], bookHeight = books[idx][1];
+        var currHeight = bookHeight + minHeightShelves(books, shelfWidth, idx+1, shelfWidth - bookWidth, bookHeight);
+        if(bookWidth <= width){
+            var heightIncr = Math.max(0, bookHeight - height);
+            var sameRack = heightIncr + minHeightShelves(books, shelfWidth, idx+1, width - bookWidth, height + heightIncr);
+            currHeight = Math.min(currHeight, sameRack);
+        }
+
+        dp[idx][width] = currHeight;
+        return currHeight;
+    }
+}
