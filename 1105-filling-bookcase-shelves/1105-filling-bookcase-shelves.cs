039b4cbd0ae5a9e7f694160f8dbcc90a17ac30c7
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-07-31 09:08:46 +0530

    Time: 72 ms (28.57%), Space: 47.6 MB (42.86%)

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    int[,] dp;
+
+    public int MinHeightShelves(int[][] books, int shelfWidth) {
+        dp = new int[books.Length, shelfWidth+1];
+        return MinHeightShelves(books, shelfWidth, 0, shelfWidth, 0);
+    }
+
+    private int MinHeightShelves(int[][] books, int shelfWidth, int idx, int width, int height){
+        if(idx == books.Length) return 0;
+        if(dp[idx, width] != 0) return dp[idx, width];
+
+        var (bookWidth, bookHeight) = (books[idx][0], books[idx][1]);
+        var currHeight = bookHeight + MinHeightShelves(books, shelfWidth, idx+1, shelfWidth - bookWidth, bookHeight);
+        if(bookWidth <= width){
+            var heightIncr = Math.Max(0, bookHeight - height);
+            var sameRack = heightIncr + MinHeightShelves(books, shelfWidth, idx+1, width - bookWidth, height + heightIncr);
+            currHeight = Math.Min(currHeight, sameRack);
+        }
+
+        dp[idx, width] = currHeight;
+        return currHeight;
+    }
+}
