cb4f30486de87b2efbeed47917d93301f062b831
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-02 18:41:01 +0530

    Time: 282 ms (12.5%), Space: 54.1 MB (83.33%)

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cs
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cs
@@ -1,27 +1,19 @@
 public class Solution {
     public int MinSwaps(int[] nums) {
-        // count number of ones
-        var totalOnes = nums.Count(x => x == 1);
-        
-        // base case - no swap needed
+        int n = nums.Length, n2 = 2 * n, totalOnes = 0;
+        foreach(var num in nums) totalOnes += num;
+
         if(totalOnes <= 1) return 0;
-        
-        // repeat the array twice for circular array
-        var nums2 = new int[2*nums.Length];
-        for(var i=0; i<nums2.Length; i++)
-            nums2[i] = nums[i % nums.Length];
-        
-        // find window having most ones
-        int maxOnesInWindow = 0, onesInCurrWindow = 0;
-        for(var i=0; i<nums2.Length; i++){
-            if(nums2[i] == 1)
-                onesInCurrWindow++;
-            if(i >= totalOnes && nums2[i-totalOnes] == 1)   // if number just before window was 1, reduce count
-                onesInCurrWindow--;
-            maxOnesInWindow = Math.Max(maxOnesInWindow, onesInCurrWindow);
+
+        int maxOnesInWindow = 0, currOnes = 0;
+        for(var i=0; i<n2; i++){
+            currOnes += nums[i % n];
+            // if left of window was one, then decrement
+            if(i >= totalOnes && nums[(i - totalOnes) % n] == 1)
+                currOnes--;
+            maxOnesInWindow = Math.Max(maxOnesInWindow, currOnes);
         }
-        
-        // answer will be number of zeroes in the window with max ones
+
         return totalOnes - maxOnesInWindow;
     }
 }
