1be2a2526532faa8626496bd553d6a5a75c0f0d9
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-06 08:52:24 +0530

    Time: 64 ms (90.91%), Space: 51 MB (90.91%)

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinimumPushes(string word) {
+        var freq = new int[26];
+        foreach(var ch in word)
+            freq[ch - 'a']++;
+        
+        Array.Sort(freq);
+        var count = 0;
+
+        for(int i=25, press=1; i>=0 && freq[i] != 0; i-=8, press++)
+            for(var j=i; j>=0 && j>(i-8); j--)
+                count += freq[j] * press;
+
+        return count;
+    }
+}
