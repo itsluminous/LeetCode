4f84b16704554fdcfddb3e6675856cb206840c5d
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-07 03:26:15 +0000

    Time: 12 ms (20.12%), Space: 42.4 MB (34.01%)

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,5 +1,30 @@
 class Solution {
     public String numberToWords(int num) {
+        String[] lessThan20 = {"","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine","Ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        String[] thousands = {"", "Thousand", "Million", "Billion"};
+
+        if(num == 0) return "Zero";
+        var words = "";
+        for(var i=0; num>0; num /= 1000, i++){
+            if(num % 1000 != 0)
+                words = helper(num % 1000, lessThan20, tens, thousands) + thousands[i] + " " + words;
+        }
+
+        return words.trim();
+    }
+
+    private String helper(int num, String[] lessThan20, String[] tens, String[] thousands){
+        if(num == 0) return "";
+        if(num < 20) return lessThan20[num] + " ";
+        if(num < 100) return tens[num / 10] + " " + helper(num % 10, lessThan20, tens, thousands);
+        return lessThan20[num / 100] + " Hundred " + helper(num % 100, lessThan20, tens, thousands);
+    }
+}
+
+// Accepted
+class SolutionBig {
+    public String numberToWords(int num) {
         if(num == 0) return "Zero";
 
         var sb = new StringBuilder();
