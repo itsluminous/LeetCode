c54b23f5b4f7641ea8dff39d5b21faf3f9a9beb1
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-08 03:01:05 +0000

    Time: 4 ms (81.31%), Space: 44.9 MB (85.51%)

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        var dirs = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
+        var path = new int[rows * cols][2];
+
+        // step should be 1,1,2,2,3,3,4,4,....
+        for(int step = 1, dirIdx = 0, pathIdx = 0; pathIdx < rows * cols; step++ ){
+            // for every step we traverse two directions.
+            for(var i=0; i<2; i++, dirIdx = (dirIdx + 1) % 4){
+                for(var j=0; j<step; j++){
+                    // if curr coordinates is a valid point, then add it to path
+                    if(rStart >= 0 && cStart >= 0 && rStart < rows && cStart < cols){
+                        path[pathIdx][0] = rStart;
+                        path[pathIdx][1] = cStart;
+                        pathIdx++;
+                    }
+
+                    // move to next pos
+                    rStart += dirs[dirIdx][0];
+                    cStart += dirs[dirIdx][1];
+                }
+            }
+        }
+
+        return path;
+    }
+}
