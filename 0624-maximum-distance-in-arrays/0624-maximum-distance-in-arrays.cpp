a1bd983b97c93c48eeb627d8d64d34a5e092b306
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-16 06:05:50 +0000

    Time: 237 ms (44.34%), Space: 107.8 MB (57.55%)

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int smallest = 10001, biggest = -10001, maxDist = 0;
+        for(auto& arr : arrays){
+            maxDist = std::max(maxDist, biggest - arr[0]);
+            maxDist = std::max(maxDist, arr[arr.size()-1] - smallest);
+            smallest = std::min(smallest, arr[0]);
+            biggest = std::max(biggest, arr[arr.size()-1]);
+        }
+
+        return maxDist;
+    }
+};
