22ea1a0a696acd1640e338bb4e1cf58343478bd3
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-19 03:54:09 +0000

    Time: 0 ms (100%), Space: 40.1 MB (94.15%)

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minSteps(int n) {
+        var count = 0;
+        while(n != 1){
+            for(var div=2; div<=n; div++){
+                if(n % div != 0) continue;
+                n /= div;
+                count += div;
+                break;
+            }
+        }
+        return count;
+    }
+}
