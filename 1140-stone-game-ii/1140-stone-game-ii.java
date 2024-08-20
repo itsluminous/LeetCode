2fdc7fa6ca109fffb3e88a5639c31c67c7bb24b8
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-20 01:53:03 +0000

    Time: 3 ms (89.81%), Space: 42.2 MB (75.97%)

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,28 +1,25 @@
 class Solution {
-    int[] suffixSum;
     int[][] dp;
 
     public int stoneGameII(int[] piles) {
         var n = piles.length;
         dp = new int[n][n];
 
-        // calculate prefix sum (from right)
-        suffixSum = new int[n+1];
-        suffixSum[n] = 0;
-        for(var i=n-1; i>=0; i--) suffixSum[i] = piles[i] + suffixSum[i+1];
+        // calculate suffix sum (from right)
+        for(var i=n-2; i>=0; i--) 
+            piles[i] += piles[i+1];
 
         return score(piles, 1, 0);
     }
 
-    private int score(int[] piles, int m, int idx){
-        if(idx == piles.length) return 0;
-        if(idx == piles.length - 1) return piles[idx];
+    private int score(int[] suffixSum, int m, int idx){
+        if(idx + 2*m >= suffixSum.length) return suffixSum[idx];    // last player takes it all
         if(dp[m][idx] != 0) return dp[m][idx];
 
         var maxScore = 0;
-        for(var i=1; i<= 2*m && idx+i<=piles.length; i++){
+        for(var i=1; i<= 2*m; i++){
             var take = suffixSum[idx] - suffixSum[idx+i];
-            var leftover = suffixSum[idx+i] - score(piles, Math.max(m, i), idx+i);
+            var leftover = suffixSum[idx+i] - score(suffixSum, Math.max(m, i), idx+i);
             maxScore = Math.max(maxScore, take + leftover);
         }
 
