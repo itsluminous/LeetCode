caae8875469f25ce5465f63b41c603d0b7d5fddc
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-22 08:32:02 +0530

    Time: 0 ms (100%), Space: 40.5 MB (27.99%)

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findComplement(int num) {
+        // step 1 : get a mask which has all bits set to 1 (starting from first 1 bit)
+        // so mask for num 0000101001 will be 0000111111
+        var mask = getMask(num);
+
+        // step 2 : xor the mask with num, so that only bits originally 0 will be set
+        return num ^ mask;
+    }
+
+    private int getMask(int num){
+        var mask = num;
+        // Copy the highest 1-bit onto all the lower bits
+        while(num != 0){
+            num >>= 1;
+            mask |= num;
+        }
+        return mask;
+    }
+
+    private int getMaskAlt(int num){
+        // Copy the highest 1-bit onto all the lower bits
+        var mask = num;     // Eg. num = 0000101001
+        mask |= mask >> 1;  // 0000111001
+        mask |= mask >> 2;  // 0000111101
+        mask |= mask >> 4;  // 0000111111
+        mask |= mask >> 8;  // 0000111111
+        mask |= mask >> 16; // 0000111111
+        
+        return mask;
+    }
+}
