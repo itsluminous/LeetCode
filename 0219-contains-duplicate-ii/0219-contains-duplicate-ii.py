cb7798330e3934968f1c59a22c9e584c8700bf25
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-25 09:08:56 +0530

    Time: 465 ms (40.45%), Space: 27.9 MB (88.94%)

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = set()
+        for i, num in enumerate(nums):
+            if num in seen: return True
+            seen.add(num)
+            if i >= k: seen.remove(nums[i-k])
+
+        return False
