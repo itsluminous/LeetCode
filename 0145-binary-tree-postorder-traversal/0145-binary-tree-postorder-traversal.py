021d7e8238c458cf2c2061e341e31c4ca633ea89
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-25 07:17:20 +0530

    Time: 25 ms (97.39%), Space: 16.5 MB (64.22%)

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__(self):
+        self.result = []
+
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return self.result
+        
+        self.postorderTraversal(root.left)
+        self.postorderTraversal(root.right)
+        self.result.append(root.val)
+        
+        return self.result
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
