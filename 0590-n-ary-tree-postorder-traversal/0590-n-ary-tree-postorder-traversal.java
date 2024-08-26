bc6d55b506861f082800e2c603a69f53db8cf594
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-08-26 09:14:16 +0530

    Time: 0 ms (100%), Space: 44.7 MB (34.61%)

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+        if(root == null) return ans;
+        for(var child : root.children)
+            postorder(child);
+        ans.add(root.val);
+        return ans;
+    }
+}
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
