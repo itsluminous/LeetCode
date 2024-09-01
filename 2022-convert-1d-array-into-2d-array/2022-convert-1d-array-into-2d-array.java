a0884a4265abd10578d5a0fa8c9e52d50115758a
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-09-01 11:00:50 +0530

    Time: 4 ms (71.26%), Space: 55.5 MB (36.41%)

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        var matrix = new int[m][n];
         int len = original.length, r = -1, c = 0;
         if(len != m*n) return new int[][]{};
 
+        var matrix = new int[m][n];
         for(var i=0; i<len; i++){
             if(i % n == 0){
                 r++;
