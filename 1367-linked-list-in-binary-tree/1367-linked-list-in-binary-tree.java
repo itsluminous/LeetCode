0a4f51ffb847234bb201d7002536407726d6e44c
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-09-07 09:52:46 +0530

    Time: 1 ms (97.88%), Space: 44.2 MB (100%)

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head == null) return true;       // reached end of linked list
+        if(root == null) return false;      // reached end of tree
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(ListNode head, TreeNode root) {
+        if(head == null) return true;       // reached end of linked list
+        if(root == null) return false;      // reached end of tree
+        
+        if(head.val != root.val) return false;
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
