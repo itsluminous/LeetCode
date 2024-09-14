4660dc457765ec55f84304e3747d3e9af751e867
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-09-14 10:41:38 +0530

    Time: 362 ms (23.08%), Space: 75.5 MB (100%)

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestSubarray(int[] nums) {
+        int prev = 0, currCount = 0, maxCount = 0, maxVal = 0;
+        for(var i=0; i<nums.Length; i++){
+            if(nums[i] > maxVal){
+                currCount = maxCount = 1;
+                maxVal = nums[i];
+            }
+            if(nums[i] == maxVal){
+                if(prev != maxVal)
+                    currCount = 1;
+                else{
+                    currCount++;
+                    maxCount = Math.Max(maxCount, currCount);
+                }
+            }
+            prev = nums[i];
+        }
+
+        return maxCount;
+    }
+}
