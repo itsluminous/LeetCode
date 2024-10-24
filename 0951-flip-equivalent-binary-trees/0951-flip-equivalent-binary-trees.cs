283bd9cc58e3e5b9fafd2d37e904d9718bf7ac77
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-10-24 03:43:42 +0000

    Time: 0 ms (100%), Space: 42.7 MB (69.77%)

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool FlipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+
+        return (FlipEquiv(root1.left, root2.left) && FlipEquiv(root1.right, root2.right))
+            || (FlipEquiv(root1.right, root2.left) && FlipEquiv(root1.left, root2.right));
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
