077535ab195e814b85e50f2cec612b1273270d52
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-03 00:14:39 +0000

    Time: 0 ms (100%), Space: 41.2 MB (66.51%)

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+
+        s += s;
+        return s.indexOf(goal) >= 0;
+    }
+}
