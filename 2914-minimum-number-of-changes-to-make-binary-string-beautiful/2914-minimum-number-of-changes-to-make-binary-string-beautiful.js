f8ad9b12a96f47e4c9c7f1b408ca221ae9157355
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-05 00:57:25 +0000

    Time: 3 ms (50%), Space: 52.1 MB (22.22%)

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minChanges = function(s) {
+    let count = 0;
+    for(let i=1; i<s.length; i+=2)
+        if(s[i] != s[i-1])
+            count++;
+    
+    return count;
+};
