07d572064c750b4590b87514e89913eab7b38014
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-08 02:44:31 +0000

    Time: 2 ms (100%), Space: 58.7 MB (73.09%)

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,4 +1,24 @@
+// the trick is that - the max xor value for array will always be 2^maximumBit - 1
+// so k will be anything that makes the total xor = 2^maximumBit - 1
 class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        var n = nums.length;
+        int allxor = 0, maxxor = (1 << maximumBit) - 1;
+        for(var num : nums)
+            allxor ^= num;
+
+        var ans = new int[n];
+        ans[0] = maxxor ^ allxor;
+
+        for(var i=1; i<n; i++)
+            ans[i] = ans[i-1] ^ nums[n-i];
+
+        return ans;
+    }
+}
+
+// Accepted
+class SolutionSimulation {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int n = nums.length, allxor = nums[0];
         for(var i=1; i<nums.length; i++)
