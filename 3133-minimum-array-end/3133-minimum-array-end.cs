38138b614028a55169acf5d9d60d47b1e6adc3cb
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-09 05:22:31 +0000

    Time: 0 ms (100%), Space: 27.2 MB (66.67%)

--- 3133-minimum-array-end/3133-minimum-array-end.cs
+++ 3133-minimum-array-end/3133-minimum-array-end.cs
@@ -1,10 +1,35 @@
+// Let x be 38 (100110), and n be 37.
+// smallest no. in array will always be x (because all bits in x need to be 1)
+// so we need remaining 37 - 1 = 36 (100100) additional elements.
+// we need to fit these 100100 of 36 in the 0's of 38 (000100110)
+// 36    1 0 0   1 0     0
+// 38    0 0 0 1 0 0 1 1 0
+// ans   1 0 0 1 1 0 1 1 0
 public class Solution {
     public long MinEnd(int n, int x) {
-        long ans = x;    // start with x, because that is the smallest number we can have
-        while(--n > 0){
-            ans++;      // increment ans
-            ans |= x;   // OR with x to keep the required bits always 1
+        long num = x, pos = 1;
+        for(long remaining = n-1; remaining > 0; pos <<= 1){
+            if((pos & x) > 0) continue;     // the bit at this pos is 1 in x, skip it
+            num |= pos * (remaining & 1);   // set num's bit at pos position if last bit of remaining is 1
+            remaining >>= 1;                // right shift remaining, so that we try next pos now
         }
-        return ans;
+
+        return num;
+    }
+}
+
+// O(n) - accepted in java & c#
+// smallest no. in array will always be x
+// so we start from x and keep incrementing till count is n
+// in every increment, ensure that at least same bits as x are set
+public class SolutionLinear {
+    public long MinEnd(int n, int x) {
+        long num = x;
+        for(var i=1; i<n; i++){
+            num++;      // increment num
+            num |= x;   // ensure that at least the bits in x are set
+        }
+
+        return num;
     }
 }
