0a7c7ad3320fd1f60c70f23d86915aba4cc52331
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-12 02:28:43 +0000

    Time: 50 ms (100%), Space: 115.2 MB (100%)

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cs
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cs
@@ -1,29 +1,26 @@
 public class Solution {
     public int[] MaximumBeauty(int[][] items, int[] queries) {
-        // sort by price
-        Array.Sort(items, (i1, i2) => i1[0].CompareTo(i2[0]));
-        var n = items.Length;
+        // sort array based on price asc
+        Array.Sort(items, (i1, i2) => i1[0] - i2[0]);
 
-        // find max beauty for each index
+        // find max beauty corresponding to each index in items
+        var n = items.Length;
         var beauty = new int[n];
         beauty[0] = items[0][1];
         for(var i=1; i<n; i++)
             beauty[i] = Math.Max(beauty[i-1], items[i][1]);
-
-        // evaluate the query
-        // find the appropriate index in items arr using binary search
-        // then find the corresponding max beauty value in beauty array
+        
+        // loop through queries and find appropriate beauty
         for(var i=0; i<queries.Length; i++){
-            // if the query is lower than lowest price, then ans will be 0
-            if(items[0][0] > queries[i]){
+            if(queries[i] < items[0][0]){
                 queries[i] = 0;
                 continue;
             }
 
-            // binary search
+            // binary search for appropriate index in queries, and then fetch corresponding beauty
             int low=0, high=n-1;
             while(low <= high){
-                var mid = low + (high-low)/2;
+                var mid = low + (high - low) / 2;
                 if(items[mid][0] > queries[i]) high = mid-1;
                 else low = mid+1;
             }
