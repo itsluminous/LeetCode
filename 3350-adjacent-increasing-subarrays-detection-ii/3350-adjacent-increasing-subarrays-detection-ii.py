7593481f0c158a94d3444f0e93322f7ecba1df2f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-12 02:49:32 +0000

    Time: 1360 ms (77.08%), Space: 44.9 MB (46.09%)

---
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        k, prev, curr = 0, 0, 1    # prev is length of prev increasing subarray
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                curr += 1
+                continue
+            k = self.getMaxLength(prev, curr, k)
+            prev = curr
+            curr = 1
+
+        k = self.getMaxLength(prev, curr, k)
+        return k
+
+    def getMaxLength(self, prev, curr, k):
+        kk = min(prev, curr) # length of k for prev & curr subarrays
+        kk = max(kk, curr//2)
+        k = max(k, kk)
+        return k
