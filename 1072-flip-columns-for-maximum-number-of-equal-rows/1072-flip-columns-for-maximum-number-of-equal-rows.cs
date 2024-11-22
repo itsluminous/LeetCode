8e88de752d3a52b3b29d3c0b16876fd54733c215
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-22 11:58:07 +0530

    Time: 42 ms (80%), Space: 90.2 MB (15.38%)

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cs
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.cs
@@ -1,4 +1,34 @@
-public class Solution {
+class Solution {
+    public int MaxEqualRowsAfterFlips(int[][] matrix) {
+        int m = matrix.Length, n = matrix[0].Length;
+        var freq = new Dictionary<string, int>();
+
+        // count how many rows have same or exact opposite bits
+        // we check opposites because after flipping, if curr row is 1111, then the opposite will be 0000
+        for(var i=0; i<m; i++){
+            StringBuilder bits = new(), flip = new();
+            for(var j=0; j<n; j++){
+                bits.Append(matrix[i][j]);
+                flip.Append(1 - matrix[i][j]);
+            }
+
+            if(freq.ContainsKey(bits.ToString())) freq[bits.ToString()]++;
+            else freq[bits.ToString()] = 1;
+
+            if(freq.ContainsKey(flip.ToString())) freq[flip.ToString()]++;
+            else freq[flip.ToString()] = 1;
+        }
+
+        var ans = 0;
+        foreach(var count in freq.Values)
+            ans = Math.Max(ans, count);
+
+        return ans;
+    }
+}
+
+// Accepted - without using dictionary
+public class Solution1 {
     public int MaxEqualRowsAfterFlips(int[][] matrix) {
         int m = matrix.Length, n = matrix[0].Length, ans = 0;
 
