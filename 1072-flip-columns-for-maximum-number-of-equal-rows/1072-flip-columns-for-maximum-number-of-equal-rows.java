71a25c8a7041437cb23c364b48564ae39cec975f
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-11-22 11:55:41 +0530

    Time: 48 ms (34.83%), Space: 57.1 MB (82.84%)

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,5 +1,32 @@
 class Solution {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        var freq = new HashMap<String, Integer>();
+
+        // count how many rows have same or exact opposite bits
+        // we check opposites because after flipping, if curr row is 1111, then the opposite will be 0000
+        for(var i=0; i<m; i++){
+            StringBuilder bits = new StringBuilder(), flip = new StringBuilder();
+            for(var j=0; j<n; j++){
+                bits.append(matrix[i][j]);
+                flip.append(1 - matrix[i][j]);
+            }
+
+            freq.put(bits.toString(), freq.getOrDefault(bits.toString(), 0) + 1);
+            freq.put(flip.toString(), freq.getOrDefault(flip.toString(), 0) + 1);
+        }
+
+        var ans = 0;
+        for(var count : freq.values())
+            ans = Math.max(ans, count);
+
+        return ans;
+    }
+}
+
+// Accepted - without using hashmap
+class Solution1 {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length, ans = 0;
 
         // count how many rows have same or exact opposite bits
