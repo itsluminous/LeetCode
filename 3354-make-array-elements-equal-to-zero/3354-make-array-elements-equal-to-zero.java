6395dd4c07df7776446a888cb85f3d64d9ae6662
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-12-01 15:51:15 +0530

    Time: 1 ms (93.01%), Space: 42.7 MB (63.84%)

---
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -0,0 +1,22 @@
+// prefixSum : track sum on left of zero and right if zero
+class Solution {
+    public int countValidSelections(int[] nums) {
+        var n = nums.length;
+        int[] leftSum = new int[n], rightSum = new int[n];
+
+        for(var i=1; i<n; i++){
+            leftSum[i] = leftSum[i-1] + nums[i-1];
+            rightSum[n-i-1] = rightSum[n-i] + nums[n-i];
+        }
+
+        var ans = 0;
+        for(var i=0; i<n; i++){
+            if(nums[i] != 0) continue;
+            var diff = Math.abs(leftSum[i] - rightSum[i]);
+            if(diff == 0) ans += 2; // we can go in left & right dir both
+            if(diff == 1) ans += 1; // we can go only in one direction
+        }
+
+        return ans;
+    }
+}
