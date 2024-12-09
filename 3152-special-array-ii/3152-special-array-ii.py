f02dd21cf172e82e12dd076a3f460e4d716376a6
Prakash <24225487+itsluminous@users.noreply.github.com>
2024-12-09 08:17:38 +0530

    Time: 31 ms (93.98%), Space: 46.4 MB (55.87%)

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -1,18 +1,21 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        n = len(queries)
-        ans = [False]*n
+        n = len(nums)
+        
+        # prefix array of how many have same parity till given idx
+        pre = [0]*(n)
+        for i in range(1, n):
+            if (nums[i] & 1) == (nums[i-1] & 1):
+                pre[i] = pre[i-1] + 1
+            else:
+                pre[i] = pre[i-1]
 
-        # calculate how many bad pairs we have seen till now
-        bad = [0] * len(nums)
-        j = 0
-        for i in range(1, len(nums)):
-            if (nums[i] & 1) == (nums[i-1] & 1):  j += 1
-            bad[i] = j
-
-        # for each query, if the count of bad pairs at start index is same as that at end index, then its special
-        for i in range(n):
-            start, end = queries[i][0], queries[i][1]
-            if bad[start] == bad[end]: ans[i] = True
-
+        # figure out ans based on prefix array
+        ans = [False] * len(queries)
+        for i, (start, end) in enumerate(queries):
+            if pre[start] == pre[end]:
+                ans[i] = True
+        
         return ans
+
+                
