163839890abfd90f0ac58c5f526568e8aec769a7
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-01-15 16:26:33 +0530

    Time: 0 ms (100%), Space: 40.6 MB (63.21%)

---
+++ 2429-minimize-xor/2429-minimize-xor.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        var num1set = Integer.bitCount(num1);
+        var num2set = Integer.bitCount(num2);
+        var mask = 1;
+
+        // if bit count is more in num1, remove higher bits
+        // (by setting lower bits to 0 because it will be used for xor)
+        while(num1set > num2set){
+            // when we find a 1 in num1 binary, set it to 0
+            if((num1 & mask) == mask){
+                num1 ^= mask;
+                num1set--;
+            }
+            mask <<= 1;
+        }
+
+        // if bit count is less in num1, add lower bits
+        while(num1set < num2set){
+            // when we find a 0 in num1 binary, set it to 1
+            if((num1 & mask) != mask){
+                num1 |= mask;
+                num1set++;
+            }
+            mask <<= 1;
+        }
+        
+        // if bit count was already same
+        // then no operation is needed
+        return num1;
+    }
+}
