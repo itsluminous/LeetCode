d03464c231b665b17a4414cc619423af9856b773
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-01 11:14:20 +0530

    Time: 0 ms (100%), Space: 46.5 MB (5%)

--- 3151-special-array-i/3151-special-array-i.cs
+++ 3151-special-array-i/3151-special-array-i.cs
@@ -1,10 +1,8 @@
 public class Solution {
     public bool IsArraySpecial(int[] nums) {
-        var n = nums.Length;
-        for(var i=0; i<n-1; i++){
-            if((nums[i] & 1) == 1 && (nums[i+1] & 1) == 1) return false;
-            if((nums[i] & 1) == 0 && (nums[i+1] & 1) == 0) return false;
-        }
+        for(var i=1; i<nums.Length; i++)
+            if((nums[i] & 1) == (nums[i-1] & 1))
+                return false;
         return true;
     }
 }
