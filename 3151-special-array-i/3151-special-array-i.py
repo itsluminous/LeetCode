979c853f569b8d95721ccad53264eaa1f501c344
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-01 11:13:52 +0530

    Time: 0 ms (100%), Space: 17.8 MB (33.33%)

--- 3151-special-array-i/3151-special-array-i.py
+++ 3151-special-array-i/3151-special-array-i.py
@@ -1,7 +1,6 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        n = len(nums)
-        for i in range(n-1):
-            if (nums[i] & 1) == 1 and (nums[i+1] & 1) == 1: return False
-            if (nums[i] & 1) == 0 and (nums[i+1] & 1) == 0: return False
+        for i in range(len(nums)-1):
+            if (nums[i] & 1) == (nums[i+1] & 1):
+                return False
         return True
