9a624b7eb6d6e6ad27bb4410cbdb9a3aaf9b2bf2
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-08 15:29:43 +0000

    Time: 0 ms (100%), Space: 17.7 MB (57.35%)

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,4 +1,18 @@
+# the robot can only touch m-1 + n-1 cells in grid, no matter what path you take
+# these (m+n-2) paths can vary based on when you take down vs when you take right
+# so possible ways to touch these cells are (m+n-2)C(m-1)
+# i.e. (m+n-2)! / (m-1)! * (n-1)!
+# refer https://betterexplained.com/articles/easy-permutations-and-combinations/
 class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        ans = j = 1
+        for i in range(m+n-2, max(m,n) - 1, -1):
+            ans = (ans * i) // j
+            j += 1
+        return ans
+
+# accepted O(m*n)
+class SolutionN2:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[1 for _ in range(n)] for _ in range(m)]
 
