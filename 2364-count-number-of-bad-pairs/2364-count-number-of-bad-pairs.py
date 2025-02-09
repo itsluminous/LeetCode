4ac84b6173e64640bbccb25359f1fc7a7d95405f
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-09 13:56:15 +0000

    Time: 79 ms (59.05%), Space: 38.8 MB (59.29%)

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -1,19 +1,18 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         # count how many numbers differ from their index
-        mismatchCount = 0
         mismatch = defaultdict(int)  # key = diff, value = count
-        
+        numsCount = len(nums)
+
         for i in range(len(nums)):
             diff = nums[i] - i
             mismatch[diff] += 1
-            mismatchCount += 1
         
         # a set of numbers with the same diff will always make good pairs
         # so we can only pair them with any other number in the world
         count = 0
         for freq in mismatch.values():
-            mismatchCount -= freq
-            count += freq * mismatchCount
+            numsCount -= freq
+            count += freq * numsCount
         
         return count
