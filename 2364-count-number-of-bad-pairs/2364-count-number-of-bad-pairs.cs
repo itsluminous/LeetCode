3e0cb6452da0db2f74f72ce1bad5c2633dfba205
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-09 13:58:38 +0000

    Time: 24 ms (81.25%), Space: 71.4 MB (43.75%)

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cs
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cs
@@ -1,21 +1,20 @@
 public class Solution {
     public long CountBadPairs(int[] nums) {
         // count how many numbers differ from their index
-        var mismatchCount = 0;
+        var numsCount = nums.Length;
         var mismatch = new Dictionary<int, int>();  // key = diff, val = count
-        for(var i=0; i<nums.Length; i++){
+        for(var i=0; i<numsCount; i++){
             var diff = nums[i] - i;
             if(mismatch.ContainsKey(diff)) mismatch[diff]++;
             else mismatch[diff] = 1;
-            mismatchCount++;
         }
 
         // a set of numbers with same diff will always make good pairs
         // so we can only pair them with any other no. in the world
         long count = 0;
         foreach(var freq in mismatch.Values){
-            mismatchCount -= freq;
-            count += freq * mismatchCount;
+            numsCount -= freq;
+            count += freq * numsCount;
         }
 
         return count;
