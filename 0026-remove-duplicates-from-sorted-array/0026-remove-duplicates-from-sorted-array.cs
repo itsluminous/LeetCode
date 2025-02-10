675ccb3d21755111a398f250ee8ae1966ddcfa46
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-10 13:01:38 +0000

    Time: 0 ms (100%), Space: 50.8 MB (41.12%)

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,16 +1,10 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        int n = nums.Length, left = 0, right = 0;
-        var seen = new bool[201];
-
-        while(right < n){
-            if(!seen[nums[right] + 100]){
-                nums[left++] = nums[right];
-                seen[nums[right] + 100] = true;
-            }
-            right++;
+        var k = 1;
+        for(var i=1; i < nums.Length; i++){
+            if(nums[i] != nums[i-1])
+                nums[k++] = nums[i];
         }
-
-        return left;
+        return k;
     }
 }
