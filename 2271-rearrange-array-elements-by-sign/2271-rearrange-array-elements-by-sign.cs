b67b1d4529cec508901376582befc9dad8f50ac0
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-12 16:43:57 +0000

    Time: 515 ms (5.19%), Space: 135.3 MB (5.19%)

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] RearrangeArray(int[] nums) {
+        var len = nums.Length;
+        var result = new int[len];
+
+        for(int i=0, p=0, n=1; i<len; i++){
+            if(nums[i] < 0){
+                result[n] = nums[i];
+                n += 2;
+            }
+            else{
+                result[p] = nums[i];
+                p += 2;
+            }
+        }
+
+        return result;
+    }
+}
