136e69a01a8a124eee195c38e88c3b457c9c3975
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-13 06:53:26 +0000

    Time: 7 ms (58.4%), Space: 18.9 MB (82.02%)

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,5 +1,43 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)
+
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if isConnected[i][j] == 1:
+                    uf.union(i, j)
+
+        return uf.count
+
+class UnionFind:
+    def __init__(self, n):
+        self.count = n
+        self.rank = [0] * n
+        self.parent = list(range(n))
+
+    def union(self, n1: int, n2: int):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+        if p1 == p2: return
+
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        else:
+            self.parent[p1] = p2
+            if self.rank[p1] == self.rank[p2]:
+                self.rank[p2] += 1
+        self.count -= 1
+
+    def find(self, node: int) -> int:
+        p = self.parent[node]
+        if p == node:
+            return p
+        return self.find(p)
+
+# Accepted
+class SolutionDFS:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n, province = len(isConnected), 0
         self.visited = [False] * (n+1)
         for city in range(n):
