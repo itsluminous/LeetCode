11f37068e2a3821560e6dea40f04d5b5b9f2a72f
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-13 06:53:32 +0000

    Time: 1 ms (87.98%), Space: 47.9 MB (46.92%)

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,5 +1,55 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        var n = isConnected.length;
+        var uf = new UnionFind(n);
+
+        for(var i=0; i<n-1; i++)
+            for(var j=i+1; j<n; j++)
+                if(isConnected[i][j] == 1)
+                    uf.union(i, j);
+
+        return uf.count;
+    }
+
+    class UnionFind{
+        int[] parent, rank;
+        public int count;
+
+        public UnionFind(int n){
+            count = n;
+            rank = new int[n];
+            parent = new int[n];
+            for(var i=0; i<n; i++)
+                parent[i] = i;
+        }
+
+        public void union(int n1, int n2){
+            var p1 = find(n1);
+            var p2 = find(n2);
+            if(p1 == p2) return;
+
+            if(rank[p1] > rank[p2])
+                parent[p2] = p1;
+            else {
+                parent[p1] = p2;
+                if(rank[p1] == rank[p2])
+                    rank[p2]++;
+            }
+            count--;
+        }
+
+        public int find(int node){
+            var p = parent[node];
+            if(p == node)
+                return p;
+            return find(p);
+        }
+    }
+}
+
+// Accepted
+class SolutionDFS {
+    public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length, province = 0;
         var visited = new boolean[n+1];
         for(var city=0; city<n; city++)
