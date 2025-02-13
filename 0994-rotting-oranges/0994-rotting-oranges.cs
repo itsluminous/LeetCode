c01830a26348a61ba5979ac2a2bcbda3ae54c1eb
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-13 08:29:32 +0000

    Time: 1 ms (100%), Space: 42.8 MB (44.33%)

--- 0994-rotting-oranges/0994-rotting-oranges.cs
+++ 0994-rotting-oranges/0994-rotting-oranges.cs
@@ -1,47 +1,38 @@
 public class Solution {
     public int OrangesRotting(int[][] grid) {
-        int m = grid.Length, n = grid[0].Length;
-        var queue = new Queue<(int, int)>();
-
-        var oranges = 0;
-        // fill this queue with all rotten oranges
+        int m = grid.Length, n = grid[0].Length, fresh = 0;
+        var rottenIdx = new Queue<int[]>();
+        
         for(var i=0; i<m; i++){
             for(var j=0; j<n; j++){
-                if(grid[i][j] > 0) oranges++;
-                if(grid[i][j] == 2) queue.Enqueue((i, j));
+                if(grid[i][j] == 1)
+                    fresh++;
+                else if(grid[i][j] == 2)
+                    rottenIdx.Enqueue([i, j]);
             }
         }
 
-        // no need to process if there are no oranges or rotten oranges
-        if(oranges != 0 && queue.Count == 0) return -1;
-        if(oranges == 0 || queue.Count == 0) return 0;
+        if(fresh == 0) return 0;    // nothing is fresh
+        if(rottenIdx.Count == 0) return -1;    // no rotten, no conversion possible
+        
+        (int, int)[] dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        for(var minute=1; rottenIdx.Count > 0; minute++){
+            for(var i=rottenIdx.Count; i>0; i--){
+                var point = rottenIdx.Dequeue();
+                int x = point[0], y = point[1];
 
-        // now start spreading the epidemic - BFS way
-        var minute = -1;
-        for(; queue.Count > 0; minute++){
-            oranges -= queue.Count;
-            for(var i=queue.Count; i>0; i--){
-                var (x,y) = queue.Dequeue();
-                if(x > 0 && grid[x-1][y] == 1) {
-                    grid[x-1][y] = 2; 
-                    queue.Enqueue((x-1, y));
-                }
-                if(x < m-1 && grid[x+1][y] == 1) {
-                    grid[x+1][y] = 2;
-                    queue.Enqueue((x+1, y));
-                }
-                if(y > 0 && grid[x][y-1] == 1) {
-                    grid[x][y-1] = 2;
-                    queue.Enqueue((x, y-1));
-                }
-                if(y < n-1 && grid[x][y+1] == 1) {
-                    grid[x][y+1] = 2;
-                    queue.Enqueue((x, y+1));
+                foreach(var (dx, dy) in dirs){
+                    int nx = x + dx, ny = y + dy;
+                    if(nx == -1 || ny == -1 || nx == m || ny == n || grid[nx][ny] != 1) continue;
+                    
+                    fresh--;
+                    grid[nx][ny] = 0;
+                    rottenIdx.Enqueue([nx, ny]);
                 }
             }
+            if(fresh == 0) return minute;
         }
 
-        if(oranges > 0) return -1;
-        return minute;
+        return -1;
     }
 }
