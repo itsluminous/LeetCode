f9f12adffedce69ab04a05fb8f738ab51428f3b3
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-14 11:58:30 +0000

    Time: 3 ms (89.25%), Space: 19.1 MB (72.01%)

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,35 @@
+# Kahn's Algorithm
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        self.dependencies = [0] * numCourses
+        adjList = self.makeGraph(numCourses, prerequisites)
+
+        # start with courses which have dependencies == 0, i.e. not dependent on anyone
+        queue = deque()
+        for i in range(numCourses):
+            if self.dependencies[i] == 0:
+                queue.append(i)
+        
+        # BFS to start covering each available course one by one
+        idx = 0
+        while queue:
+            curr = queue.popleft()
+            idx += 1
+
+            # each dependent of this course has one dependency satisfied
+            # if all dependencies for a course is satisfied, then we add that in queue
+            for next in adjList[curr]:
+                self.dependencies[next] -= 1
+                if self.dependencies[next] == 0:
+                    queue.append(next)
+        
+        return idx == numCourses
+    
+    def makeGraph(self, numCourses: int, prerequisites: List[List[int]]) -> List[List[int]]:
+        adjList = [[] for _ in range(numCourses)]
+
+        for after, before in prerequisites:
+            adjList[before].append(after)
+            self.dependencies[after] += 1
+        
+        return adjList
