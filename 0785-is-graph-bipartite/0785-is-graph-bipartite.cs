0265884b15ac490e1911b72b06be75b3dacb4d18
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-14 09:39:42 +0000

    Time: 0 ms (100%), Space: 49.8 MB (73.5%)

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cs
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cs
@@ -1,29 +1,25 @@
-// a node will have either color 1 or -1. 0 means its not colored yet
 public class Solution {
     public bool IsBipartite(int[][] graph) {
         var n = graph.Length;
         var colors = new int[n];
-        
-        //This graph might be a disconnected graph. So check each unvisited node.
-        for(var i=0; i<n; i++){
-            if(colors[i] == 0 && !IsValidColor(graph, colors, 1, i))
-                return false;
+
+        bool IsValid(int node, int color){
+            // if already colored, then check if it matches expectation
+            if(colors[node] != 0)
+                return colors[node] == color;
+            
+            colors[node] = color;
+            
+            // try to color all neighbours in opposite color
+            foreach(var next in graph[node])
+                if (!IsValid(next, -color))
+                    return false;
+            return true;
         }
-        return true;
-    }
-    
-    private bool IsValidColor(int[][] graph, int[] colors, int color, int node){
-        // if it is alrady colored, then just check if the color we are about to apply matches it
-        if(colors[node] !=0) 
-            return colors[node] == color;
-        
-        colors[node] = color;
-        // all neighbours should have opposite color of this node
-        foreach(var neighbour in graph[node]){
-            if(!IsValidColor(graph, colors, -color, neighbour))
+
+        for(var i=0; i<n; i++)
+            if(colors[i] == 0 && !IsValid(i, 1))
                 return false;
-        }
-        
         return true;
     }
 }
