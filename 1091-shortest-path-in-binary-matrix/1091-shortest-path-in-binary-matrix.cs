c2467c802686d2e96459b5ef2c7bbac40fe36b25
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-17 09:41:19 +0000

    Time: 9 ms (84.71%), Space: 58.7 MB (23.06%)

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
@@ -1,31 +1,29 @@
 public class Solution {
     public int ShortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.Length, path = 0;
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;   // invalid case
-        
-        // BFS
-        var q = new Queue<(int x, int y)>();
-        q.Enqueue((0,0));
-        while(q.Count > 0){
-            var len = q.Count;
-            path++;
-            for(var i=0; i<len; i++){
-                var point = q.Dequeue();
-                if(point.x == n-1 && point.y == n-1)
-                    return path;
-                // check all neighbours
-                for(var x=-1; x<=1; x++){
-                    for(var y=-1; y<=1; y++){
-                        int newx = point.x + x, newy = point.y + y;
-                        if(newx < 0 || newx == n || newy < 0 || newy == n || grid[newx][newy] == 1)
-                            continue;
-                        q.Enqueue((newx, newy));
-                        grid[newx][newy] = 1;   // mark it as visited
-                    }
+        var n = grid.Length;
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+
+        var queue = new Queue<int[]>();
+        queue.Enqueue([0, 0]);
+
+        (int, int)[] dirs = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
+
+        for(var steps = 1; queue.Count > 0; steps++){
+            for(var i=queue.Count; i>0 ;i--){
+                var coord = queue.Dequeue();
+                int x = coord[0], y = coord[1];
+                if(x == n-1 && y == n-1) return steps;
+
+                foreach(var (dx, dy) in dirs){
+                    int nx = x + dx, ny = y + dy;
+                    if(nx == -1 || ny == -1 || nx == n || ny == n || grid[nx][ny] == 1)
+                        continue;
+                    queue.Enqueue(new int[]{nx, ny});
+                    grid[nx][ny] = 1;   // mark visited
                 }
             }
         }
         
-        return -1;  // if we reached here, it means there is no path to reach last point
+        return -1;
     }
 }
