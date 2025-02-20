97d3ffe8d609c44f52adc22f248ccec4cf03fd7b
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-20 03:25:50 +0000

    Time: 0 ms (100%), Space: 41.8 MB (40.73%)

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,20 @@
+// Cantor's Diagonal Argument
+// basically, we have to form a new string of length n, and no. of items in array are also n
+// so if say, i-th posn in string ensures that this string is different from i-th word
+// then the final string is guaranteed to be different from all strings
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        var ans = new StringBuilder();
+
+        // for i-th string, assign opposite value of i-th char in that string
+        // technically the char can be any random posn, not necessarily matching i
+        // but we want to ensure that one posn corresponds to only one string
+        // so it is easy to keep char posn same as string posn
+        for(var i=0; i<nums.length; i++){
+            var ch = nums[i].charAt(i);
+            ans.append(ch == '0' ? '1' : '0');
+        }
+
+        return ans.toString();
+    }
+}
