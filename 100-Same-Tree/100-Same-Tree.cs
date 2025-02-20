4b2567abea1caf0e570db42a74b1e5c257f1d91c
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-20 11:40:19 +0530

    deleted duplicate directories
    2-add-two-numbers
    5-longest-palindromic-substring
    8-string-to-integer-atoi
    9-palindrome-number
    11-container-with-most-water
    13-roman-to-integer
    17-letter-combinations-of-a-phone-number
    20-valid-parentheses
    21-merge-two-sorted-lists
    23-merge-k-sorted-lists
    31-next-permutation
    39-combination-sum
    42-trapping-rain-water
    61-rotate-list
    62-unique-paths
    78-subsets
    84-largest-rectangle-in-histogram
    98-validate-binary-search-tree
    100-Same-Tree
    104-maximum-depth-of-binary-tree
    121-best-time-to-buy-and-sell-stock
    127-word-ladder
    128-longest-consecutive-sequence
    134-gas-station
    135-candy
    136-single-number
    138-copy-list-with-random-pointer
    141-linked-list-cycle
    150-Evaluate-Reverse-Polish-Notation
    165-compare-version-numbers
    169-majority-element
    189-rotate-array
    198-House-Robber
    199-binary-tree-right-side-view
    200-number-of-islands
    215-kth-largest-element-in-an-array
    216-combination-sum-iii
    234-palindrome-linked-list
    236-lowest-common-ancestor-of-a-binary-tree
    268-missing-number
    287-find-the-duplicate-number
    289-game-of-life
    300-longest-increasing-subsequence
    322-coin-change
    329-longest-increasing-path-in-a-matrix
    338-counting-bits
    344-reverse-string
    392-is-subsequence
    399-evaluate-division
    402-remove-k-digits
    438-find-all-anagrams-in-a-string
    452-minimum-number-of-arrows-to-burst-balloons
    525-contiguous-array
    535-encode-and-decode-tinyurl
    543-Diameter-of-Binary-Tree
    560-subarray-sum-equals-k
    567-permutation-in-string
    605-can-place-flowers
    700-search-in-a-binary-search-tree
    703-kth-largest-element-in-a-stream
    729-my-calendar-i
    739-Daily-Temperatures
    743-network-delay-time
    746-min-cost-climbing-stairs
    785-is-graph-bipartite
    787-Cheapest-Flights-Within-K-Stops
    872-Leaf-Similar-Trees
    881-boats-to-save-people
    916-word-subsets
    977-Squares-of-a-Sorted-Array

--- 100-Same-Tree/100-Same-Tree.cs
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public bool IsSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
-        if(p.val != q.val) return false;
-        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
-    }
-}
