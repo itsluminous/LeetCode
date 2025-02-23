8716e96c89f5a5b915aba9ae5c8aac317814f23c
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-23 07:39:18 +0000

    Time: 166 ms (100%), Space: 45.6 MB (100%)

--- 3463-check-if-digits-are-equal-in-string-after-operations-ii/3463-check-if-digits-are-equal-in-string-after-operations-ii.java
+++ 3463-check-if-digits-are-equal-in-string-after-operations-ii/3463-check-if-digits-are-equal-in-string-after-operations-ii.java
@@ -27,9 +27,10 @@ class Solution {
         var n = s.length() - 2;
         // possible combinations of a (mod2) and b (mod5) and their corresponding x mod10 (using CRT)
         // eg. 0 |0 → 0, 0 |1 → 6, 0 |2 → 2, 0 |3 → 8 and so on
+        // so a even number (eg. 8), will have mod2 = 0, mod5 = 3, so look at index [0, 3] which should be 8
         int[][] combTable = {
-            {0, 6, 2, 8, 4}, 
-            {5, 1, 7, 3, 9}
+            {0, 6, 2, 8, 4},    // Even numbers (mod 2 = 0)
+            {5, 1, 7, 3, 9}     // Odd numbers (mod 2 = 1)
         };
 
         var coefficients = new int[n + 1];
