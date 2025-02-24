2bfc0546871d18ee191d796a7e384b4144454e02
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-24 07:26:00 +0000

    Time: 0 ms (100%), Space: 47.1 MB (32.17%)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        var idx = m + n -1;
+        m--; n--;
+        while(m >= 0 && n >= 0){
+            if(nums1[m] > nums2[n])
+                nums1[idx--] = nums1[m--];
+            else
+                nums1[idx--] = nums2[n--];
+        }
+        
+        while(n >= 0)
+            nums1[idx--] = nums2[n--];
+    }
+}
