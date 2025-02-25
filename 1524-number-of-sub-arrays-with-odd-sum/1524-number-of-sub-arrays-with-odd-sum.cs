2d7d2b504ffb65b90ae97a06cb51c8c982aa511f
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-25 04:44:28 +0000

    Time: 5 ms (73.68%), Space: 66.2 MB (52.63%)

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cs
@@ -0,0 +1,65 @@
+// Accepted - O(1) space : we only need to know odd/even count from prev step
+public class Solution {
+    public int NumOfSubarrays(int[] arr) {
+        var n = arr.Length;
+        var MOD = 1_000_000_007;
+        int even = 0, odd = 0;  // count no. of odd & even subarrays till any idx
+        var ans = 0;
+
+        if(arr[0] % 2 == 0) even = 1;
+        else odd = 1;
+        ans = odd;
+
+        for(var i=1; i<n; i++){
+            var isEven = arr[i] % 2 == 0;
+            if(isEven){
+                // odd + even = odd, so no change there
+                // even + even = even
+                even = (1 + even) % MOD;        // add 1 because curr number alone is also even
+            }
+            else{
+                var prevOdd = odd;
+                // even + odd = odd
+                odd = (1 + even) % MOD;         // add 1 because curr number alone is also odd
+                // odd + odd = even 
+                even = prevOdd;                 
+            }
+
+            // update ans with count of odd subarrays till now
+            ans = (ans + odd) % MOD;
+        }
+        
+        return ans;
+    }
+}
+
+// Accepted - O(n) space
+public class SolutionN {
+    public int NumOfSubarrays(int[] arr) {
+        var n = arr.Length;
+        var MOD = 1_000_000_007;
+        int[] even = new int[n], odd = new int[n];  // count no. of odd & even subarrays till any idx
+        var ans = 0;
+
+        if(arr[0] % 2 == 0) even[0] = 1;
+        else odd[0] = 1;
+        ans = odd[0];
+
+        for(var i=1; i<n; i++){
+            var isEven = arr[i] % 2 == 0;
+            if(isEven){
+                odd[i] = odd[i-1];                  // odd + even = odd
+                even[i] = (1 + even[i-1]) % MOD;    // even + even = even
+            }
+            else{
+                odd[i] = (1 + even[i-1]) % MOD;     // even + odd = odd
+                even[i] = odd[i-1];                 // odd + odd = even
+            }
+
+            // update ans with count of odd subarrays till now
+            ans = (ans + odd[i]) % MOD;
+        }
+        
+        return ans;
+    }
+}
