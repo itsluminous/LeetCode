885d537258bd9150a57e0ae5c5f8ee39fc2eb462
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-02-27 05:43:39 +0000

    Time: 49 ms (87.87%), Space: 55.3 MB (40.53%)

---
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lenLongestFibSubseq(int[] arr) {
+        int n = arr.length, maxLen = 0;
+        var dp = new int[n][n];
+
+        for(var curr=2; curr<n; curr++){
+            int left = 0, right = curr - 1;
+            while(left < right){
+                var sum = arr[left] + arr[right];
+                if(sum == arr[curr]){
+                    dp[right][curr] = 1 + dp[left][right];
+                    maxLen = Math.max(maxLen, dp[right][curr]);
+                    right--;
+                    left++;
+                }
+                else if(sum < arr[curr])
+                    left++;
+                else
+                    right--;
+            }
+        }
+
+        return maxLen > 0 ? maxLen + 2 : 0; // +2 because we did not count first 2 numbers
+    }
+}
