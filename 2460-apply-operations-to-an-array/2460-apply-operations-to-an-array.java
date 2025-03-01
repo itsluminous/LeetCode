6882a923f06aeb8088134770c1875e2e0daec0df
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-01 00:25:55 +0000

    Time: 0 ms (100%), Space: 43.7 MB (70.32%)

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        var n = nums.length;
+        int left = 0, right = 0;
+
+        while(right < n-1){
+            if(nums[right] != nums[right+1] && nums[right] != 0)
+                nums[left++] = nums[right];
+            else if(nums[right] == nums[right+1] && nums[right] == 0)
+                right++;
+            else if(nums[right] == nums[right+1])
+                nums[left++] = 2 * nums[right++];
+
+            right++;
+        }
+
+        if(right < n)
+            nums[left++] = nums[right]; // explicit operation for last digit
+        
+        // append 0
+        while(left < n) nums[left++] = 0;
+        return nums;
+    }
+}
