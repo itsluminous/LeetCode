6f5aca7b79f2f6f4843d14affb7ee2c8cf61918f
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-05 04:02:43 +0000

    Time: 0 ms (100%), Space: 17.9 MB (30.45%)

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -0,0 +1,13 @@
+# num of squares in each step increases in multiples of 4
+# 1st = 1 + 4*0
+# 2nd = 1 + 4*0 + 4*1
+# 3rd = 1 + 4*0 + 4*1 + 4*2
+# 4th = 1 + 4*0 + 4*1 + 4*2 + 4*3
+
+# apSum formula = n/2 * (2a + (n-1) * d)
+# a = 0 in our case, d = 4
+
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        apSum = n * (n - 1) * 2
+        return 1 + apSum
