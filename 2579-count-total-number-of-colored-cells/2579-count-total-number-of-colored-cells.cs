365cc4fd07bc3a4d1932d856d08c45c85f7b64f1
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-05 04:03:19 +0000

    Time: 0 ms (100%), Space: 28.5 MB (100%)

---
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cs
@@ -0,0 +1,15 @@
+// num of squares in each step increases in multiples of 4
+// 1st = 1 + 4*0
+// 2nd = 1 + 4*0 + 4*1
+// 3rd = 1 + 4*0 + 4*1 + 4*2
+// 4th = 1 + 4*0 + 4*1 + 4*2 + 4*3
+
+// apSum formula = n/2 * (2a + (n-1) * d)
+// a = 0 in our case, d = 4
+
+public class Solution {
+    public long ColoredCells(int n) {
+        var apSum = (long)n * (n - 1) * 2;
+        return 1 + apSum;
+    }
+}
