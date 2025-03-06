613adbec8e9c9494c8525e2f7bf7fd6d492f7856
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-06 03:07:40 +0000

    Time: 5 ms (79.04%), Space: 18.3 MB (45%)

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid) ** 2
+        currSum = dup = 0
+        uniq = set()
+        for row in grid:
+            for num in row:
+                if num in uniq:
+                    dup = num
+                else:
+                    uniq.add(num)
+                    currSum += num
+        
+        expectedSum = (n * (n + 1)) // 2
+        missing = expectedSum - currSum
+        return [dup, missing]
