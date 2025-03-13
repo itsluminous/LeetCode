3d4d181ba57bf41e81f8c8733f4fd30d3f44912f
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-13 02:23:22 +0000

    Time: 6 ms (78.57%), Space: 100.8 MB (57.14%)

---
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MinZeroArray(int[] nums, int[][] queries) {
+        int n = nums.Length, q = queries.Length;
+        var decr = new int[n];  // prefix sum of how much can be decremented at each index
+        var currMaxDecr = 0;    // max decr allowed currently
+        var k = 0;
+
+        for(var i=0; i<n; i++){
+            // keep using queries till we cannot qualify current num
+            while(nums[i] > currMaxDecr + decr[i]){
+                if(k == q) return -1;   // exhausted all queries, cannot make it 0
+
+                // read k-th query
+                int l = queries[k][0], r = queries[k][1], val = queries[k][2];
+                k++;
+                
+                // if the query ends before curr idx, it is useless
+                // if the query start after curr idx, then it is useless now, but may be used later
+                if(r < i) continue;
+
+                // apply the k-th query (line sweeping algo)
+                decr[Math.Max(l, i)] += val;
+                if(r < n-1) decr[r + 1] -= val;
+            }
+            currMaxDecr += decr[i];
+        }
+        
+        return k;
+    }
+}
