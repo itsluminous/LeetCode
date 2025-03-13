0a394e01b873a439dc8409585d09e0b6f0f010bb
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-13 02:23:34 +0000

    Time: 3 ms (100%), Space: 129.1 MB (16.15%)

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length, q = queries.length, k = 0, currSum = 0;
-        var prefixSum = new int[n+1];
+        int n = nums.length, q = queries.length;
+        var decr = new int[n];  // prefix sum of how much can be decremented at each index
+        var currMaxDecr = 0;    // max decr allowed currently
+        var k = 0;
 
         for(var i=0; i<n; i++){
             // keep using queries till we cannot qualify current num
-            while(currSum + prefixSum[i] < nums[i]){
-                if(k == q) return -1;    // exhausted all queries
+            while(nums[i] > currMaxDecr + decr[i]){
+                if(k == q) return -1;   // exhausted all queries, cannot make it 0
 
-                int left = queries[k][0], right = queries[k][1], val = queries[k][2];
+                // read k-th query
+                int l = queries[k][0], r = queries[k][1], val = queries[k][2];
                 k++;
-
+                
                 // if the query ends before curr idx, it is useless
                 // if the query start after curr idx, then it is useless now, but may be used later
-                if(right < i) continue;
+                if(r < i) continue;
 
-                // update prefixSum with line sweeping algo (same as 3355. Zero Array Transformation I)
-                prefixSum[Math.max(left, i)] += val;
-                prefixSum[right+1] -= val;
+                // apply the k-th query (line sweeping algo)
+                decr[Math.max(l, i)] += val;
+                if(r < n-1) decr[r + 1] -= val;
             }
-            currSum += prefixSum[i];
+            currMaxDecr += decr[i];
         }
         
         return k;
