95f20c5693942473d207adbeff39d1e023a92dc2
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-15 09:31:39 +0000

    Time: 457 ms (27.31%), Space: 28.5 MB (93.07%)

--- 2560-house-robber-iv/2560-house-robber-iv.py
+++ 2560-house-robber-iv/2560-house-robber-iv.py
@@ -1,24 +1,23 @@
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
-        # function to count how many houses can be robbed without exceeding a per house limit
-        def robCount(limit: int) -> int:
-            count = i = 0
-            while i < len(nums):
-                if nums[i] <= limit:
-                    count += 1
-                    i += 2  # skip next house
-                else:
-                    i += 1
-        
-            return count
-
-        left, right = min(nums), max(nums)
-        while left < right:
-            mid = left + (right - left) // 2
-            count = robCount(mid)
-            if count >= k:
-                right = mid
+        low, high = 1, max(nums)
+        while low < high:
+            mid = low + (high - low) // 2
+            if self.canRob(nums, k, mid):
+                high = mid
             else:
-                left = mid + 1
+                low = mid + 1
+
+        return low
+
+    # check if we can rob k houses without exceeding a per house limit
+    def canRob(self, nums: List[int], k: int, maxRob: int) -> int:
+        i = 0   
+        while i < len(nums):
+            if nums[i] <= maxRob:
+                k -= 1
+                i += 2  # skip next house
+            else:
+                i += 1
         
-        return left
+        return k <= 0
