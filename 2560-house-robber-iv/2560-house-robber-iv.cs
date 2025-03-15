56d297ab4345fe3530232e8c4b88e7548e590821
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-15 09:06:41 +0000

    Time: 26 ms (38.89%), Space: 63.5 MB (44.44%)

--- 2560-house-robber-iv/2560-house-robber-iv.cs
+++ 2560-house-robber-iv/2560-house-robber-iv.cs
@@ -1,25 +1,25 @@
 public class Solution {
     public int MinCapability(int[] nums, int k) {
-        // function to count how many houses can be robbed without exceeding a per house limit
-        int RobCount(int limit){
-            var count = 0;
-            for(var i=0; i<nums.Length; i++){
-                if(nums[i] <= limit){
-                    count++;
-                    i++;    // skip next house
-                }
+        int low = 1, high = nums.Max();
+        while(low < high){
+            var mid = low + (high - low) / 2;
+            if(CanRob(nums, k, mid))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+
+    // check if we can rob k houses without exceeding a per house limit
+    private bool CanRob(int[] nums, int k, int maxRob){
+        for(var i=0; i<nums.Length && k > 0; i++){
+            if(nums[i] <= maxRob){
+                k--;
+                i++;    // skip next house
             }
-            return count;
         }
-
-        int left = 1, right = nums.Max();
-        while(left < right){
-            var mid = left + (right - left) / 2;
-            var count = RobCount(mid);
-            if(count >= k) right = mid;
-            else left = mid+1;
-        }
-
-        return left;
+        return k == 0;
     }
 }
