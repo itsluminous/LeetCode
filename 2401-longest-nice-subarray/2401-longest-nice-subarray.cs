b6481e14e3d17cdd2914a3ac06bf059ab4517da9
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-18 03:11:13 +0000

    Time: 1 ms (100%), Space: 63.3 MB (37.5%)

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cs
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cs
@@ -1,17 +1,37 @@
 public class Solution {
     public int LongestNiceSubarray(int[] nums) {
-        int max = 1, n = nums.Length, left = 0;
-        while(left < n && n - left > max){
-            var allBits = nums[left];
-            var right = left+1;
-            for(; right<n; right++){
-                if((allBits & nums[right]) != 0) break;
-                allBits |= nums[right];
-            }
-            max = Math.Max(max, right - left);
-            left++;
+        int longest = 0, mask = 0;
+
+        for(int left = 0, right = 0; right < nums.Length; right++){
+            // while even a single bit was already set, shift left window
+            while((mask & nums[right]) != 0)
+                mask ^= nums[left++];
+            
+            mask |= nums[right];    // set bits from curr number in mask
+            longest = Math.Max(longest, right - left + 1);
         }
-        
-        return max;
+
+        return longest;
+    }
+}
+
+// Accepted - original soln, one extra step
+public class SolutionOrig {
+    public int LongestNiceSubarray(int[] nums) {
+        int longest = 0, mask = 0;
+
+        for(int left = 0, right = 0; right < nums.Length; right++){
+            var curr = mask ^ nums[right];
+
+            // while even a single bit was already set, shift left window
+            while((curr & mask) != mask){
+                mask ^= nums[left++];
+                curr = mask ^ nums[right];
+            }
+            longest = Math.Max(longest, right - left + 1);
+            mask ^= nums[right];    // set bits from curr number in mask
+        }
+
+        return longest;
     }
 }
