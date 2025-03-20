9fc9ba4a4d3b2fcfd123e42c0d70d143676ed843
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-20 09:25:09 +0000

    Time: 1 ms (63.54%), Space: 46.4 MB (59.72%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,23 @@
-// water level for any index will be equal to smaller one of largest block on left and right
-// now, subtract height[i] from that to get actual water at that index
-public class Solution {
+class Solution {
     public int trap(int[] height) {
-        var n = height.length;
+        int n = height.length, water = 0;
 
-        // find the maximum height on either side of an index
-        int[] lmax = new int[n], rmax = new int[n];
-        for(int i=1, j=n-2; i<n; i++, j--){
-            lmax[i] = Math.max(lmax[i-1], height[i-1]);
-            rmax[j] = Math.max(rmax[j+1], height[j+1]);
+        // find the maximum height on right side of an index
+        var largestOnRight = new int[n];
+        for(var i=n-2; i>=0; i--)
+            largestOnRight[i] = Math.max(largestOnRight[i+1], height[i+1]);
+
+        var largestOnLeft = height[0];
+        for(var i=1; i<n; i++){
+            // water level for any index will be equal to min of largest block on left or right
+            var maxHeight = Math.min(largestOnLeft, largestOnRight[i]);
+            largestOnLeft = Math.max(largestOnLeft, height[i]);
+
+            // subtract height[i] from maxHeight to get actual water at that index
+            if(maxHeight > height[i])
+                water += maxHeight - height[i];
         }
 
-        // now calculated water at an index, using intution
-        var trapped = 0;
-        for(var i=1; i<n-1; i++){
-            var water = Math.min(lmax[i], rmax[i]);
-            var curr = (water - height[i]) > 0 ? (water - height[i]) : 0;
-            trapped += curr;
-        }
-
-        return trapped;
+        return water;
     }
 }
