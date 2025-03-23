36fa4d951bee302a32947549a208776b2c786b9d
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-23 05:25:05 +0000

    Time: 15 ms (91%), Space: 101.4 MB (39.34%)

---
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long minOperations(int[][] queries) {
+        long ans = 0;
+        
+        for(var query : queries){
+            int start = query[0], end = query[1];
+            long ops = 0;
+            long pow4prev = 1; // initially d = 0, so 4 ^ d = 1
+
+            // d = power of 4
+            // 4^15 > 1e9, so enough to cover all number ranges
+            for(var d = 1; d <= 15; d++){
+                long pow4curr = pow4prev * 4;
+
+                // what is the first no. in query range that we can cover
+                long left = Math.max(start, pow4prev);
+                
+                // what is the last no. in query range that we can cover (with next power)
+                long right = Math.min(end, pow4curr - 1);   // pow4curr - 1 because pow4curr will be used as start for next loop
+
+                // if we can cover some numbers, then add them to ops
+                // each number in range will need to be divided "d" times by 4
+                if(right >= left)
+                    ops += (right - left + 1) * d;
+                
+                // set up for next loop
+                pow4prev = pow4curr;
+            }
+
+            // since we can divide two nums in one go, so actual ops will be Ceil(ops / 2)
+            ans += (ops + 1) / 2;
+        }
+
+        return ans;
+    }
+}
