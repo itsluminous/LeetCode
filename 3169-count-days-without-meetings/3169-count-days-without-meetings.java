7001d7ba05e3955f20b0ef9413810d1aa54beb10
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-03-24 03:33:52 +0000

    Time: 36 ms (89.9%), Space: 101.2 MB (12.79%)

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,14 +1,11 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
         Arrays.sort(meetings, (m1, m2) -> m1[0] - m2[0]);
-        
-        // initialize freeDays with number of days we are free before first meeting
-        var freeDays = meetings[0][0] - 1;
-        var prevEnd = meetings[0][1];
+        int freeDays = 0, prevEnd = 0;
 
         // merge intervals, and track free days when new interval is started
-        for(var i=1; i<meetings.length; i++){
-            int currStart = meetings[i][0], currEnd = meetings[i][1];
+        for(var meeting : meetings){
+            int currStart = meeting[0], currEnd = meeting[1];
             if(currStart > prevEnd)
                 freeDays += currStart - prevEnd - 1;
             prevEnd = Math.max(prevEnd, currEnd);
