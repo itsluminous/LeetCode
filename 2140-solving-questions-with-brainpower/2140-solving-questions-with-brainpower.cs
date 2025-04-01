c0aa05db7e4e32e4d3f9f0be727515280a4279f6
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-04-01 02:54:54 +0000

    Time: 3 ms (63.33%), Space: 93 MB (86.67%)

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cs
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cs
@@ -1,5 +1,30 @@
-// DFS with memoization
 public class Solution {
+    public long MostPoints(int[][] questions) {
+        var n = questions.Length;
+        
+        // calculate max points achievable when starting from index i
+        long maxPossible = 0;
+        var maxPoints = new long[n];
+        for(var i=n-1; i>=0; i--){
+            int points = questions[i][0], brainpower = questions[i][1];
+            
+            // score if we include curr point
+            maxPoints[i] = points;
+            var nextIdx = i + brainpower + 1;
+            if(nextIdx < n)
+                maxPoints[i] += maxPoints[nextIdx];
+            
+            // if the score after including curr point is bad, skip it
+            if(maxPoints[i] < maxPossible) maxPoints[i] = maxPossible;
+            else maxPossible = maxPoints[i];
+        }
+
+        return maxPossible;
+    }
+}
+
+// Accepted - DFS with memoization
+public class Solutiondfs {
     public long MostPoints(int[][] questions) {
         var dp = new long[questions.Length];
         return MostPoints(questions, dp, 0);
