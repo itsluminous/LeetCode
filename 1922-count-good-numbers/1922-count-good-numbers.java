2186b10c69005545f096a6ceb656c91b93d1a69e
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-04-13 04:55:38 +0000

    Time: 0 ms (100%), Space: 40.5 MB (65.84%)

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    int MOD = 1_000_000_007;
+
+    public int countGoodNumbers(long n) {
+        var oddCount = 4;   // can be 2, 3, 5, 7
+        var evenCount = 5;  // can be 0, 2, 4, 6, 8
+
+        var oddPos = n / 2;         // n/2 positions will have odd index
+        var evenPos = n - oddPos;   // remaining positions are even
+        
+        return (int)((power(oddCount, oddPos) * power(evenCount, evenPos)) % MOD);
+    }
+
+    // calculate x^y efficiently
+    private long power(long x, long y){
+        long ans = 1;
+        while(y > 0){
+            if((y & 1) == 1)
+                ans = (ans * x) % MOD;
+            
+            y >>= 1;    // divide y by 2
+            x = (x * x) % MOD;
+        }
+
+        return ans % MOD;
+    }
+}
