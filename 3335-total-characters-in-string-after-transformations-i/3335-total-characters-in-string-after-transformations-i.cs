5a95ed50fd2db9e0b6562f6b389158db16e32c90
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-05-13 02:43:29 +0000

    Time: 225 ms (12.5%), Space: 46.3 MB (100%)

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    int MOD = 1_000_000_007;
+
+    public int LengthAfterTransformations(string s, int t) {
+        var ans = 0;
+        var dp = LengthAfterNTransformations();
+
+        foreach(var ch in s){
+            var idx = ch - 'a' + t;
+            ans = (ans + dp[idx]) % MOD;
+        }
+
+        return ans;
+    }
+
+    // function to find length of "a" after N transformations
+    private int[] LengthAfterNTransformations(){
+        var n = 100026; // total 10^5 transformations are allowed, so 10^5 + 26 (buffer)
+        var dp = new int[n];
+        for(var i=0; i<n; i++){
+            if(i < 26) dp[i] = 1;   // length will be 1 till z (because transformation has not happened)
+            else dp[i] = (dp[i-26] + dp[i-25]) % MOD;   // 26 steps to double from a, 25 from b
+        }
+        return dp;
+    }
+}
