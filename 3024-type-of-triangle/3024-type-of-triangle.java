08cd19b90acc7f2298333fbf882a90f7d6fd0875
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-05-20 05:05:48 +0000

    Time: 1 ms (34.63%), Space: 41.9 MB (87.26%)

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        int s1 = nums[0], s2 = nums[1], s3 = nums[2];
+
+        if(s1 + s2 <= s3) return "none";  // can't form triangle
+        if(s1 == s3) return "equilateral";
+        if(s1 == s2 || s2 == s3) return "isosceles";
+        return "scalene";
+    }
+}
