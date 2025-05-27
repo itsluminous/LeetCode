be873685829d8c61737ac6a7241509dc9ad26fa7
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-05-27 01:45:04 +0000

    Time: 0 ms (100%), Space: 41.2 MB (11.01%)

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.java
@@ -1,5 +1,15 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
+        var c = n / m;                              // count of numbers divisble by m
+        var allSum = n * (n+1) / 2;                 // AP sum of first n numbers
+        var mSum = (c * (2 * m + (c - 1) * m)) / 2; // AP sum formula = n/2 * (2*a + (n - 1)*d)
+        return allSum - 2 * mSum;                   // 2* because we need to subtract extra mSum counted in allSum
+    }
+}
+
+// Accepted - O(n)
+class SolutionN {
+    public int differenceOfSums(int n, int m) {
         var ans = 0;
         for(var i=1; i<=n; i++){
             if(i % m == 0) ans -= i;
