7482a48697b3be3589f831a2416b5b6cf4ae0c65
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-05-27 02:16:34 +0000

    Time: 0 ms (100%), Space: 17.9 MB (39.75%)

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
@@ -1,9 +1,9 @@
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
         c = n // m                              # count of numbers divisble by m
-        allSum = n * (n+1) / 2                  # AP sum of first n numbers
-        mSum = (c * (2 * m + (c - 1) * m)) / 2  # AP sum formula = n/2 * (2*a + (n - 1)*d)
-        return int(allSum - 2 * mSum)           # 2* because we need to subtract extra mSum counted in allSum
+        allSum = n * (n+1) // 2                 # AP sum of first n numbers
+        mSum = (c * (2 * m + (c - 1) * m)) // 2 # AP sum formula = n/2 * (2*a + (n - 1)*d)
+        return allSum - 2 * mSum                # 2* because we need to subtract extra mSum counted in allSum
 
 # Accepted - O(n)
 class SolutionN:
