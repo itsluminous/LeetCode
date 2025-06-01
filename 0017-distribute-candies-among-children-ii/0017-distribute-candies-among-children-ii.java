25e3b36e1fcce1ea712322e7680ced4a558b330d
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-06-01 03:07:36 +0000

    Time: 18 ms (58.82%), Space: 41.3 MB (14.71%)

---
+++ 0017-distribute-candies-among-children-ii/0017-distribute-candies-among-children-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        long ans = 0;
+        var child1Min = Math.max(0, n - 2 * limit); // to ensure child2 & child3 are in limit
+        var child1Max = Math.min(limit, n);         // cannot exceed limit
+
+        for(var child1 = child1Min; child1 <= child1Max; child1++){
+            var remaining = n - child1;
+            var child2Min = Math.max(0, remaining - limit); // so that child3 is in limit
+            var child2Max = Math.min(remaining, limit);     // ensure child2 is in limit
+            ans += child2Max - child2Min + 1;               // +1 because max & min range is inclusive
+        }
+        return ans;
+    }
+}
