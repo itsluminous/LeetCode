20efab005a1e422802d4b2f9b558d887c400a76b
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-06-02 02:25:09 +0000

    Time: 4 ms (26.17%), Space: 46.2 MB (16.41%)

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,18 +2,18 @@ class Solution {
     public int candy(int[] ratings) {
         var n = ratings.length;
         var candies = new int[n];
-        Arrays.fill(candies, 1);
-
-        // person with higher rating than immediate left should have more candy
+        Arrays.fill(candies, 1);    // give 1 candy to everyone initially
+        
+        // person with higher rating than immediate left should have more
         for(var i=1; i<n; i++)
             if(ratings[i] > ratings[i-1])
-                candies[i] = candies[i-1] + 1;
+                candies[i] = 1 + candies[i-1];
         
         // person with higher rating than immediate right should have more candy
         var count = candies[n-1];
         for(var i=n-2; i>=0; i--){
             if(ratings[i] > ratings[i+1])
-                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+                candies[i] = Math.max(candies[i], 1 + candies[i+1]);
             count += candies[i];
         }
 
