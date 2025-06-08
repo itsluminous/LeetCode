5195c0deb3ec9a8aa6a204ab1bd3baf7f733daa7
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-06-08 04:11:21 +0000

    Time: 5 ms (40.48%), Space: 49.3 MB (88.1%)

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cs
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cs
@@ -2,6 +2,26 @@ public class Solution {
     IList<int> ans = new List<int>();
 
     public IList<int> LexicalOrder(int n, int curr = 1) {
+        Helper(0, n);
+        return ans;
+    }
+
+    private void Helper(int seed, int max){
+        seed *= 10;
+        for(var i=0; i<10; i++){
+            if(seed + i == 0) continue;
+            if(seed + i > max) break;
+            ans.Add(seed + i);
+            Helper(seed + i, max);
+        }
+    }
+}
+
+// Accepted
+public class SolutionAlt {
+    IList<int> ans = new List<int>();
+
+    public IList<int> LexicalOrder(int n, int curr = 1) {
         if(curr > n) return ans;
         
         ans.Add(curr);
