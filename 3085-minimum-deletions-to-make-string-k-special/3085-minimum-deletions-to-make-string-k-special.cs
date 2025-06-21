8274ff303114215ce73a8f72ed7709299c58bd4e
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-06-21 05:20:49 +0000

    Time: 10 ms (85.71%), Space: 48.6 MB (71.43%)

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.cs
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.cs
@@ -1,23 +1,23 @@
 public class Solution {
-    Dictionary<int,int> frequencyCount;
     public int MinimumDeletions(string word, int k) {
-        var dict = new Dictionary<char, int>();
-        foreach(var ch in word)
-            if(dict.ContainsKey(ch)) dict[ch]++;
-            else dict[ch] = 1;
-        
-        var freq = dict.Values.ToList();
-        freq.Sort();
-        
-        return Delete(freq, 0, freq.Count-1, k);
-    }
-    
-    private int Delete(List<int> freq, int lIdx, int rIdx, int k){
-        var (lVal,rVal) = (freq[lIdx], freq[rIdx]);
-        if(rVal-lVal <= k) return 0;
+        var freq = new int[26];
+        foreach(var ch in word) freq[ch - 'a']++;
 
-        var leftDelete = lVal + Delete(freq, lIdx+1, rIdx, k);
-        var rightDelete = (rVal-lVal-k) + Delete(freq, lIdx, rIdx-1, k);
-        return Math.Min(leftDelete, rightDelete);
+        var uniqFreq = new HashSet<int>(freq);
+
+        var minRemove = int.MaxValue;
+        // we assume each freq as smallest in answer, and then check which numbers need to be removed to make it true
+        foreach(var minFreq in uniqFreq) {
+            if (minFreq == 0) continue;
+
+            int currRemove = 0;
+            foreach(var f in freq) {
+                if(f < minFreq) currRemove += f;
+                else if (f > minFreq + k) currRemove += f - minFreq - k;
+            }
+            minRemove = Math.Min(minRemove, currRemove);
+        }
+
+        return minRemove;
     }
 }
