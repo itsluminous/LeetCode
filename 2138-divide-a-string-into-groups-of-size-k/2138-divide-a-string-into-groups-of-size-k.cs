acd1545bb1e995853d89eb17e732f7f4bfa41370
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-06-22 05:06:42 +0000

    Time: 0 ms (100%), Space: 48.8 MB (95.56%)

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cs
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cs
@@ -1,24 +1,22 @@
 public class Solution {
     public string[] DivideString(string s, int k, char fill) {
-        var len = s.Length;
-        var n = len/k;
-        string[] result = new string[n];
-        // if there is some character left at the end, then array will need one more element
-        if(len%k != 0) result = new string[n+1];
+        var n = s.Length;
+        var groups = (n + k - 1) / k;   // how many groups can we make with s
+        var ans = new string[groups];
 
-        // fill up all strings which fit perfectly
-        for(var i=0; i<n; i++){
-            var sub = s.Substring(i*k, k);
-            result[i] = sub;
-        }
+        // split s into groups
+        for(var i=0; i<groups; i++)
+            ans[i] = s.Substring(i*k, Math.Min(n - i*k, k));
         
-        // fill last string with fill characters
-        if(len%k != 0){
-            var sb = new StringBuilder(s.Substring(n*k, len-(n*k)));
-            for(var j=sb.Length; j<k; j++) sb.Append(fill.ToString());
-            result[n] = sb.ToString();
+        // fill extra chars in last word if applicable
+        if(ans[groups-1].Length < k){
+            var sb = new StringBuilder();
+            sb.Append(ans[groups-1]);
+            for(var i=ans[groups-1].Length; i<k; i++)
+                sb.Append(fill);
+            ans[groups-1] = sb.ToString();
         }
-        
-        return result;
+
+        return ans;
     }
 }
