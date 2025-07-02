67d9a9451c191db7ae65fe4f0f53ed5f6538c0d5
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-07-02 02:08:53 +0000

    Time: 165 ms (63.89%), Space: 55.5 MB (83.33%)

---
+++ 3333-find-the-original-typed-string-ii/3333-find-the-original-typed-string-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int possibleStringCount(String word, int k) {
+        var MOD = 1_000_000_007;
+
+        // we don't care about the actual alphabets, we only need to know freq of each char sitting together
+        var freq = new ArrayList<Integer>();
+        var count = 1;
+        for(var i=1; i<word.length(); i++){
+            if(word.charAt(i) == word.charAt(i-1))
+                count++;
+            else{
+                freq.add(count);
+                count = 1;
+            }
+        }
+        freq.add(count);
+
+        // compute the total number of possible original strings, regardless of the minimum length constraint (k)
+        // for each freq of repeated characters of length `n`, Alice could have originally typed
+        // any number from 1 to n of those characters (e.g., 'aaa' could be 'a', 'aa', or 'aaa')
+        // so for each freq, total ways = freq length
+        // eg. if word=abbcccaa, possible choices is 1 * 2 * 3 * 2
+        long ans = 1;
+        for(var f : freq)
+            ans = (ans * f) % MOD;
+        
+        // if we already have k or fewer freq, then the shortest possible original string
+        // would be of size equal to number of freq (one character per freq).
+        // so, no need to filter out shorter strings — return total
+        if(freq.size() >= k)
+            return (int)ans;
+
+        // now we find all string combinations with length < k and subtract from ans
+        // dp will count the number of original strings whose length < k
+        // dp[j] = number of ways to construct an original string of length j using i freq
+        var dp = new int[k];
+        dp[0] = 1;  // empty string with 0 freq
+
+        for(var i = 1; i <= freq.size(); i++){
+            var prefix = new int[k+1];
+            for(var j = 0; j < k; j++)
+                prefix[j+1] = (prefix[j] +  dp[j]) % MOD;
+
+            dp = new int[k];
+            for(var j = i; j < k; j++) {
+                // for freq i, we want to add a run of at least 1 character and at most min(freq length, j - i + 1)
+                // range sum from prefix[j] to prefix[left]
+                var left = j - Math.min(freq.get(i - 1), j - i + 1);
+                dp[j] = (prefix[j] - prefix[left] + MOD) % MOD;
+            }
+        }
+
+        // sum up the number of ways to form strings of length < k
+        var lessThanK = 0;
+        for(var val : dp)
+            lessThanK = (lessThanK + val) % MOD;
+
+        // subtract shorter strings from total to get valid strings with length ≥ k
+        return ((int)ans - lessThanK + MOD) % MOD;
+    }
+}
