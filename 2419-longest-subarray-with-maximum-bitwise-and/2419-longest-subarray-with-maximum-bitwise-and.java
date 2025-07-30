8fbc8a0be4ebd642b8b429174d66ba0c84cf8726
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-07-30 08:03:20 +0530

    Time: 4 ms (22.41%), Space: 60.3 MB (70.12%)

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
@@ -1,22 +1,21 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int prev = 0, currCount = 0, maxCount = 0, maxVal = 0;
-        for(var i=0; i<nums.length; i++){
-            if(nums[i] > maxVal){
-                currCount = maxCount = 1;
-                maxVal = nums[i];
+        // max AND will always be equal to largest number
+        int maxNum = nums[0], prev = nums[0], maxLen = 0, currLen = 0;
+        for(var num : nums){
+            if(prev != num)
+                currLen = 0;    // reset curr counting
+            if(num > maxNum){
+                maxNum = num;
+                maxLen = 0; // reset max counting
             }
-            if(nums[i] == maxVal){
-                if(prev != maxVal)
-                    currCount = 1;
-                else{
-                    currCount++;
-                    maxCount = Math.max(maxCount, currCount);
-                }
+            if(num == maxNum){
+                currLen++;
+                maxLen = Math.max(maxLen, currLen);
             }
-            prev = nums[i];
+            prev = num;
         }
 
-        return maxCount;
+        return maxLen;
     }
 }
