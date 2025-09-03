a8ed9d30abdcb99e9f5c2a399c601a8fe58868a4
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-09-03 05:50:45 +0530

    Time: 1279 ms (78.43%), Space: 18.3 MB (82.35%)

---
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        SMALLEST = -1_000_000_001  # smallest possible y
+        n = len(points)
+        ans = 0
+
+        # Sort by x ascending, then by y descending
+        points.sort(key=lambda p: (p[0], -p[1]))
+
+        for i in range(n):
+            x1, y1 = points[i]
+            ymax = SMALLEST
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+
+                if y2 > y1: continue  # point 2 is above point 1. We want reverse
+                # we cannot go lower than already visited y, else it will include more points in rectangle
+                if y2 > ymax:
+                    ans += 1
+                    ymax = y2
+
+        return ans
