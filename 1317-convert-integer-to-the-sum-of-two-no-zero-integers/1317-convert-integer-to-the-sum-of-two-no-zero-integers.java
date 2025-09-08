ea16bb701a37dd2df2560c7917a8d9aa0f378d3d
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-09-08 06:23:48 +0530

    Time: 0 ms (100%), Space: 41.4 MB (40.36%)

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        int num1 = 0, num2 = 0, mul = 1;
+        while(n > 0){
+            var digit = n % 10;
+            n /= 10;
+
+            // treat 0 & 1 as 10 & 11
+            // need to check n>0 to handle case where just carry can fill next digit (eg. n = 19)
+            if(digit < 2 && n > 0){
+                num1 += mul * 9;
+                num2 += mul * (1 + digit);  // same as mul * (10 + digit - 9)
+                n--;    // handle carry
+            }
+            else {
+                num1 += mul * 1;
+                num2 += mul * (digit - 1);
+            }
+            mul *= 10;
+        }
+
+        return new int[]{num1, num2};
+    }
+}
