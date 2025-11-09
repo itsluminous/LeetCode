24c201ee4ac025bcfd011aba3e93c4d21e9b967d
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-11-09 04:17:54 +0000

    Time: 0 ms (100%), Space: 29 MB (91.3%)

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cs
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cs
@@ -1,13 +1,13 @@
 public class Solution {
     public int CountOperations(int num1, int num2) {
-        var operations = 0;
-        while(num1 != 0 && num2 != 0){
-            if(num1 == num2) return operations+1;
-            
-            if(num1 > num2) num1 -= num2;
-            else if(num2 > num1) num2 -= num1;
-            operations++;
+        var ans = 0;
+
+        while(num1 != 0 && num2 != 0) {
+            ans += num1 / num2; //  we may have to subtract num2 multiple times, so taking shortcut
+            num1 %= num2;
+            (num1, num2) = (num2, num1);    // swap numbers
         }
-        return operations;
+
+        return ans;
     }
 }
