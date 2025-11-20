47bba5153475a6bc72910724fef41f73df367bfc
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-11-20 16:22:06 +0000

    Time: 7 ms (53.85%), Space: 43.8 MB (61.54%)

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
@@ -2,9 +2,8 @@
 public class Solution {
     public int FindFinalValue(int[] nums, int original) {
         Array.Sort(nums);
-        foreach(var num in nums){
+        foreach(var num in nums)
             if(num == original) original*=2;
-        }
         return original;
     }
 }
