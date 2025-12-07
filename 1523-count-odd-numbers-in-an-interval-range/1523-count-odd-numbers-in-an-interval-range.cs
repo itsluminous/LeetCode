0c7f9ab20c15e3c96bf9b62f734e0c1b1d94a544
Prakash <24225487+itsluminous@users.noreply.github.com>
2025-12-07 12:02:38 +0530

    Time: 20 ms (95.33%), Space: 29.3 MB (24.3%)

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int CountOdds(int low, int high) {
+        var ans = (high - low) / 2;
+        if((low & 1) == 1 || (high & 1) == 1) ans++;
+        return ans;
+    }
+}
