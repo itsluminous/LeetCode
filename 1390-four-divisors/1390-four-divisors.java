a43a3322f72edbbb65bccc2d0364ba835973448c
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-01-04 15:57:39 +0530

    Time: 14 ms (60.1%), Space: 46.2 MB (81.57%)

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        var ans = 0;
+        for(var num : nums){
+            var div = 0;
+            for(var i=2; i <= Math.sqrt(num); i++){
+                if(num % i == 0)
+                    if(div == 0) div = i;   // no other divisor
+                else{
+                    // if number has more than one divisor, then it will have more than 4 total divisors
+                    // because num itself + 1 + prev_div + num / prev_div is already 4
+                    div = 0;
+                    break;
+                }
+            }
+            
+            if(div > 0 && div != num / div) // div and num/div same means it is total 3 divisors
+                ans += num + 1 + div + num / div;
+        }
+
+        return ans;
+    }
+}
