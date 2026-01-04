7421415bffb811bf0d5752deda6769653ef1c6f5
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-01-04 15:29:33 +0530

    Time: 42 ms (80.27%), Space: 109.3 MB (36.68%)

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,7 +1,7 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
         Arrays.sort(happiness);
-        long val = 0;
+        var val = 0L;
 
         for(int i=happiness.length-1, selected = 0; selected < k; i--, selected++){
             var currHappiness = happiness[i] - selected;
