0d5f601757e40fae6da44f9862087ea10901f0e8
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-01-04 15:31:27 +0530

    Time: 3 ms (93.78%), Space: 42.3 MB (34.72%)

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,22 @@
+// explaination : https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/solutions/574923/javacpython-dp-o1-space-by-lee215-cr89
+class Solution {
+    public int numOfWays(int n) {
+        var MOD = 1_000_000_007;
+        // a121: 121, 131, 212, 232, 313, 323
+        // a123: 123, 132, 213, 231, 312, 321
+        long a121 = 6, a123 = 6;
+
+        // b121: 212, 213, 232, 312, 313    = 3 * 121 + 2 * 123
+        // b123: 212, 231, 312, 232         = 2 * 121 + 2 * 123
+        long b121 = 0, b123 = 0;
+
+        for(var i=1; i<n; i++){
+            b121 = 3 * a121 + 2 * a123;
+            b123 = 2 * a121 + 2 * a123;
+            a121 = b121 % MOD;
+            a123 = b123 % MOD;
+        }
+
+        return (int)((a121 + a123) % MOD);
+    }
+}
