bbf67bb2d82dd7dca13819210250a9c8d7a24a5a
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-02-04 16:06:56 +0000

    Time: 12 ms (100%), Space: 73.3 MB (100%)

---
+++ 3640-trionic-array-ii/3640-trionic-array-ii.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public long MaxSumTrionic(int[] nums) {
+        var n = nums.Length;
+        long ans = long.MinValue, sum = nums[0];
+        for(int l=0, r=1, peak=0, valley=0; r < n; r++){
+            sum += nums[r];
+            // same level = reset
+            if(nums[r] == nums[r-1]){
+                sum = nums[r];
+                l = r;
+            }
+            // found down slope
+            else if(nums[r] < nums[r-1]){
+                // expand on left
+                if(r > 1 && nums[r-2] < nums[r-1]){
+                    peak = r-1;
+                    while(l < valley) sum -= nums[l++];
+                    while(l+1 < peak && nums[l] < 0) sum -= nums[l++];
+                }
+            }
+            // found up slope
+            else {
+                if(r > 1 && nums[r-2] > nums[r-1])  valley = r-1;
+                if(l < peak && peak < valley) ans = Math.Max(ans, sum);
+            }
+        }
+
+        return ans;
+    }
+}
