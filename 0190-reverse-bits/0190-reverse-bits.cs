7e74628aca270da7c9482aac697b24c056654ed9
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-02-21 11:07:48 +0000

    Time: 33 ms (7.08%), Space: 29.2 MB (32.19%)

---
+++ 0190-reverse-bits/0190-reverse-bits.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int ReverseBits(int n) {
+        if (n == 0) return 0;
+    
+        var result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            if ((n & 1) == 1) result++;
+            n >>= 1;
+        }
+        return result;
+    }
+}
