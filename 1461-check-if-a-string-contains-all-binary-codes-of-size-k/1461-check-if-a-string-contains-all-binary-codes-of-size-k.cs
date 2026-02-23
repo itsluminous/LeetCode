9983e356a4347819dec838f94e2d658c1f002ece
Prakash <24225487+itsluminous@users.noreply.github.com>
2026-02-23 06:35:46 +0530

    Time: 143 ms (31.25%), Space: 94.8 MB (12.5%)

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cs
@@ -1,40 +1,13 @@
 public class Solution {
     public bool HasAllCodes(string s, int k) {
-        // create a set of all possible numbers in given string
-        var codes = new HashSet<string>();
-        for(var i=0; i<=s.Length-k; i++){
-            codes.Add(s.Substring(i, k));
-        }
+        var str = new HashSet<string>();
+        for(var i=0; i<=s.Length-k; i++)
+            str.Add(s.Substring(i, k));
         
-        // max possible number with k size
-        var max = 1 << k;
-        
-        // if total items in set is not same as max possible, then some numbers are missing
-        if(max != codes.Count) return false;
-        return true;
-    }
-}
+        var expectedCount = (1 << k);   // this is max number possible with k binary digits
+        var actualCount = str.Count;
 
-// Accepted - same logic as above but extra loops and string to int conversion
-public class Solution1 {
-    public bool HasAllCodes(string s, int k) {
-        // create a set of all possible numbers in given string
-        var codes = new HashSet<int>();
-        for(var i=0; i<=s.Length-k; i++){
-            var str = s.Substring(i, k);
-            codes.Add(Convert.ToInt32(str, 2));
-        }
-        
-        // generate max possible number with k size
-        var max_str = new StringBuilder();
-        for(var i=0; i<k; i++) max_str.Append("1");
-        var max = Convert.ToInt32(max_str.ToString(), 2);
-        
-        // check if all numbers are present
-        for(var i=0; i<=max; i++){
-            if(!codes.Contains(i))
-                return false;
-        }
-        return true;
+        // if total items in set is not same as max possible, then some numbers are missing
+        return expectedCount == actualCount;
     }
 }
